238453af61908bdfe414cac2bd492e289646df0f
Ubuntu <howardc93@gmail.com>
2016-05-28 04:45:45 +0000

    try adding travis

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+rev=$(git rev-parse --short HEAD)
+
+git config user.name "Travis CI"
+git config user.email "travis@travis-ci.org"
+
+git remote add upstream "https://$GH_TOKEN@github.com/yasp-dota/ui.git"
+git add --all
+git commit -m "rebuild pages at ${rev}"
+git push -f upstream HEAD:gh-pages
