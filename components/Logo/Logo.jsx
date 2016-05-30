b5987ba0e508a56f1991e29336bb22413b12ab8e
Mike Robertson <michael.robertson@kroger.com>
2016-05-30 12:28:59 -0400

    Mike | fixed it so that logo uses a <Link> element instead (otherwise it causes page refresh)

--- components/Logo/Logo.jsx
+++ components/Logo/Logo.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default () => (
   <div className="navbar-header">
-    <a href="/" className="navbar-brand">
+    <Link to="/" className="navbar-brand">
       <strong className="theme-blue">YASP</strong>
       <div data-toggle="collapse" data-target=".navbar-collapse" className="navbar-toggle">
         <span className="icon-bar" />
         <span className="icon-bar" />
         <span className="icon-bar" />
       </div>
-    </a>
+    </Link>
   </div>
 );
