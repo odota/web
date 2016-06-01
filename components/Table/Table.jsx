68379af835886065008afbf416da69bae3f36cd6
Mike Robertson <michael.robertson@kroger.com>
2016-06-01 15:07:16 -0400

    Mike | updated table so it is no longer a monolith. Material tableheader and tablebody components must be in the top level table or they won't render. I don't know why, probably has something to do with the fact that they are tables and how material-ui is wired under the hood

--- components/Table/Table.jsx
+++ components/Table/Table.jsx
@@ -4,77 +4,44 @@
 import React from 'react';
 import {
   Table as MaterialTable,
-  TableBody,
-  TableHeader,
-  TableHeaderColumn,
-  TableRow,
-  TableRowColumn,
+  TableBody as MaterialTableBody,
+  TableHeader as MaterialTableHeader,
+  TableRow as MaterialTableRow,
+  TableRowColumn as MaterialTableRowColumn,
 } from 'material-ui/Table';
+import TableHeader from './TableHeader';
 import Spinner from '../Spinner';
-import { Text } from '../Text';
 import Error from '../Error';
 import styles from './Table.css';
-import FontIcon from 'material-ui/FontIcon';
-// import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+// import TableBody from './TableBody';
+import TableRow from './TableRow';
+import { getTotalWidth } from './tableHelpers';
 
 const Table = ({ data, columns, loading, error, sortState, sortField, sortClick }) => {
-  const totalWidth = columns.reduce((prev, current) => prev + current.width, 0);
-
-  const getWidthStyle = (column, total) => ({ width: `${column / total}%` });
-
-  const isSortField = (sortField, field) => sortField === field;
-
-  const getSortIcon = (sortState, sortField, field) => {
-    let sort = 'sort';
-    if (isSortField(sortField, field)) {
-      if (sortState === 'asc') {
-        sort = 'arrow_upward';
-      } else if (sortState === 'desc') {
-        sort = 'arrow_downward';
-      }
-    }
-    return sort;
-  };
+  const totalWidth = getTotalWidth(columns);
 
   const getTable = () => (
     <MaterialTable selectable={false}>
-      <TableHeader displaySelectAll={false} adjustForCheckbox={false} className={styles.header}>
-        <TableRow>
-          {columns.map((column, index) => (
-            <TableHeaderColumn
-              key={index}
-              style={getWidthStyle(column.width, totalWidth)}
-            >
-              <div
-                className={styles.headerCell}
-                onClick={() => sortClick(column.field, isSortField(sortField, column.field) ? sortState : '', column.sortFn)}
-              >
-                <Text size={16}>{column.displayName}</Text>
-                <FontIcon
-                  style={{ fontSize: 16 }}
-                  className="material-icons"
-                >
-                  {getSortIcon(sortState, sortField, column.field)}
-                </FontIcon>
-              </div>
-            </TableHeaderColumn>
-          ))}
-        </TableRow>
-      </TableHeader>
-      <TableBody displayRowCheckbox={false} selectable={false}>
+      <MaterialTableHeader displaySelectAll={false} adjustForCheckbox={false} className={styles.header}>
+        <TableHeader
+          columns={columns}
+          sortState={sortState}
+          sortField={sortField}
+          sortClick={sortClick}
+          totalWidth={totalWidth}
+        />
+      </MaterialTableHeader>
+      <MaterialTableBody displayRowCheckbox={false} selectable={false}>
         {data.map((match, index) => (
-          <TableRow key={index} className={styles[`row${index % 2}`]}>
-            {columns.map((column, indexCol) => (
-              <TableRowColumn
-                key={indexCol}
-                style={getWidthStyle(column.width, totalWidth)}
-              >
-                {column.component ? column.component(match[column.field]) : match[column.field]}
-              </TableRowColumn>
-            ))}
-          </TableRow>
+          <TableRow
+            key={index}
+            match={match}
+            columns={columns}
+            totalWidth={totalWidth}
+            index={index}
+          />
         ))}
-      </TableBody>
+      </MaterialTableBody>
     </MaterialTable>
   );
 
