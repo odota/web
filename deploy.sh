b897f57c7124c091f71086fa397fb90f1d1d95eb
Ubuntu <howardc93@gmail.com>
2016-06-04 20:26:47 +0000

    gitignore build

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,6 @@ git config user.email "travis@travis-ci.org"
 
 git remote add upstream "https://$GH_TOKEN@github.com/yasp-dota/ui.git"
 git add --all
-git commit -m "rebuild pages at $TRAVIS_COMMIT"
+git add --force build
+git commit -m "Build $TRAVIS_COMMIT"
 git push -f upstream HEAD:gh-pages
