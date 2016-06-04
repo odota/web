1dcd654bfb6087edc863519a1558ec744f8a660d
Ubuntu <howardc93@gmail.com>
2016-06-04 03:30:44 +0000

    fix lint

--- components/Table/TableRowColumn.jsx
+++ components/Table/TableRowColumn.jsx
@@ -4,6 +4,7 @@ import { getWidthStyle } from './tableHelpers';
 
 export default ({ row, column, totalWidth }) => (
   <MaterialTableRowColumn style={getWidthStyle(column.width, totalWidth)}>
-    {row && column.dispFn ? column.dispFn(row, column, row[column.field]) : row[column.field] ? row[column.field].display || row[column.field] : ""}
+    {row && column.dispFn && column.dispFn(row, column, row[column.field])}
+    {row && row[column.field] && !column.dispFn && (row[column.field].display || row[column.field])}
   </MaterialTableRowColumn>
 );
