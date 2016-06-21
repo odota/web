691c3e29c8ac7186e31f0eaa52f1aaded125d452
Mike Robertson <mjr127+github@case.edu>
2016-06-21 17:59:00 -0400

    Mike | fixed bug with playermatches hero images

--- selectors/utility.js
+++ selectors/utility.js
@@ -1,14 +1,12 @@
 import transform from '../transformations';
 
-export const transformationFunction = (list, constants) => {
-  return list.map(listItem => {
-    const transformedList = {};
-    Object.keys(listItem).forEach((field) => {
-      transformedList[`${field}`] = {
-        display: transform(listItem, field, constants),
-        value: listItem[field],
-      };
-    });
-    return transformedList;
+export const transformationFunction = (list, constants) => list.map(listItem => {
+  const transformedList = {};
+  Object.keys(listItem).forEach((field) => {
+    transformedList[`${field}`] = {
+      display: transform(listItem, field, constants),
+      value: listItem[field],
+    };
   });
-}
+  return transformedList;
+});
