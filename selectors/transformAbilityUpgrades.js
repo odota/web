22472e81c85606b91f9c1a141ffd33f17fcd058b
Mike Robertson <mjr127+github@case.edu>
2016-06-22 16:51:20 -0400

    Mike | fix the linting

--- selectors/transformAbilityUpgrades.js
+++ selectors/transformAbilityUpgrades.js
@@ -4,7 +4,6 @@ import { transformationFunction } from './utility';
 
 const getMatchWithAbilities = (state) => {
   const matchArray = state[REDUCER_KEY].gotMatch.match.players.matchArray;
-  console.log(matchArray[0])
   return matchArray && matchArray[0] && matchArray[0].ability_upgrades_arr ?
     matchArray :
     null;
