38d6f8f72dfb08ee15bfcb11f70bc1e5eacaad6b
Mike Robertson <mjr127+github@case.edu>
2016-06-22 14:25:33 -0400

    Mike | fixed tabbar so that it will select the tab of the proper router you go to if you are going straight to a route (ex: players/12345/matches will preselect the matches tab)

--- components/TabBar/TabBar.jsx
+++ components/TabBar/TabBar.jsx
@@ -3,8 +3,13 @@ import { Tabs, Tab } from 'material-ui/Tabs';
 import { withRouter } from 'react-router';
 import styles from './TabBar.css';
 
+const getInitialSelectedRoute = (tabs, router) => tabs.findIndex((tab) => router.isActive(tab.route));
+
 const TabBar = ({ router, tabs }) => (
-  <Tabs inkBarStyle={{ backgroundColor: styles.inkBarColor }}>
+  <Tabs
+    inkBarStyle={{ backgroundColor: styles.inkBarColor }}
+    initialSelectedIndex={getInitialSelectedRoute(tabs, router)}
+  >
     {tabs.map((tab, index) => (
       <Tab key={index} className={styles.tab} value={tab.route} label={tab.label} onActive={(tab) => router.push(tab.props.value)} />
     ))}
