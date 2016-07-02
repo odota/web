3f5b96645b4b76184f743f1169cd55112c40472d
pcollinsmusic <thepatrickcollins@gmail.com>
2016-07-01 21:03:47 -0400

    Heroes Tab Complete
    Corrections made. Functionality is complete minus sorting of win%
    with/against and the fact that not all heroes show up.

--- components/Table/columnDefinitions/index.js
+++ components/Table/columnDefinitions/index.js
@@ -1,5 +1,6 @@
 import playerMatchesColumns from './playerMatchesColumns';
-import playerHeroesColumns from './playerHeroesColumns';
+import { playerHeroesOverviewColumns } from './playerHeroesColumns';
 import { overviewColumns } from './matchColumns';
+import playerHeroesColumns from './playerHeroesColumns'
 
-export { playerMatchesColumns, overviewColumns, playerHeroesColumns };
+export { playerMatchesColumns, overviewColumns, playerHeroesOverviewColumns, playerHeroesColumns};
--- components/Table/columnDefinitions/playerHeroesColumns.jsx
+++ components/Table/columnDefinitions/playerHeroesColumns.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { defaultSort, transformedSort } from './utility';
+import { defaultSort, transformedSort, useOriginalValueSort } from './utility';
 import { HOST_URL } from '../../../yasp.config';
 import styles from './playerHeroesColumns.css';
 import PiePercent from '../../PiePercent';
@@ -8,15 +8,14 @@ const winPercentTransform = (list, field, property) => list[field][property] / l
 
 const getPercentWin = (wins, games) => Math.ceil(1000 * (wins / games)) / 10;
 
-export default [{
+const playerHeroesOverviewColumns = [{
   displayName: 'Hero',
   field: 'hero_id',
   width: 2,
   displayFn: ({ field }) => (
     <div style={{ marginTop: 5 }}>
       <img src={`${HOST_URL}${field.display.img}`} style={{ height: 30 }} role="presentation" />
-    </div>
-  ),
+    </div>),
 }, {
   displayName: 'Played',
   field: 'games',
@@ -32,7 +31,54 @@ export default [{
       <span>
         <PiePercent percent={getPercentWin(field.display, row.games.display)} />
       </span>
-    </div>
-  ),
+    </div>),
   sortFn: transformedSort.bind(null, winPercentTransform),
 }];
+
+const restColumns = [{
+  displayName: 'Last Match',
+  field: 'last_played',
+  width: 2,
+  sortFn: useOriginalValueSort,
+}, {
+  displayName: 'With',
+  field: 'with_games',
+  width: 1.5,
+  sortFn: defaultSort,
+}, {
+  displayName: 'Win %',
+  field: 'with_win',
+  width: 2,
+  displayFn: ({ field, row }) => (
+    <div className={styles.percentContainer}>
+      <span className={styles.textContainer}>{getPercentWin(field.display, row.with_games.display).toFixed(1)}</span>
+      <span>
+        <PiePercent percent={getPercentWin(field.display, row.with_games.display)} />
+      </span>
+    </div>),
+  sortFn: transformedSort.bind(null, winPercentTransform),
+}, {
+  displayName: 'Against',
+  field: 'against_games',
+  width: 1.5,
+  sortFn: defaultSort,
+}, {
+  displayName: 'Win %',
+  field: 'against_win',
+  width: 2,
+  displayFn: ({ field, row }) => (
+    <div className={styles.percentContainer}>
+      <span className={styles.textContainer}>{getPercentWin(field.display, row.against_games.display).toFixed(1)}</span>
+      <span>
+        <PiePercent percent={getPercentWin(field.display, row.against_games.display)} />
+      </span>
+    </div>),
+  sortFn: transformedSort.bind(null, winPercentTransform),
+}];
+
+export default [
+  ...playerHeroesOverviewColumns,
+  ...restColumns,
+];
+
+export { playerHeroesOverviewColumns };
