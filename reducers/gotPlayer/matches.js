33d7eb74531ef5927f36e99ee3c6f9171e99d8d4
Mike Robertson <mjr127+github@case.edu>
2016-07-04 12:03:07 -0400

    Mike | add reducerFactory which is based off the default pattern for reducers. This will only work for things which have a list of data (example: player matches, matches, etc.). It can be modified if there need to be reducers with more than just list based reducers

--- reducers/gotPlayer/matches.js
+++ reducers/gotPlayer/matches.js
@@ -1,84 +1,17 @@
 import { playerMatchesActions } from '../../actions';
-import { SORT_ENUM } from '../utility';
-import { combineReducers } from 'redux';
+import createReducer from '../reducerFactory';
 
 const initialState = {
   loading: true,
   error: false,
   loaded: false,
-  matchList: [],
+  list: [],
   sortState: '',
   sortField: '',
   sortFn: f => f,
 };
 
-const matches = (state = initialState, action) => {
-  switch (action.type) {
-    case playerMatchesActions.REQUEST:
-      return {
-        ...state,
-        loaded: false,
-        loading: true,
-      };
-    case playerMatchesActions.OK:
-      return {
-        ...state,
-        loading: false,
-        loaded: true,
-        matchList: [...action.payload],
-      };
-    case playerMatchesActions.ERROR:
-      return {
-        ...state,
-        loading: false,
-        loaded: false,
-        error: true,
-      };
-    case playerMatchesActions.SORT:
-      return {
-        ...state,
-        sortState: action.sortField === state.sortField ? SORT_ENUM.next(SORT_ENUM[state.sortState]) : SORT_ENUM[0],
-        sortField: action.sortField,
-        sortFn: action.sortFn,
-      };
-    default:
-      return state;
-  }
-};
-
-
-const byId = (state = {}, action) => {
-  switch (action.type) {
-    case playerMatchesActions.REQUEST:
-    case playerMatchesActions.OK:
-    case playerMatchesActions.ERROR:
-    case playerMatchesActions.SORT:
-      return {
-        ...state,
-        [action.id]: matches(state[action.id], action),
-      };
-    default:
-      return state;
-  }
-};
-
-const allIds = (state = [], action) => {
-  switch (action.type) {
-    case playerMatchesActions.OK:
-      if (state.includes(action.id)) {
-        return state;
-      }
-      return [...state, action.id];
-    default:
-      return state;
-  }
-};
-
-export default combineReducers({
-  byId,
-  allIds,
-});
-
+export default createReducer(initialState, playerMatchesActions);
 
 export const getPlayerMatches = {
   getPlayerMatchesById: (state, id) => {
@@ -92,7 +25,7 @@ export const getPlayerMatches = {
   getError: (state, id) => getPlayerMatches.getPlayerMatchesById(state, id).error,
   getLoading: (state, id) => getPlayerMatches.getPlayerMatchesById(state, id).loading,
   isLoaded: (state, id) => getPlayerMatches.getPlayerMatchesById(state, id).loaded,
-  getMatchList: (state, id) => getPlayerMatches.getPlayerMatchesById(state, id).matchList,
+  getMatchList: (state, id) => getPlayerMatches.getPlayerMatchesById(state, id).list,
   getSortState: (state, id) => getPlayerMatches.getPlayerMatchesById(state, id).sortState,
   getSortField: (state, id) => getPlayerMatches.getPlayerMatchesById(state, id).sortField,
   getSortFn: (state, id) => getPlayerMatches.getPlayerMatchesById(state, id).sortFn,
--- reducers/gotPlayer/peers.js
+++ reducers/gotPlayer/peers.js
@@ -1,84 +1,17 @@
 import { playerPeersActions } from '../../actions';
-import { SORT_ENUM } from '../utility';
-import { combineReducers } from 'redux';
+import createReducer from '../reducerFactory';
 
 const initialState = {
   loading: true,
   error: false,
   loaded: false,
-  peerList: [],
+  list: [],
   sortState: '',
   sortField: '',
   sortFn: f => f,
 };
 
-const peers = (state = initialState, action) => {
-  switch (action.type) {
-    case playerPeersActions.REQUEST:
-      return {
-        ...state,
-        loaded: false,
-        loading: true,
-      };
-    case playerPeersActions.OK:
-      return {
-        ...state,
-        loading: false,
-        loaded: true,
-        peerList: [...action.payload],
-      };
-    case playerPeersActions.ERROR:
-      return {
-        ...state,
-        loading: false,
-        loaded: false,
-        error: true,
-      };
-    case playerPeersActions.SORT:
-      return {
-        ...state,
-        sortState: action.sortField === state.sortField ? SORT_ENUM.next(SORT_ENUM[state.sortState]) : SORT_ENUM[0],
-        sortField: action.sortField,
-        sortFn: action.sortFn,
-      };
-    default:
-      return state;
-  }
-};
-
-
-const byId = (state = {}, action) => {
-  switch (action.type) {
-    case playerPeersActions.REQUEST:
-    case playerPeersActions.OK:
-    case playerPeersActions.ERROR:
-    case playerPeersActions.SORT:
-      return {
-        ...state,
-        [action.id]: peers(state[action.id], action),
-      };
-    default:
-      return state;
-  }
-};
-
-const allIds = (state = [], action) => {
-  switch (action.type) {
-    case playerPeersActions.OK:
-      if (state.includes(action.id)) {
-        return state;
-      }
-      return [...state, action.id];
-    default:
-      return state;
-  }
-};
-
-export default combineReducers({
-  byId,
-  allIds,
-});
-
+export default createReducer(initialState, playerPeersActions);
 
 export const getPlayerPeers = {
   getPlayerPeersById: (state, id) => {
@@ -92,7 +25,7 @@ export const getPlayerPeers = {
   getError: (state, id) => getPlayerPeers.getPlayerPeersById(state, id).error,
   getLoading: (state, id) => getPlayerPeers.getPlayerPeersById(state, id).loading,
   isLoaded: (state, id) => getPlayerPeers.getPlayerPeersById(state, id).loaded,
-  getPeerList: (state, id) => getPlayerPeers.getPlayerPeersById(state, id).peerList,
+  getPeerList: (state, id) => getPlayerPeers.getPlayerPeersById(state, id).list,
   getSortState: (state, id) => getPlayerPeers.getPlayerPeersById(state, id).sortState,
   getSortField: (state, id) => getPlayerPeers.getPlayerPeersById(state, id).sortField,
   getSortFn: (state, id) => getPlayerPeers.getPlayerPeersById(state, id).sortFn,
