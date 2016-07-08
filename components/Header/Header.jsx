fd6758a9133e5096a9f8591ba83aef12c77b4de1
Ubuntu <howardc93@gmail.com>
2016-07-08 08:12:12 +0000

    do the same for player/match routing tabs

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -5,36 +5,148 @@ import AccountWidget from '../AccountWidget';
 import AppBar from 'material-ui/AppBar';
 import styles from './Header.css';
 import TabBar from '../TabBar';
-import constants from '../../constants';
+const playerPages = (accountId) => [
+{
+  'name': 'Overview',
+},
+{
+  'name': 'Matches',
+},
+{
+  'name': 'Heroes',
+},
+{
+  'name': 'Peers',
+},
+{
+  'name': 'Pros',
+},
+{
+  'name': 'Activity',
+},
+{
+  'name': 'Records',
+},
+{
+  'name': 'Counts',
+},
+{
+  'name': 'Histograms',
+},
+{
+  'name': 'Trends',
+},
+{
+  'name': 'Wardmap',
+},
+{
+  'name': 'Items',
+},
+{
+  'name': 'Wordcloud',
+},
+{
+  'name': 'MMR',
+},
+{
+  'name': 'Rankings',
+  'new-feature': true
+}].map(function (e)
+{
+  return Object.assign(
+  {}, e,
+  {
+    route: `/players/${accountId}/${e.name.toLowerCase()}`,
+    label: e.name,
+  });
+});
 
-const getPlayerPages = (playerId) => Object.keys(constants.player_pages).map(
-  (key) => {
-    if (constants.player_pages[key].name.toLowerCase() === 'overview') {
-      return { route: `/players/${playerId}/overview`, label: constants.player_pages[key].name };
-    }
-    return { route: `/players/${playerId}/${key}`, label: constants.player_pages[key].name };
+const matchPages = (matchId) => [
+{
+  'name': 'Overview',
+},
+{
+  'name': 'Benchmarks',
+},
+{
+  'name': 'Performances',
+  'parsed': true,
+},
+{
+  'name': 'Damage',
+  'parsed': true,
+},
+{
+  'name': 'Purchases',
+  'parsed': true,
+},
+{
+  'name': 'Farm',
+  'parsed': true,
+},
+{
+  'name': 'Combat',
+  'parsed': true,
+},
+{
+  'name': 'Graphs',
+  'parsed': true,
+},
+{
+  'name': 'Vision',
+  'parsed': true,
+},
+{
+  'name': 'Objectives',
+  'parsed': true,
+},
+{
+  'name': 'Teamfights',
+  'parsed': true,
+},
+{
+  'name': 'Actions',
+  'parsed': true,
+},
+{
+  'name': 'Analysis',
+  'parsed': true,
+},
+{
+  'name': 'Chat',
+  'parsed': true,
+}].map(function (e)
+{
+  return Object.assign(
+  {}, e,
+  {
+    route: `/matches/${matchId}/${e.name.toLowerCase()}`,
+    label: e.name,
+  });
+});
+
+const getTabBar = (params, location) =>
+{
+  if (location.pathname.startsWith('/players'))
+  {
+    return (<div className={styles.tabBarContainer}>
+        <TabBar tabs={playerPages(params.account_id)} />
+      </div>);
   }
-);
-
-const getTabBar = (params, location) => {
-  if (location.pathname.includes('/players')) {
-    return (
-      <div className={styles.tabBarContainer}>
-        <TabBar tabs={getPlayerPages(params.account_id)} />
-      </div>
-    );
-  } else if (location.pathname.includes('/matches')) {
-    return (
-      <div className={styles.tabBarContainer}>
-        <TabBar tabs={getPlayerPages(params.account_id)} />
-      </div>
-    );
+  else if (location.pathname.startsWith('/matches'))
+  {
+    return (<div className={styles.tabBarContainer}>
+        <TabBar tabs={matchPages(params.match_id)} />
+      </div>);
   }
   return <div className={styles.emptyTabBarContainer} />;
 };
-
-export default ({ openMenu, params, location }) => (
-  <div>
+export default (
+{
+  openMenu,
+  params,
+  location
+}) => (<div>
     <AppBar
       className={styles.header}
       iconElementRight={(<AccountWidget />)}
@@ -45,5 +157,4 @@ export default ({ openMenu, params, location }) => (
     />
   {getTabBar(params, location)}
     <NavDrawer />
-  </div>
-);
+  </div>);
--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -1,55 +1,50 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import Spinner from '../Spinner';
-import Error from '../Error';
 import { Link } from 'react-router';
 import Divider from 'material-ui/Divider';
 import styles from './NavBar.css';
-import { openMenu } from '../../actions';
-import constants from '../../constants';
 
-const pages = {
-  "request": {
-    "name": "Request",
-    "path": "/request"
+const pages = [
+  {
+    'name': 'Request',
+    'path': '/request'
   },
-  "rankings": {
-    "name": "Rankings",
-    "path": "/rankings",
-    "new-feature": true
+  {
+    'name': 'Rankings',
+    'path': '/rankings',
+    'new-feature': true
   },
-  "benchmarks": {
-    "name": "Benchmarks",
-    "path": "/benchmarks"
+  {
+    'name': 'Benchmarks',
+    'path': '/benchmarks'
   },
-  "distributions": {
-    "name": "Distributions",
-    "path": "/distributions"
+  {
+    'name': 'Distributions',
+    'path': '/distributions'
   },
-  "picks": {
-    "name": "Picks",
-    "path": "/picks"
+  {
+    'name': 'Picks',
+    'path': '/picks'
   },
-  "mmstats": {
-    "name": "MMStats",
-    "path": "/mmstats"
+  {
+    'name': 'MMStats',
+    'path': '/mmstats'
   },
-  "carry": {
-    "name": "Carry",
-    "path": "/carry"
+  {
+    'name': 'Carry',
+    'path': '/carry'
   },
-  "search": {
-    "name": "Search",
-    "path": "/search"
+  {
+    'name': 'Search',
+    'path': '/search'
   }
-};
+];
 
 export default function NavBar({ toggleMenu })
 {
-    const links = Object.keys(pages).map((link, index) => (
-      <Link key={index} to={pages[link].path} onTouchTap={toggleMenu}>
+    const links = pages.map((p, index) => (
+      <Link key={index} to={p.path} onTouchTap={toggleMenu}>
         <li className={styles.listItem}>
-          {pages[link].name}
+          {p.name}
         </li>
         <Divider style={{ backgroundColor: styles.dividerColor }} />
       </Link>
