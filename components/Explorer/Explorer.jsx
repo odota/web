0c8b4e6909757604b760e564150b16e9a6520aba
Ubuntu <howardc93@gmail.com>
2016-07-12 08:27:11 +0000

    improve data explorer

--- components/Explorer/Explorer.jsx
+++ components/Explorer/Explorer.jsx
@@ -5,9 +5,12 @@ require('brace/theme/monokai');
 import fetch from 'isomorphic-fetch';
 import { HOST_URL } from '../../yasp.config';
 import RaisedButton from 'material-ui/RaisedButton';
-import TextField from 'material-ui/TextField';
-import { List, ListItem } from 'material-ui/List';
 import { Tabs, Tab } from 'material-ui/Tabs';
+import Popover from 'material-ui/Popover';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+import Spinner from '../Spinner';
+import queries from './queries';
 // import {blue300} from 'material-ui/styles/colors';
 import {
     Table,
@@ -22,54 +25,33 @@ function jsonResponse(response) {
   return response.json();
 }
 
-function handleResponse(json) {
-  this.editor.setValue(json.sql);
-  window.history.pushState('', '', `?id=${json.id}`);
-  this.setState(Object.assign(
-  {}, this.state,
-  {
-    result: json,
-  }));
-}
-
-function handleExamples(json) {
-  this.setState(Object.assign(
-  {}, this.state,
-  {
-    examples: json,
-  }));
-}
-
 class Explorer extends React.Component
 {
   constructor() {
     super();
     this.state = {
-      examples: [],
-      result:
-      {
-        result:
-        {
-          rows: [],
-          fields: [],
-        },
-      },
+      loading: false,
+      result: {},
     };
+    this.handleQuery = this.handleQuery.bind(this);
+    this.handleExampleChange = this.handleExampleChange.bind(this);
+    this.handleResponse = this.handleResponse.bind(this);
+    this.handleTouchTap = this.handleTouchTap.bind(this);
+    this.handleRequestClose = this.handleRequestClose.bind(this);
   }
   componentDidMount() {
-    this.editor = ace.edit('editor');
-    this.handleClick = this.handleClick.bind(this);
-    const editor = this.editor;
+    const editor = ace.edit('editor');
     editor.setTheme('ace/theme/monokai');
     editor.getSession().setMode('ace/mode/sql');
     editor.setShowPrintMargin(false);
+    this.editor = editor;
     const id = this.props.location.query.id;
     if (id) {
-      fetch(`${HOST_URL}/api/explorer?id=${id}`).then(jsonResponse).then(handleResponse.bind(this));
+      fetch(`${HOST_URL}/api/explorer?id=${id}`).then(jsonResponse).then(this.handleResponse);
     }
-    fetch(`${HOST_URL}/api/explorer/examples`).then(jsonResponse).then(handleExamples.bind(this));
   }
-  handleClick() {
+  handleQuery() {
+    this.setState(Object.assign({}, this.state, { loading: true }));
     fetch(`${HOST_URL}/api/explorer`,
     {
       method: 'post',
@@ -80,75 +62,100 @@ class Explorer extends React.Component
       },
       body: JSON.stringify(
       {
-        name: document.getElementById('name').value,
         sql: this.editor.getValue(),
       }),
-    }).then(jsonResponse).then(handleResponse.bind(this));
+    }).then(jsonResponse).then(this.handleResponse);
+  }
+  handleExampleChange(event, value) {
+    this.editor.setValue(queries[value].sql);
+    this.handleRequestClose();
+  }
+  handleResponse(json) {
+    window.history.pushState('', '', `?id=${json.id}`);
+    this.editor.setValue(json.sql);
+    this.setState(Object.assign(
+    {}, this.state,
+    {
+      loading: false,
+      open: false,
+      result: json,
+    }));
+  }
+  handleTouchTap(event) {
+    // This prevents ghost click.
+    event.preventDefault();
+
+    this.setState({
+      open: true,
+    });
+  }
+  handleRequestClose() {
+    this.setState({
+      open: false,
+    });
   }
   render() {
     return (<div>
       <h3>Data Explorer
         <small> - Explore data from professional Dota 2 matches</small>
       </h3>
-      <div style={{ display: 'flex' }}>
-        <div
-          style={{
-            width: '50%',
-            height: '400px',
-            overflow: 'auto',
-          }}
-        >
-          <h4>Examples</h4>
-          <List id={'example'}>
-            {this.state.examples.map(function example(e) {
-              return (<a href={`/explorer?id=${e.id}`}>
-                <ListItem primaryText={e.name} />
-              </a>);
-            })
-            }
-          </List>
-        </div>
-        <div style={{ width: '50%' }}>
-          <TextField
-            id={'name'}
-            hintText={'Name this query'}
-            value={this.state.result.name}
-          />
-          <h4>SQL</h4>
-          <div id={'editor'} style={{ width: '100%', height: '200px' }}></div>
+      <div>
+        <div>
           <RaisedButton
+            onTouchTap={this.handleTouchTap}
+            label={'Examples'}
+          />
+          <Popover
+            open={this.state.open}
+            onRequestClose={this.handleRequestClose}
+          >
+            <Menu onChange={this.handleExampleChange}>
+              {Object.keys(queries).map(function example(k) {
+                const e = queries[k];
+                return <MenuItem value={k} primaryText={e.name} />;
+              })
+              }
+            </Menu>
+          </Popover>
+        </div>
+        <div>
+          <div id={'editor'} style={{ width: '100%', height: 200 }}></div>
+          <RaisedButton
+            style={{ margin: '5px' }}
             label={'Query'}
-            onClick={this.handleClick}
+            onClick={this.handleQuery}
           />
         </div>
       </div>
       <Tabs>
         <Tab label={'Table'}>
-          <Table id={'table'}>
-            <TableHeader displaySelectAll={false} adjustForCheckbox={false} >
-              <TableRow>
-                {this.state.result.result ? this.state.result.result.fields.map(function field(f) {
-                  return <TableHeaderColumn>{f.name}</TableHeaderColumn>;
+          {!this.state.loading ?
+            <Table>
+              <TableHeader displaySelectAll={false} adjustForCheckbox={false} >
+                <TableRow>
+                  {this.state.result.result && !this.state.loading ? this.state.result.result.fields.map(function field(f) {
+                    return <TableHeaderColumn>{f.name}</TableHeaderColumn>;
+                  }) : []
+                  }
+                </TableRow>
+              </TableHeader>
+              <TableBody displayRowCheckbox={false}>
+                {this.state.result.result && !this.state.loading ? this.state.result.result.rows.map(function row(r) {
+                  return (<TableRow>
+                  {Object.keys(r).map(function key(k) {
+                      return (<TableRowColumn>{r[k]}</TableRowColumn>);
+                  })
+                  }
+                  </TableRow>);
                 }) : []
                 }
-              </TableRow>
-            </TableHeader>
-            <TableBody displayRowCheckbox={false}>
-              {this.state.result.result ? this.state.result.result.rows.map(function row(r) {
-                return (<TableRow>
-                {Object.keys(r).map(function key(k) {
-                    return (<TableRowColumn>{r[k]}</TableRowColumn>);
-                })
-                }
-                </TableRow>);
-              }) : []
-              }
-            </TableBody>
-          </Table>
+              </TableBody>
+            </Table> : <Spinner />
+          }
         </Tab>
         <Tab label={'Raw'}>
-          <pre id={'result'}>
-            {JSON.stringify(this.state.result, null, 2)}
+          <pre style={{ whiteSpace: 'pre-wrap' }}>
+            {JSON.stringify(this.state, null, 2)}
           </pre>
         </Tab>
       </Tabs>
