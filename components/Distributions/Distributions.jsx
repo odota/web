c9ca186f1be13dcabc538710aec6a3adc4fb0d63
Ubuntu <howardc93@gmail.com>
2016-08-09 06:25:33 +0000

    fix linting

--- components/Distributions/Distributions.jsx
+++ components/Distributions/Distributions.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import fetch from 'isomorphic-fetch';
 import { API_HOST } from '../../yasp.config';
-import CircularProgress from 'material-ui/CircularProgress';
+// import CircularProgress from 'material-ui/CircularProgress';
 import { Tabs, Tab } from 'material-ui/Tabs';
 import c3 from 'c3';
 
@@ -19,82 +19,83 @@ class Distributions extends React.Component {
     this.componentDidMount = this.componentDidMount.bind(this);
     this.gotData = this.gotData.bind(this);
   }
+  componentDidMount() {
+    fetch(`${API_HOST}/api/distributions`).then(jsonResponse).then(this.gotData);
+  }
   gotData(json) {
-    this.setState(Object.assign({}, this.state, {data: json}));
-    var mmr = this.state.data.mmr && this.state.data.mmr.rows;
+    const mmr = this.state.data.mmr && this.state.data.mmr.rows;
+    this.setState(Object.assign({}, this.state, { data: json }));
     if (mmr) {
-      var counts = mmr.map(function(d){return d.count;});
-      var count = counts.reduce(function(c,n){return c+n;},0);
-      var names = mmr.map(function(d){return d.bin_name});
-      var pcts = mmr.map(function(d){return d.cumulative_sum/count*100;});
-      var options = {
-          bindto: "#mmr",
-          size: {height: 500},
-          data: {
-              x: 'MMR',
-              columns: [
-              ['MMR'].concat(names),
-              ['Players'].concat(counts),
-              ['Percentile'].concat(pcts)
-              ],
-              type: "bar",
-              types: {
-              "Percentile": "spline"
-              },
-              axes: {
-              "Players":"y",
-              "Percentile":"y2"
-              },
-              groups: [
-              ['Players','Percentile']
-              ]
+      const counts = mmr.map((d) => (d.count));
+      const count = counts.reduce((c, n) => (c + n), 0);
+      const names = mmr.map((d) => (d.bin_name));
+      const pcts = mmr.map((d) => (d.cumulative_sum / count * 100));
+      const options = {
+        bindto: '#mmr',
+        size: { height: 500 },
+        data: {
+          x: 'MMR',
+          columns: [
+            ['MMR'].concat(names),
+            ['Players'].concat(counts),
+            ['Percentile'].concat(pcts),
+          ],
+          type: 'bar',
+          types: {
+            Percentile: 'spline',
           },
-          bar: {
-              width: {
-                  ratio: 0.8
-              }
+          axes: {
+            Players: 'y',
+            Percentile: 'y2',
           },
-          axis: {
-              x: {
-                  label: "MMR"
-              },
-              y: {
-                  label: "# Players"
-              },
-              y2: {
-                  show:true,
-                  label: "Percentile"
-              }
+          groups: [
+            ['Players', 'Percentile'],
+          ],
+        },
+        bar: {
+          width: {
+            ratio: 0.8,
           },
-          tooltip: {
-              format: {
-                  value: function(value, ratio, id, ind) {
-                      if (id==="Percentile"){
-                          return value.toFixed(2);
-                      }
-                      return value;
-                  }
+        },
+        axis: {
+          x: {
+            label: 'MMR',
+          },
+          y: {
+            label: '# Players',
+          },
+          y2: {
+            show: true,
+            label: 'Percentile',
+          },
+        },
+        tooltip: {
+          format: {
+            value: function tooltip(value, ratio, id) {
+              // also has ind param
+              if (id === 'Percentile') {
+                return value.toFixed(2);
               }
-          }
+              return value;
+            },
+          },
+        },
       };
       c3.generate(options);
     }
   }
-  componentDidMount() {
-    fetch(`${API_HOST}/api/distributions`).then(jsonResponse).then(this.gotData);
-  }
   render() {
     return (
-     <Tabs>
-       {Object.keys(this.state.data).map((key)=>{
-          return (
-            <Tab label={key}>
-              {(key === 'mmr') ? <div id="mmr" /> : 
+      <Tabs>
+        {Object.keys(this.state.data).map((key) => (
+          <Tab label={key}>
+            {(key === 'mmr') ?
+              <div id="mmr" /> :
               <pre style={{ whiteSpace: 'pre-wrap' }}>
                 {JSON.stringify(this.state.data[key], null, 2)}
               </pre>}
-            </Tab>);
-       })}
+          </Tab>))
+        }
       </Tabs>
     );
   }
--- components/Request/Request.jsx
+++ components/Request/Request.jsx
@@ -14,26 +14,46 @@ class Request extends React.Component {
     super();
     this.state = {
       replay_blob: null,
-      match_id: 0,
+      match_id: window.location.hash.slice(1) || 0,
       progress: 0,
-      error : '',
+      error: '',
       isLoading: false,
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   componentDidMount() {
-    this.setState(Object.assign({}, this.state, {match_id: window.location.hash.slice(1)}));
   }
   handleChange(event) {
     this.setState(Object.assign({}, this.state, {
-      match_id: event.target.value
+      match_id: event.target.value,
     }));
   }
   handleSubmit() {
-    var checker;
-    //const grecaptcha = grecaptcha;
-    this.setState(Object.assign({}, this.state, {isLoading: true}));
+    let checker;
+    // const grecaptcha = grecaptcha;
+
+    function showError(data) {
+      this.setState(Object.assign({}, this.state, { isLoading: false, error: data }));
+      clearInterval(checker);
+      // grecaptcha.reset();
+    }
+
+    function poll(jobId) {
+      fetch(`${API_HOST}/api/request_job?id=${jobId}`).then(jsonResponse).then((json) => {
+        if (json.state === 'completed') {
+          window.history.pushState(`/matches/${(json.data.payload.replay_blob_key || json.data.payload.match_id)}`);
+        } else if (json.error) {
+          showError(json.error);
+        } else if (json.state === 'failed') {
+          showError('Failed to parse replay.  Please make sure the replay is available in client and has not expired.');
+        } else if (json.progress) {
+          this.setState(Object.assign({}, this.state, { progress: json.progress }));
+        }
+      });
+    }
+
+    this.setState(Object.assign({}, this.state, { isLoading: true }));
     fetch(`${API_HOST}/api/request_job`, {
       method: 'post',
       headers:
@@ -41,63 +61,36 @@ class Request extends React.Component {
         Accept: 'application/json',
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({match_id: this.state.match_id, replay_blob: this.state.replay_blob}),
-    }).then(jsonResponse).then(function(json){
+      body: JSON.stringify({
+        match_id: this.state.match_id,
+        replay_blob: this.state.replay_blob,
+      }),
+    }).then(jsonResponse).then((json) => {
       if (json.error) {
-          showError(json.error);
-      }
-      else {
-        checker = setInterval(function() {
-            poll(json.job.jobId);
+        this.showError(json.error);
+      } else {
+        checker = setInterval(() => {
+          poll(json.job.jobId);
         }, 2000);
       }
     });
-
-    function showError(data)
-    {
-        this.setState(Object.assign({}, this.state, {isLoading: false, error: data}));
-        clearInterval(checker);
-        //grecaptcha.reset();
-    }
-
-    function poll(job_id)
-    {
-      fetch(`${API_HOST}/api/request_job?id=${job_id}`).then(jsonResponse).then(function(json){
-        if (json.state === "completed")
-        {
-            window.history.pushState(`/matches/${(json.data.payload.replay_blob_key || json.data.payload.match_id)}`);
-        }
-        else if (json.error)
-        {
-            showError(json.error);
-        }
-        else if (json.state === "failed")
-        {
-            showError("Failed to parse replay.  Please make sure the replay is available in client and has not expired.");
-        }
-        else if (json.progress)
-        {
-            this.setState(Object.assign({}, this.state, {progress: json.progress}));
-        }
-      });
-    }
   }
   render() {
     return (
-    <div>
-      <h1>Request a Parse</h1>
-      <TextField
-        id="match_id"
-        floatingLabelText="Match ID"
-        value={this.state.match_id}
-        onChange={this.handleChange}
-      />
-      <ul>
-         <li>Only works for public matches with replay available in client</li>
-      </ul>
-      <div>{this.state.error}</div>
-      {this.state.isLoading ? <CircularProgress value={this.state.progress} /> : <RaisedButton label="Submit" onClick={this.handleSubmit} />}
-    </div>);
+      <div>
+        <h1>Request a Parse</h1>
+        <TextField
+          id="match_id"
+          floatingLabelText="Match ID"
+          value={this.state.match_id}
+          onChange={this.handleChange}
+        />
+        <ul>
+          <li>Only works for public matches with replay available in client</li>
+        </ul>
+        <div>{this.state.error}</div>
+        {this.state.isLoading ? <CircularProgress value={this.state.progress} /> : <RaisedButton label="Submit" onClick={this.handleSubmit} />}
+      </div>);
   }
 }
 
@@ -107,7 +100,7 @@ export default Request;
   script(src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit",async,defer)
   //script(src="/socket.io/socket.io.js")
   script.
-    requestForm(); 
+    requestForm();
     var onloadCallback = function()
     {
         grecaptcha.render('recaptcha',
