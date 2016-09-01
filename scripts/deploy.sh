b15423ad9d58b77afdd1d90936e360fb7ae9f025
Howard Chung <howardc93@gmail.com>
2016-08-31 20:49:48 -0700

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,10 @@
 git config user.name "Travis CI"
 git config user.email "travis@travis-ci.org"
 
+git remote add upstream "https://$GH_TOKEN@github.com/odota/odota.github.io.git"
+git fetch upstream
+touch .
 git add --all
 git add --force build
 git commit -m "Build $TRAVIS_COMMIT"
-git push -f "https://$GH_TOKEN@github.com/odota/odota.github.io.git" master
+git push -qf upstream HEAD:master
