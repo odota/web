173f8e4c3b7c0122901e209bacf0be2ceba06689
blukai <init1@protonmail.com>
2016-09-16 00:32:00 +0300

    playerHeader

--- components/Player/PlayerHeader.jsx
+++ components/Player/PlayerHeader.jsx
@@ -1,18 +1,104 @@
 import React from 'react';
-import { Card, CardActions } from 'material-ui/Card';
-import PName from './PlayerName';
-import PMMR from './PlayerMMR';
-import PRecord from './PlayerRecord';
+import { connect } from 'react-redux';
+import { CardHeader } from 'material-ui/Card';
+import Avatar from 'material-ui/Avatar';
+import IconButton from 'material-ui/IconButton';
+import ActionPermIdentity from 'material-ui/svg-icons/action/perm-identity';
+import FontIcon from 'material-ui/FontIcon';
+import Cheese from '../Cheese/Cheese.css';
 import styles from './PlayerHeader.css';
+// import PlayerPicture from './PlayerPicture';
+import Error from '../Error';
+import Spinner from '../Spinner';
+import { player } from '../../reducers';
+// import styles from './PlayerHeader.css';
+import PlayerStats from './PlayerStats';
 
-export default ({ playerId }) => (
-  <Card>
-    <div className={styles.container}>
-      <PName playerId={playerId} style={{ width: '40%' }} />
-      <CardActions>
-        <PRecord playerId={playerId} />
-        <PMMR playerId={playerId} />
-      </CardActions>
-    </div>
-  </Card>
-);
+const PlayerName = ({ playerName, playerId, picture, registered, cheese, steamLink, loading, error }) => {
+  const getPlayerName = () => {
+    if (error) return <Error />;
+    if (loading) return <Spinner />;
+
+    const nameSteam = (
+      <span>
+        {playerName}
+        <a className={styles.steamLink} href={steamLink} rel="noopener noreferrer" target="_blank">
+          <img alt="Steam" style={{ marginLeft: '14px' }} src="/assets/steam_icon.png" width="16" height="16" />
+        </a>
+      </span>
+    );
+
+    return (
+      <div style={{ width: '100%' }} className={styles.container}>
+        <div style={{ width: '100%' }}>
+          <CardHeader
+            style={{ padding: 0 }}
+            avatar={
+              <Avatar
+                src={picture}
+                size={124}
+                style={{
+                  WebkitBoxShadow: '0px 0px 15px 2px rgba(0,0,0,0.4)',
+                  MozBoxShadow: '0px 0px 15px 2px rgba(0,0,0,0.4)',
+                  BoxShadow: '0px 0px 15px 2px rgba(0,0,0,0.4)',
+                  margin: '0 30px',
+                }}
+              />
+            }
+            title={
+              registered ?
+                <div>
+                  {nameSteam}
+                  <div style={{ opacity: '.6', float: 'right' }}>
+                    {cheese ?
+                      <div style={{ float: 'right', lineHeight: 1 }}>
+                        <FontIcon
+                          className={Cheese.cheese}
+                          style={{ fontSize: '18px', margin: 0, fontWeight: '600', textShadow: 'none' }}
+                        />
+                        <span style={{ fontSize: '16px', marginLeft: '8px' }}>X {cheese}</span>
+                      </div> :
+                    null}
+                    <IconButton
+                      tooltip="This user is registered!"
+                      touch={true}
+                      disableTouchRipple={true}
+                      tooltipPosition="top-right"
+                      style={{
+                        display: 'block',
+                        float: 'right',
+                        marginTop: '-8px',
+                        marginLeft: '-2px',
+                        cursor: 'default',
+                      }}
+                    >
+                      <ActionPermIdentity />
+                    </IconButton>
+                  </div>
+                </div> :
+              nameSteam
+            }
+            titleStyle={{ fontSize: '28px', marginTop: '8px' }}
+            subtitle={<PlayerStats playerId={playerId} />}
+          />
+        </div>
+      </div>
+    );
+  };
+
+  return getPlayerName();
+};
+
+// metadata.getUserId(state)
+const mapStateToProps = (state, ownProps) => ({
+  loading: player.getLoading(state, ownProps.playerId),
+  error: player.getError(state, ownProps.playerId),
+  playerName: player.getPlayerName(state, ownProps.playerId),
+  registered: player.getLastLogin(state, ownProps.playerId),
+  picture: player.getPictureFull(state, ownProps.playerId),
+  steamLink: player.getSteamLink(state, ownProps.playerId),
+  cheese: player.getCheese(state, ownProps.playerId),
+  playerId: ownProps.playerId,
+});
+
+export default connect(mapStateToProps)(PlayerName);
--- components/Player/PlayerStats.jsx
+++ components/Player/PlayerStats.jsx
@@ -1,10 +1,83 @@
 import React from 'react';
-import PMMR from './PlayerMMR';
-import PRecord from './PlayerRecord';
+import { connect } from 'react-redux';
+import { CardTitle } from 'material-ui/Card';
+import { red400, lightGreen400 } from 'material-ui/styles/colors';
+import Error from '../Error';
+import Spinner from '../Spinner';
+import { player } from '../../reducers';
+import styles from './PlayerHeader.css';
 
-export default () => (
-  <div>
-    <PMMR />
-    <PRecord />
-  </div>
-);
+export const PlayerMMR = ({ loading, error, partyRank, soloRank, mmrEstimate, wins, losses }) => {
+  const getPlayerMMR = () => {
+    if (error) return <Error />;
+    if (loading) return <Spinner />;
+
+    const card = {
+      general: {
+        display: 'inline-block',
+        padding: 0,
+        marginRight: '25px',
+        marginTop: '20px',
+      },
+      title: {
+        fontSize: '24px',
+        color: 'rgba(255, 255, 255, 0.87)',
+        lineHeight: '36px',
+      },
+      subtitle: {
+        fontSize: '14px',
+        color: 'rgba(255, 255, 255, 0.54)',
+        lineHeight: 1,
+        textTransform: 'uppercase',
+      },
+    };
+
+    return (
+      <div style={{ position: 'absolute' }}>
+        <CardTitle
+          style={card.general} subtitleStyle={card.title} titleStyle={card.subtitle}
+          subtitle={<span style={{ color: lightGreen400 }}>{wins}</span>} title="wins"
+        />
+        <CardTitle
+          style={card.general} subtitleStyle={card.title} titleStyle={card.subtitle}
+          subtitle={<span style={{ color: red400 }}>{losses}</span>} title="losses"
+        />
+        <CardTitle
+          style={card.general} subtitleStyle={card.title} titleStyle={card.subtitle}
+          subtitle={`${((wins / (wins + losses)) * 100).toFixed(2)}%`} title="winrate"
+        />
+        {soloRank ? <CardTitle
+          style={card.general} subtitleStyle={card.title} titleStyle={card.subtitle}
+          subtitle={soloRank || 'N/A'} title="Solo MMR"
+        /> : null}
+        {partyRank ? <CardTitle
+          style={card.general} subtitleStyle={card.title} titleStyle={card.subtitle}
+          subtitle={partyRank || 'N/A'} title="Party MMR"
+        /> : null}
+        {mmrEstimate.estimate ? <CardTitle
+          style={card.general} subtitleStyle={card.title} titleStyle={card.subtitle}
+          subtitle={mmrEstimate.estimate || 'N/A'} title="estimate MMR"
+        /> : null}
+      </div>
+    );
+  };
+
+  return (
+    <div className={styles.mmrContainer}>
+      {getPlayerMMR()}
+    </div>
+  );
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  loading: player.getLoading(state, ownProps.playerId),
+  error: player.getError(state, ownProps.playerId),
+  partyRank: player.getCompetitiveRank(state, ownProps.playerId),
+  soloRank: player.getSoloMmrEstimate(state, ownProps.playerId),
+  mmrEstimate: player.getMmrEstimate(state, ownProps.playerId),
+  wins: player.getWins(state, ownProps.playerId),
+  losses: player.getLosses(state, ownProps.playerId),
+});
+
+
+export default connect(mapStateToProps)(PlayerMMR);
