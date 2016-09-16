6ba37bd994720b8251a5038d36d75da2fff1dc41
blukai <init1@protonmail.com>
2016-09-16 17:59:58 +0300

    better font, chesse icon to palette, rm playerId

--- components/Player/PlayerHeader.jsx
+++ components/Player/PlayerHeader.jsx
@@ -5,13 +5,10 @@ import Avatar from 'material-ui/Avatar';
 import IconButton from 'material-ui/IconButton';
 import ActionPermIdentity from 'material-ui/svg-icons/action/perm-identity';
 import FontIcon from 'material-ui/FontIcon';
-import Cheese from '../Cheese/Cheese.css';
 import styles from './PlayerHeader.css';
-// import PlayerPicture from './PlayerPicture';
 import Error from '../Error';
 import Spinner from '../Spinner';
 import { player } from '../../reducers';
-// import styles from './PlayerHeader.css';
 import PlayerStats from './PlayerStats';
 
 const PlayerName = ({ playerName, playerId, picture, registered, cheese, steamLink, loading, error }) => {
@@ -22,8 +19,16 @@ const PlayerName = ({ playerName, playerId, picture, registered, cheese, steamLi
     const nameSteam = (
       <span>
         {playerName}
-        <a className={styles.steamLink} href={steamLink} rel="noopener noreferrer" target="_blank">
-          <img alt="Steam" style={{ marginLeft: '14px' }} src="/assets/steam_icon.png" width="16" height="16" />
+        <a style={{ marginLeft: '15px' }} className={styles.icon} href={steamLink} rel="noopener noreferrer" target="_blank">
+          <IconButton
+            tooltip="Steam profile"
+            touch={true}
+            disableTouchRipple={true}
+            tooltipPosition="top-right"
+            className={styles.oreviewTTIcon}
+          >
+            <img alt="Steam" src="/assets/steam_icon.png" width="16" height="16" />
+          </IconButton>
         </a>
       </span>
     );
@@ -37,23 +42,18 @@ const PlayerName = ({ playerName, playerId, picture, registered, cheese, steamLi
               <Avatar
                 src={picture}
                 size={124}
-                style={{
-                  WebkitBoxShadow: '0px 0px 15px 2px rgba(0,0,0,0.4)',
-                  MozBoxShadow: '0px 0px 15px 2px rgba(0,0,0,0.4)',
-                  BoxShadow: '0px 0px 15px 2px rgba(0,0,0,0.4)',
-                  margin: '0 30px',
-                }}
+                className={styles.oreviewAvatar}
               />
             }
             title={
               registered ?
                 <div>
                   {nameSteam}
-                  <div style={{ opacity: '.6', float: 'right' }}>
+                  <div style={{ float: 'right' }}>
                     {cheese ?
-                      <div style={{ float: 'right', lineHeight: 1 }}>
+                      <div className={styles.icon} style={{ float: 'right', lineHeight: 1.3 }}>
                         <FontIcon
-                          className={Cheese.cheese}
+                          className={styles.cheese}
                           style={{ fontSize: '18px', margin: 0, fontWeight: '600', textShadow: 'none' }}
                         />
                         <span style={{ fontSize: '16px', marginLeft: '8px' }}>X {cheese}</span>
@@ -64,13 +64,7 @@ const PlayerName = ({ playerName, playerId, picture, registered, cheese, steamLi
                       touch={true}
                       disableTouchRipple={true}
                       tooltipPosition="top-right"
-                      style={{
-                        display: 'block',
-                        float: 'right',
-                        marginTop: '-8px',
-                        marginLeft: '-2px',
-                        cursor: 'default',
-                      }}
+                      className={`${styles.oreviewTTIcon} ${styles.icon}`}
                     >
                       <ActionPermIdentity />
                     </IconButton>
@@ -98,7 +92,6 @@ const mapStateToProps = (state, ownProps) => ({
   picture: player.getPictureFull(state, ownProps.playerId),
   steamLink: player.getSteamLink(state, ownProps.playerId),
   cheese: player.getCheese(state, ownProps.playerId),
-  playerId: ownProps.playerId,
 });
 
 export default connect(mapStateToProps)(PlayerName);
