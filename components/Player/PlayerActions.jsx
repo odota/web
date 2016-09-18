e6a456ed04a5d82280feda869fd6f372edc5ba08
blukai <init1@protonmail.com>
2016-09-19 00:13:54 +0300

    player header buttons, transition

--- components/Player/PlayerActions.jsx
+++ components/Player/PlayerActions.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { CardActions } from 'material-ui/Card';
-import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
 import ActionUpdate from 'material-ui/svg-icons/action/update';
 import ReactTooltip from 'react-tooltip';
 
@@ -8,10 +8,12 @@ export default () => (
   <CardActions style={{ width: '30%', boxSizing: 'border-box', padding: '15px 30px', textAlign: 'right' }}>
     <div style={{ display: 'inline-block' }}>
       <div data-tip data-for="update">
-        <RaisedButton
+        <FlatButton
           disabled={true}
           icon={<ActionUpdate />}
           style={{ minWidth: 50 }}
+          backgroundColor="rgba(255, 255, 255, .08)"
+          hoverColor="rgba(255, 255, 255, .12)"
         />
       </div>
       <ReactTooltip id="update" place="left" type="light" effect="float">
@@ -22,12 +24,14 @@ export default () => (
         </div>
       </ReactTooltip>
     </div>
-    <RaisedButton
+    <FlatButton
       disabled={true}
       label="Ask a Coach"
       labelPosition="before"
       icon={<img src="/assets/dotacoach-32x24.png" alt="DotaCoach" />}
       style={{ marginLeft: 15 }}
+      backgroundColor="rgba(255, 255, 255, .08)"
+      hoverColor="rgba(255, 255, 255, .12)"
     />
   </CardActions>
 );
