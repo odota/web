435224f538025488a41161c914a62f2c97a8ddd7
blukai <init1@protonmail.com>
2016-09-23 17:08:46 +0300

    Footer icons fix

--- components/Footer/Links.jsx
+++ components/Footer/Links.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import IconButton from 'material-ui/IconButton';
 import styles from './Footer.css';
 import { IconSteam } from '../Icons';
 
@@ -12,10 +13,17 @@ export default () => (
     <p>
       <small>
         Dota 2 API powered by
-        <a href="//steampowered.com" target="_blank" rel="noopener noreferrer">
-          <IconSteam className={styles.icon} width="14" height="14" />
-        </a>
-        &bull; Cheese icon by <a href="//www.flaticon.com/authors/belc" target="_blank" rel="noopener noreferrer">Belc </a>
+        <IconButton
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.button}
+          tooltipPosition="top-center"
+          tooltip="Steam"
+          href="//steampowered.com"
+        >
+          <IconSteam style={{ width: 14, height: 14 }} />
+        </IconButton>
+        Cheese icon by <a href="//www.flaticon.com/authors/belc" target="_blank" rel="noopener noreferrer">Belc </a>
         on <a href="//www.flaticon.com" target="_blank" rel="noopener noreferrer">Flaticon</a>
       </small>
     </p>
