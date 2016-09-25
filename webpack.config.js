29b0d57e58d601798437081bacad729bf6e51c75
blukai <init1@protonmail.com>
2016-09-26 00:14:24 +0300

    Revert "webpack css fix since flexboxgrid just broke"
    This reverts commit faf6b2b1c3defa375104d42e5a270b94d1798020.

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ const config = {
       exclude: /node_modules/,
     }, {
       test: /\.css$/,
-      loader: 'style-loader!css-loader?modules',
+      loader: 'style-loader!css-loader',
       include: /node_modules/,
     }, {
       test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
