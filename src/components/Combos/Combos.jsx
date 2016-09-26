a6d07e4f7a5e79075f0b8d33625f330701be503d
Albert Cui <aqc2109@columbia.edu>
2016-09-26 02:06:19 +0000

    match id queries working

--- src/components/Combos/Combos.jsx
+++ src/components/Combos/Combos.jsx
@@ -13,6 +13,7 @@ import {
   TableRow,
   TableRowColumn,
 } from 'material-ui/Table';
+import { Row, Col } from 'react-flexbox-grid';
 // import { Tabs, Tab } from 'material-ui/Tabs';
 import DatePicker from 'material-ui/DatePicker';
 import constants from 'dotaconstants';
@@ -33,75 +34,87 @@ class Combos extends React.Component {
       team_size: 2,
       oppo_size: 1,
       loading: false,
-      loading_match_ids: false,
+      loading_matchup: false,
       date_max: today,
       date_min: aWhileAgo,
       result: {},
-      match_ids: {}
+      matchup: {},
+      matchup_ids: {}
     };
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.getMatchIds = this.getMatchIds.bind(this);
+    this.getMatchupIds = this.getMatchupIds.bind(this);
   }
 
   componentDidMount() {}
 
-  getMatchIds() {
+  getMatchupIds(matchup) {
+    this.setState(Object.assign({}, this.state, { loading_matchup: true }));
+    
     const dateToTimestamp = date => date.getTime() / 1000;
     const minTimestamp = dateToTimestamp(this.state.date_min);
     const maxTimestamp = dateToTimestamp(this.state.date_max);
-    this.setState(Object.assign({}, this.state, { loading_match_ids: true }));
-    alert("yay")
-    // fetch(`${API_HOST}/api/explorer`, {
-    //   method: 'post',
-    //   headers: {
-    //     Accept: 'application/json',
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify({
-    //     sql: `
-    //     select
-    //     ${[1, 2, 3, 4, 5].map(i => `${i <= this.state.team_size ?
-    //         `pm${i}.hero_id` : '\'\''} team${i},`).join('')}
-    //     ${[1, 2, 3, 4, 5].map(i => `${i <= this.state.oppo_size ?
-    //         `opm${i}.hero_id` : '\'\''} opp${i},`).join('')}
-    //     count(*),
-    //     sum(case when ((pm1.player_slot < 128) = m.radiant_win)
-    //         then 1 else 0 end)::float / count(*) as win
-    //     from player_matches pm1
-    //     ${[2, 3, 4, 5].map(i => (i <= this.state.team_size ? `
-    //       JOIN player_matches pm${i} 
-    //       ON pm1.match_id = pm${i}.match_id 
-    //       AND (pm1.player_slot < 128) = (pm${i}.player_slot < 128)
-    //       AND pm${i}.hero_id != pm1.hero_id
-    //       AND pm${i}.hero_id > 0
-    //       ${i >= 3 ? `AND pm${i}.hero_id > pm${i - 1}.hero_id` : ''}
-    //       ` : '').join(''))}
-    //     ${[1, 2, 3, 4, 5].map(i => (i <= this.state.oppo_size ? `
-    //       JOIN player_matches opm${i} 
-    //       ON pm1.match_id = opm${i}.match_id 
-    //       AND (pm1.player_slot < 128) != (opm${i}.player_slot < 128)
-    //       ${i >= 2 ? `AND opm${i}.hero_id > opm${i - 1}.hero_id` : ''}
-    //       ` : '').join(''))}
-    //     JOIN matches m
-    //       ON pm1.match_id = m.match_id
-    //       WHERE pm1.hero_id = ${this.state.hero_id}
-    //       AND m.start_time >= ${minTimestamp}
-    //       AND m.start_time <= ${maxTimestamp}
-    //     GROUP BY
-    //     ${[1, 2, 3, 4, 5].map(i => `team${i}`)
-    //     .concat([1, 2, 3, 4, 5].map(i => `opp${i}`))
-    //     .join()}
-    //     HAVING count(*) > 5
-    //     ORDER BY win DESC
-    //     LIMIT 1000
-    //     `,
-    //   }),
-    // }).then(resp => resp.json()).then((json) =>
-    //   this.setState(Object.assign({}, this.state, {
-    //     loading: false,
-    //     match_ids: json,
-    //   }))
-    // );
+
+    const join1 = [2, 3, 4, 5].map(i => (i <= this.state.team_size ? `
+          JOIN player_matches pm${i} 
+          ON pm1.match_id = pm${i}.match_id 
+          AND (pm1.player_slot < 128) = (pm${i}.player_slot < 128)
+          AND pm${i}.hero_id != pm1.hero_id
+          AND pm${i}.hero_id > 0
+          ${i >= 3 ? `AND pm${i}.hero_id > pm${i - 1}.hero_id` : ''}
+          ` : '')).join('');
+    const join2 = [1, 2, 3, 4, 5].map(i => (i <= this.state.oppo_size ? `
+          JOIN player_matches opm${i} 
+          ON pm1.match_id = opm${i}.match_id 
+          AND (pm1.player_slot < 128) != (opm${i}.player_slot < 128)
+          ${i >= 2 ? `AND opm${i}.hero_id > opm${i - 1}.hero_id` : ''}
+          ` : '')).join('');
+
+    const team_heroes = Object.keys(matchup)
+      .map((k) => k.indexOf('team') === 0 ? matchup[k] : null)
+      .filter((k) => k);
+    
+    // const opp_heroes =  Object.keys(matchup)
+    //   .map((k) => k.indexOf('opp') === 0 ? matchup[k] : null)
+    //   .filter((k) => k);
+    
+    const filter = team_heroes.map((k, i) => `pm${i + 1}.hero_id = ${k}`)
+      .concat(
+         Object.keys(matchup)
+        .map((k) => k.indexOf('opp') === 0 ? matchup[k] : null)
+        .filter((k) => k)
+        .map((k, i) => `opm${i + 1}.hero_id = ${k}`)
+      )
+      .join(' AND ');
+    
+    console.log(filter);
+    
+    fetch(`${API_HOST}/api/explorer`, {
+      method: 'post',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        sql: `
+        select pm1.match_id
+        from player_matches pm1
+        ${join1}
+        ${join2}
+        JOIN matches m
+          ON pm1.match_id = m.match_id
+          WHERE ${filter}
+          AND m.start_time >= ${minTimestamp}
+          AND m.start_time <= ${maxTimestamp}
+        LIMIT 1000
+        `,
+      }),
+    }).then(resp => resp.json()).then((json) =>
+      this.setState(Object.assign({}, this.state, {
+        loading_matchup: false,
+        matchup: matchup,
+        matchup_ids: json,
+      }))
+    );
   }
   
   handleSubmit() {
@@ -171,52 +184,97 @@ class Combos extends React.Component {
   render() {
     return (<div>
       <h3>Hero Combos</h3>
-      <SelectField
-        value={this.state.hero_id}
-        floatingLabelFixed
-        onChange={(e, i, value) => this.setState(Object.assign({}, this.state, { hero_id: value }))}
-        floatingLabelText="Hero"
-      >
-        {Object.keys(constants.heroes)
-        .sort((a, b) => constants.heroes[a].localized_name.localeCompare(
-            constants.heroes[b].localized_name))
-        .map(h => (
-          <MenuItem value={constants.heroes[h].id} primaryText={constants.heroes[h].localized_name} />
-        ))}
-      </SelectField> <br />
-      <SelectField
-        value={this.state.team_size}
-        floatingLabelFixed
-        onChange={(e, i, value) => this.setState(Object.assign({}, this.state, { team_size: value }))}
-        floatingLabelText="Team Stack Size"
-      >
-        {[1, 2, 3, 4, 5].map(i => (
-          <MenuItem value={i} primaryText={i.toString()} />
-        ))}
-      </SelectField> <br />
-      <SelectField
-        value={this.state.oppo_size}
-        floatingLabelFixed
-        onChange={(e, i, value) => this.setState(Object.assign({}, this.state, { oppo_size: value }))}
-        floatingLabelText="Opponent Stack Size"
-      >
-        {[0, 1, 2, 3, 4, 5].map(i => (
-          <MenuItem value={i} primaryText={i.toString()} />
-        ))}
-      </SelectField> <br />
-      <DatePicker
-        value={this.state.date_min}
-        floatingLabelFixed
-        onChange={(e, date) => this.setState(Object.assign({}, this.state, { date_min: date }))}
-        floatingLabelText="Date Range: Min"
-      />
-      <DatePicker
-        value={this.state.date_max}
-        floatingLabelFixed
-        onChange={(e, date) => this.setState(Object.assign({}, this.state, { date_max: date }))}
-        floatingLabelText="Date Range: Max"
-      />
-      <RaisedButton label="Submit" primary onClick={this.handleSubmit} />
+      {/* Form */}
+      <Row>
+        <Col md={3}>
+          <SelectField
+            value={this.state.hero_id}
+            floatingLabelFixed
+            onChange={(e, i, value) => this.setState(Object.assign({}, this.state, { hero_id: value }))}
+            floatingLabelText="Hero"
+          >
+            {Object.keys(constants.heroes)
+            .sort((a, b) => constants.heroes[a].localized_name.localeCompare(
+                constants.heroes[b].localized_name))
+            .map(h => (
+              <MenuItem value={constants.heroes[h].id} primaryText={constants.heroes[h].localized_name} />
+            ))}
+          </SelectField> <br />
+          <SelectField
+            value={this.state.team_size}
+            floatingLabelFixed
+            onChange={(e, i, value) => this.setState(Object.assign({}, this.state, { team_size: value }))}
+            floatingLabelText="Team Stack Size"
+          >
+            {[1, 2, 3, 4, 5].map(i => (
+              <MenuItem value={i} primaryText={i.toString()} />
+            ))}
+          </SelectField> <br />
+          <SelectField
+            value={this.state.oppo_size}
+            floatingLabelFixed
+            onChange={(e, i, value) => this.setState(Object.assign({}, this.state, { oppo_size: value }))}
+            floatingLabelText="Opponent Stack Size"
+          >
+            {[0, 1, 2, 3, 4, 5].map(i => (
+              <MenuItem value={i} primaryText={i.toString()} />
+            ))}
+          </SelectField> <br />
+          <DatePicker
+            value={this.state.date_min}
+            floatingLabelFixed
+            onChange={(e, date) => this.setState(Object.assign({}, this.state, { date_min: date }))}
+            floatingLabelText="Date Range: Min"
+          />
+          <DatePicker
+            value={this.state.date_max}
+            floatingLabelFixed
+            onChange={(e, date) => this.setState(Object.assign({}, this.state, { date_max: date }))}
+            floatingLabelText="Date Range: Max"
+          />
+          <RaisedButton label="Submit" primary onClick={this.handleSubmit} />
+        </Col>
+        <Col md={9}>
+          {/* Detailed Row */}
+          {this.state.loading_matchup ?
+            <Spinner />
+            :
+            <div>
+              <h3>Match Up</h3>
+                {this.state.matchup_ids.result ?
+                  (Object.keys(this.state.matchup).map((k) => (
+                    (k.indexOf('team') === 0 || k.indexOf('opp') === 0) &&
+                    constants.heroes[this.state.matchup_ids.result[k]] ?
+                      <TableRowColumn>
+                        <img
+                          src={`${API_HOST}${constants.heroes[this.state.matchup_ids.result[k]].img}`}
+                          style={{ width: '50px' }} title={this.state.matchup_ids.result[k]}
+                          role="presentation"
+                        />
+                        <div>{constants.heroes[this.state.matchup_ids.result[k]].localized_name}</div>
+                      </TableRowColumn> : ""
+                  ))
+                : <p>{"No matchup selected. Click a number in the 'counts' column."}</p>}
+              <Table>
+                 <TableBody displayRowCheckbox={false}>
+                    {
+                      this.state.matchup_ids.result ? 
+                        this.state.matchup_ids.result.rows.map(r =>
+                          (
+                          <TableRow><TableRowColumn>
+                            <a href={`${API_HOST}/matches/${r.match_id}`}>{r.match_id}</a>
+                          </TableRowColumn></TableRow>
+                          )
+                        )
+                      : <div/>
+                    }
+                  </TableBody>
+              </Table>
+            </div>
+          }
+        </Col>
+      </Row>
+      {/* Counts table */}
       <br /> <br /> {!this.state.loading ?
         <Table selectable={false}>
           <TableHeader displaySelectAll={false} adjustForCheckbox={false} >
@@ -247,7 +305,7 @@ class Combos extends React.Component {
                             `${(r[k] * 100).toFixed(2)}%`
                             : 
                           k === 'count' ?
-                            <div onClick={this.getMatchIds}>{r[k]}</div>
+                            <div onClick={() => {this.getMatchupIds(r)}}>{r[k]}</div>
                             :
                             r[k]
                           }
