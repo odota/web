b056cae33fd9b32895a2940941c85c5f5de73351
dartharaknus <dartharaknus@gmail.com>
2016-10-02 01:54:17 +0200

    Pages list cleanup (#217)
    * Pages list cleanup
    
    * Remove navbar

--- src/components/Player/Pages/index.js
+++ src/components/Player/Pages/index.js
@@ -1,3 +1,5 @@
+import strings from 'lang';
+
 export { default as PeersPage } from './Peers/Peers';
 export { default as OverviewPage } from './Overview/Overview';
 export { default as MatchesPage } from './Matches/Matches';
@@ -7,3 +9,34 @@ export { default as RankingsPage } from './Rankings/Rankings';
 export { default as HistogramsPage } from './Histograms/Histograms';
 export { default as RecordsPage } from './Records/Records';
 export { default as CountsPage } from './Counts/Counts';
+
+export const playerPages = [{
+  name: strings.tab_overview,
+}, {
+  name: strings.tab_matches,
+}, {
+  name: strings.tab_heroes,
+}, {
+  name: strings.tab_peers,
+}, {
+  name: strings.tab_pros,
+}, {
+  name: strings.tab_activity,
+}, {
+  name: strings.tab_records,
+}, {
+  name: strings.tab_counts,
+}, {
+  name: strings.tab_histograms,
+}, {
+  name: strings.tab_trends,
+}, {
+  name: strings.tab_wardmap,
+}, {
+  name: strings.tab_wordcloud,
+}, {
+  name: strings.tab_mmr,
+}, {
+  name: strings.tab_rankings,
+  'new-feature': true,
+}];
