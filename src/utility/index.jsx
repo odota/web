2d5f70584086be11a8f1c4832136e9de1998329a
blukai <init1@protonmail.com>
2016-10-02 01:56:15 +0300

    whoops

--- src/utility/index.jsx
+++ src/utility/index.jsx
@@ -107,7 +107,7 @@ export const camelToSnake = str =>
   str.replace(/\.?([A-Z]+)/g, (match, group) => `_${group.toLowerCase()}`).replace(/^_/, '');
 
 const getSubtext = row => {
-  if (row.match_id && (row.player_slot || row.player_slot === 0)) {
+  if (row.match_id && row.player_slot !== undefined) {
     return isRadiant(row.player_slot) ? 'Radiant' : 'Dire';
   }
   if (row.last_played) {
