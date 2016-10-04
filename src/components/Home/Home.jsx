52e9d64f218c6752d6b10204b3f510ea2d90e131
blukai <init1@protonmail.com>
2016-10-04 16:09:10 +0300

    Add buttons

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import FlatButton from 'material-ui/FlatButton';
 import strings from 'lang';
+import { IconSteam } from 'components/Icons';
 import styles from './Home.css';
 
 const Home = () => (
@@ -11,6 +13,59 @@ const Home = () => (
       </div>
       <div className={styles.description}>
         {strings.app_description}
+      </div>
+      <div className={styles.Buttons}>
+        <div>
+          <FlatButton
+            label={
+              <span className={styles.label}>
+                <span>
+                  Login
+                </span>
+                <span>
+                  start parsng my matches
+                </span>
+              </span>
+            }
+            icon={<IconSteam />}
+          />
+        </div>
+        <div>
+          <FlatButton
+            label={
+              <span className={styles.label}>
+                <span>
+                  Parse
+                </span>
+                <span>
+                  a match by ID
+                </span>
+              </span>
+            }
+          />
+        </div>
+        <div>
+          <FlatButton
+            label={
+              <span className={styles.label}>
+                <span>
+                  Get a video
+                </span>
+                <span>
+                  of your match with
+                </span>
+              </span>
+            }
+            labelPosition="before"
+            icon={
+              <img
+                src="/assets/images/jist-white-logo.png"
+                role="presentation"
+                height={24}
+              />
+            }
+          />
+        </div>
       </div>
     </div>
   </div>
