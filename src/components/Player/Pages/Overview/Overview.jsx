12280b4bdf4475310bc483db32fc98bb5a601e2a
blukai <init1@protonmail.com>
2016-10-10 21:04:52 +0300

    📱Overview match/hero tables no more squished together

--- src/components/Player/Pages/Overview/Overview.jsx
+++ src/components/Player/Pages/Overview/Overview.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import {
-  connect,
-} from 'react-redux';
+import { connect } from 'react-redux';
 import strings from 'lang';
 import {
   getPlayerMatches,
@@ -11,16 +9,10 @@ import {
   playerMatches,
   playerHeroes,
 } from 'reducers';
-import Table, {
-  TableContainer,
-} from 'components/Table';
-import {
-  TableFilterForm,
-} from 'components/Form';
+import Table, { TableContainer } from 'components/Table';
+import { TableFilterForm } from 'components/Form';
 import playerMatchesColumns from 'components/Player/Pages/Matches/playerMatchesColumns';
-import {
-  playerHeroesOverviewColumns,
-} from 'components/Player/Pages/Heroes/playerHeroesColumns';
+import { playerHeroesOverviewColumns } from 'components/Player/Pages/Heroes/playerHeroesColumns';
 import styles from './Overview.css';
 
 const getPlayerMatchesAndHeroes = (playerId, options) => (dispatch) => {
@@ -29,23 +21,34 @@ const getPlayerMatchesAndHeroes = (playerId, options) => (dispatch) => {
 };
 
 const MAX_OVERVIEW_ROWS = 20;
+const OVERVIEW_TABLES_BREAK_SIZE = 1224;
 
 const Overview = ({
   playerId,
   matchesData,
   heroesData,
+  width,
 }) => (
   <div>
     <TableFilterForm submitAction={getPlayerMatchesAndHeroes} id={playerId} page="overview" />
     <div className={styles.overviewContainer}>
-      <TableContainer title={strings.heading_matches} style={{ width: '70%' }}>
+      <TableContainer
+        title={strings.heading_matches}
+        style={{
+          width: width >= OVERVIEW_TABLES_BREAK_SIZE ? 'calc(70% - 30px)' : '100%',
+          marginRight: width >= OVERVIEW_TABLES_BREAK_SIZE ? 30 : 0,
+        }}
+      >
         <Table
           columns={playerMatchesColumns}
           data={matchesData}
           maxRows={MAX_OVERVIEW_ROWS}
         />
       </TableContainer>
-      <TableContainer title={strings.heading_heroes} style={{ marginLeft: 30, width: '30%' }}>
+      <TableContainer
+        title={strings.heading_heroes}
+        style={{ width: width >= OVERVIEW_TABLES_BREAK_SIZE ? '30%' : '100%' }}
+      >
         <Table
           columns={playerHeroesOverviewColumns}
           data={heroesData}
@@ -84,6 +87,7 @@ const defaultOptions = {
 const mapStateToProps = (state, { playerId }) => ({
   matchesData: playerMatches.getMatchList(state, playerId),
   heroesData: playerHeroes.getHeroList(state, playerId),
+  width: state.browser.width,
 });
 
 const mapDispatchToProps = dispatch => ({
