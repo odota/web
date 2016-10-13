d4780092aef0bade655fdc41b3972813e168dcc3
blukai <init1@protonmail.com>
2016-10-14 00:20:18 +0300

    Fix indexes

--- src/components/Match/CrossTable.jsx
+++ src/components/Match/CrossTable.jsx
@@ -5,16 +5,9 @@ import {
   TableRowColumn,
   TableBody,
 } from 'material-ui/Table';
-/*
-import {
-  heroes,
-} from 'dotaconstants';
-*/
 import heroes from 'dotaconstants/json/heroes.json';
 import { abbreviateNumber } from 'utility';
-import {
-  heroTd,
-} from './matchColumns';
+import { heroTd } from './matchColumns';
 
 const CrossTable = ({
   match,
@@ -25,14 +18,14 @@ const CrossTable = ({
     <TableBody displayRowCheckbox={false}>
       <TableRow>
         <TableRowColumn>Hero</TableRowColumn>
-        {match.players.slice(0, match.players.length / 2).map(p => (
+        {match.players.slice(0, match.players.length / 2).map((p, i) => (
           <TableRowColumn key={p.hero_id}>
-            {heroTd(p, 'hero_id', p.hero_id, true)}
+            {heroTd(p, 'hero_id', p.hero_id, i, true)}
           </TableRowColumn>)
         )}
       </TableRow>
-      {match.players.slice(match.players.length / 2, match.players.length).map(p => (<TableRow key={p.hero_id}>
-        <TableRowColumn>{heroTd(p, 'hero_id', p.hero_id, true)}</TableRowColumn>
+      {match.players.slice(match.players.length / 2, match.players.length).map((p, i) => (<TableRow key={p.hero_id}>
+        <TableRowColumn>{heroTd(p, 'hero_id', p.hero_id, i, true)}</TableRowColumn>
         {match.players.slice(0, match.players.length / 2).map((p2) => {
           const hero2 = heroes[p2.hero_id] || {};
           const pfield1 = p[field1] || {};
--- src/components/Match/matchColumns.jsx
+++ src/components/Match/matchColumns.jsx
@@ -23,7 +23,7 @@ import NavigationMoreHoriz from 'material-ui/svg-icons/navigation/more-horiz';
 import styles from './Match.css';
 
 // {row.last_login && row.last_login && <span style={{ marginLeft: 3 }}><AppBadge /></span>}
-export const heroTd = (row, col, field, hideName) => (
+export const heroTd = (row, col, field, index, hideName) => (
   <TableHeroImage
     image={`${heroes[row.hero_id] ? API_HOST + heroes[row.hero_id].img : '/assets/images/blank-1x1.gif'}`}
     title={row.account_id ? (row.game_mode === 2 && row.name) || row.personaname : strings.general_anonymous}
