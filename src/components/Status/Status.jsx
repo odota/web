dbda1c6bfc6e7b120394dfbdceec0c9c1b3caca1
Ubuntu <howardc93@gmail.com>
2016-10-20 07:50:37 +0000

    basic status page

---
+++ src/components/Status/Status.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import fetch from 'isomorphic-fetch';
+import { API_HOST } from 'config';
+// import Spinner from '../Spinner';
+
+function jsonResponse(response) {
+  return response.json();
+}
+
+class Status extends React.Component
+{
+  componentWillMount() {
+    this.setState({
+      loading: false,
+      result: {},
+    });
+    fetch(`${API_HOST}/api/status`).then(jsonResponse).then((json)=>this.setState({loading: false, result: json}));
+  }
+  render() {
+    return (<pre>{JSON.stringify(this.state, null, 2)}</pre>);
+  }
+}
+
+export default Status;
