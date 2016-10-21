6bfcca50e591215ef0950cc62be67ab19264a82d
Ubuntu <howardc93@gmail.com>
2016-10-21 07:21:37 +0000

    fix lint

--- src/components/Form/ChipList.jsx
+++ src/components/Form/ChipList.jsx
@@ -35,6 +35,6 @@ const ChipList = ({ chipList, name }) => (
 
 const mapStateToProps = () => ({});
 
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = () => ({});
 
 export default connect(mapStateToProps, mapDispatchToProps)(ChipList);
