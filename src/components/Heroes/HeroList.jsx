2547d781bcf2ad01357502f5b30163ba1e806cc5
Mike Robertson <mjr127+github@case.edu>
2016-10-22 16:15:23 -0400

    remove flexbox grid and add pointer cursor on images

--- src/components/Heroes/HeroList.jsx
+++ src/components/Heroes/HeroList.jsx
@@ -1,23 +1,17 @@
 import React from 'react';
-import { Grid, Row, Col } from 'react-flexbox-grid';
-import { Link } from 'react-router';
 import HeroListItem from './HeroListItem';
 
-import style from './Heroes.css';
+import style from './HeroList.css';
 
 export default ({ heroes }) => (
-  <Grid fluid className={style.HeroListWrapper}>
-    <Row>
-      {heroes.map((hero, i) => (
-        <Col className={style.HeroListCol} key={i} xs={12} sm={4} md={4} lg={2}>
-          <Link to={`/heroes/${hero.id}`}>
-            <HeroListItem
-              name={hero.localized_name}
-              imageUrl={hero.img}
-            />
-          </Link>
-        </Col>
-      ))}
-    </Row>
-  </Grid>
+  <div className={style.container}>
+    {heroes.map((hero, i) => (
+      <HeroListItem
+        key={i}
+        id={hero.id}
+        name={hero.localized_name}
+        imageUrl={hero.img}
+      />
+    ))}
+  </div>
 );
--- src/components/Heroes/HeroListItem.jsx
+++ src/components/Heroes/HeroListItem.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import style from './Heroes.css';
+import style from './HeroListItem.css';
 
 export default ({ name, imageUrl }) => (
-  <div className={style.HeroItem} style={{ backgroundImage: `url('${imageUrl}')` }}>
-    <span>
-      {name}
-    </span>
+  <div className={style.HeroListCol}>
+    <div className={style.HeroItem} style={{ backgroundImage: `url('${imageUrl}')` }}>
+      <span>
+        {name}
+      </span>
+    </div>
   </div>
 );
