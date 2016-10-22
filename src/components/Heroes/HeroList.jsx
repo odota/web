d7e365aceebf359167ab31ba5f329157ecce87da
blukai <init1@protonmail.com>
2016-10-22 22:35:13 +0300

    prettify heroes page

--- src/components/Heroes/HeroList.jsx
+++ src/components/Heroes/HeroList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
+import { Link } from 'react-router';
 import HeroListItem from './HeroListItem';
 
 import style from './Heroes.css';
@@ -9,11 +10,12 @@ export default ({ heroes }) => (
     <Row>
       {heroes.map((hero, i) => (
         <Col className={style.HeroListCol} key={i} xs={12} sm={4} md={4} lg={2}>
-          <HeroListItem
-            id={hero.id}
-            name={hero.localized_name}
-            imageUrl={hero.img}
-          />
+          <Link to={`/heroes/${hero.id}`}>
+            <HeroListItem
+              name={hero.localized_name}
+              imageUrl={hero.img}
+            />
+          </Link>
         </Col>
       ))}
     </Row>
--- src/components/Heroes/HeroListItem.jsx
+++ src/components/Heroes/HeroListItem.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router';
-
 import style from './Heroes.css';
 
-export default ({ id, name, imageUrl }) => (
+export default ({ name, imageUrl }) => (
   <div className={style.HeroItem} style={{ backgroundImage: `url('${imageUrl}')` }}>
-    <h4 className={style.HeroItemName}>
-      <Link className={style.HeroItemActions} to={`/heroes/${id}`}>
-        {name}
-      </Link>
-    </h4>
+    <span>
+      {name}
+    </span>
   </div>
 );
