52d165547909f7f56b5112849797c462eabe1727
Mike Robertson <mjr127+github@case.edu>
2016-10-22 16:22:00 -0400

    add back in the link so that the page will route

--- src/components/Heroes/HeroListItem.jsx
+++ src/components/Heroes/HeroListItem.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router';
 import style from './HeroListItem.css';
 
-export default ({ name, imageUrl }) => (
+export default ({ name, imageUrl, id }) => (
   <div className={style.HeroListCol}>
-    <div className={style.HeroItem} style={{ backgroundImage: `url('${imageUrl}')` }}>
-      <span>
-        {name}
-      </span>
-    </div>
+    <Link to={`/heroes/${id}`}>
+      <div className={style.HeroItem} style={{ backgroundImage: `url('${imageUrl}')` }}>
+        <span>
+          {name}
+        </span>
+      </div>
+    </Link>
   </div>
 );
