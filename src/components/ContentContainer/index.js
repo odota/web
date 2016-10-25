bc6aedaac832214d035c4e26ab93a750443b54c6
Mike Robertson <mjr127+github@case.edu>
2016-10-25 08:38:36 -0400

    refactor

--- src/components/ContentContainer/index.js
+++
@@ -1,2 +0,0 @@
-export { default as ContentContainer } from './ContentContainer';
-export { default } from './withContainer';
--- src/components/Table/TableContainer.jsx
+++ src/components/Table/TableContainer.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import Heading from 'components/Heading';
 import { ContentContainer } from 'components/ContentContainer';
-import styles from './TableContainer.css';
 
 const TableContainer = ({ title, style, className, children, error, loading }) => (
-  <div className={`${styles.container} ${className}`} style={{ ...style }}>
-    <div className={styles.heroesContainer}>
-      <Heading title={title} />
-      <ContentContainer error={error} loading={loading}>
-        {children}
-      </ContentContainer>
-    </div>
+  <div className={className} style={{ ...style }}>
+    {title && <Heading title={title} />}
+    <ContentContainer error={error} loading={loading}>
+      {children}
+    </ContentContainer>
   </div>
 );
 
