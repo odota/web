bfc9917bdfd4a92551fec40bbc35c9ad96164f20
Ubuntu <howardc93@gmail.com>
2016-10-29 20:26:57 +0000

    release

--- webpack.config.js
+++ webpack.config.js
@@ -72,13 +72,8 @@ const config = {
       },
     }),
     new webpack.DefinePlugin({
-      /*
-      'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-      },
-      */
-       // TODO Change this to api.opendota.com when beta is GA
-      API_HOST: JSON.stringify(process.env.API_HOST || 'https://www.opendota.com'),
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      API_HOST: JSON.stringify(process.env.API_HOST || 'https://api.opendota.com'),
     }),
   ],
   devServer: {
