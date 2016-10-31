d009f4a28dd41597b2d2bd0c83666ac617c0f09f
Albert Cui <aqc2109@columbia.edu>
2016-10-31 01:45:42 +0000

    Add stanza

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,33 +10,40 @@ import styles from './Footer.css';
 import { IconSteam } from '../Icons';
 
 export default () => (
-  <footer className={styles.footer}>
+  <div>
     <Row>
-      <Col xs>
-        <AppLogo />
-        <SocialLinks />
-        <div>
-          <small>
-            {strings.app_description}
-            {' - '}
-            {strings.app_powered_by}
-            <IconButton
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.iconButton}
-              href="//steampowered.com"
-              style={{ padding: '0px' }}
-            >
-              <IconSteam style={{ width: 14, height: 14 }} />
-            </IconButton>
-          </small>
-        </div>
-        <hr />
-        <Links />
-      </Col>
-      <Col xs>
-        <Cheese />
+      <Col md>
+        <iframe style={{border: "none", margin:0, width:"100%", height:250}} src="https://www.stanza.co/@dota2?embed=true&banner=true&site=opendota" />
       </Col>
     </Row>
-  </footer>
+    <footer className={styles.footer}>
+      <Row>
+        <Col xs>
+          <AppLogo />
+          <SocialLinks />
+          <div>
+            <small>
+              {strings.app_description}
+              {' - '}
+              {strings.app_powered_by}
+              <IconButton
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.iconButton}
+                href="//steampowered.com"
+                style={{ padding: '0px' }}
+              >
+                <IconSteam style={{ width: 14, height: 14 }} />
+              </IconButton>
+            </small>
+          </div>
+          <hr />
+          <Links />
+        </Col>
+        <Col xs>
+          <Cheese />
+        </Col>
+      </Row>
+    </footer>
+  </div>
 );
