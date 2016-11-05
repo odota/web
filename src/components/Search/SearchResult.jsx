f58c767ab8b3e060ed7b82e98192f9c8648cd475
Ubuntu <howardc93@gmail.com>
2016-11-05 14:23:47 +0000

    Use Table for search results

--- src/components/Search/SearchResult.jsx
+++ src/components/Search/SearchResult.jsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import { List } from 'material-ui/List';
 import strings from 'lang';
-import SearchResultItem from './SearchResultItem';
-import style from './search.css';
+import { transformations } from 'utility';
+import Table from 'components/Table';
+// import { List } from 'material-ui/List';
+// import SearchResultItem from './SearchResultItem';
+// import style from './search.css';
 
-export default ({ players }) => {
-  const playerResult = players.map((player, idx) => (
-    <SearchResultItem
-      key={idx}
-      name={player.personaname}
-      steamId={player.account_id}
-      avatarFullUrl={player.avatarfull}
-    />
-  ));
-
-  return (
-    <div className={style.searchResult}>
-      <p>
-        <em>{players.length} {strings.app_results}</em>
-      </p>
-      <List>{playerResult}</List>
-    </div>
-  );
-};
+const searchColumns = [{
+  displayName: strings.th_name,
+  field: 'personaname',
+  displayFn: (row, col, field) => {
+    // const subtitle = <span>{`${row.solo_competitive_rank || strings.general_unknown} ${strings.th_mmr}`}</span>;
+    return transformations.player({...row
+    }, col, field);
+  },
+}];
+export default ({
+  players,
+}) => (<Table data={players} columns={searchColumns} />);
--- src/components/Search/SearchResultItem.jsx
+++ src/components/Search/SearchResultItem.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router';
 import Divider from 'material-ui/Divider';
 import Avatar from 'material-ui/Avatar';
 import { ListItem } from 'material-ui/List';
+import { TableHeroImage } from 'components/Visualizations';
 
 export default ({ steamId, name, avatarFullUrl }) => (
   <div>
