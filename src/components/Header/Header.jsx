d1880a9b5cb7466ad5a1c90e4aeb426ff18329c8
blukai <init1@protonmail.com>
2016-11-07 20:39:11 +0200

    Added "Report bug" link 📱 responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,6 +10,7 @@ import { Row, Col } from 'react-flexbox-grid';
 // import MenuItem from 'material-ui/MenuItem';
 // import IconMenu from 'material-ui/IconMenu';
 // import AppBar from 'material-ui/AppBar';
+import Bug from 'material-ui/svg-icons/action/bug-report';
 import strings from 'lang';
 import AccountWidget from '../AccountWidget';
 import styles from './Header.css';
@@ -17,7 +18,7 @@ import SearchForm from '../Search/SearchForm';
 import AppLogo from '../App/AppLogo';
 import BurgerMenu from '../BurgerMenu';
 
-const tablet = 800;
+const tablet = 864;
 const mobile = 425;
 
 const navbarPages = [
@@ -36,20 +37,31 @@ const navbarPages = [
     path: 'https://dota2.becomethegamer.com/yasp',
   }];
 
-const LogoGroup = ({ width }) => (
-  <ToolbarGroup className={styles.verticalAlign}>
-    {width < tablet && <BurgerMenu links={navbarPages} top={<AccountWidget />} />}
-    <AppLogo style={{ marginRight: 18 }} size={width < mobile && '14px'} />
-  </ToolbarGroup>
-);
+const LogoGroup = ({ width }) => {
+  if (width <= mobile) {
+    navbarPages.push({
+      name: strings.app_report_bug,
+      external: true,
+      path: 'https://github.com/odota/ui/issues',
+    });
+  }
+
+  return (
+    <ToolbarGroup className={styles.verticalAlign}>
+      {width < tablet && <BurgerMenu links={navbarPages} top={<AccountWidget />} />}
+      <AppLogo style={{ marginRight: 18 }} size={width < mobile && '14px'} />
+    </ToolbarGroup>
+  );
+};
 
 const LinkGroup = () => (
   <ToolbarGroup className={styles.verticalAlign}>
     {navbarPages.map(page => (
       <div key={page.name} className={styles.tabContainer}>
         {page.external ?
-          <a href={page.path} className={styles.tab} rel="noopener noreferrer" target="_blank">{page.name}</a> :
-          <Link to={page.path} className={styles.tab}>{page.name}</Link>}
+          <a href={page.path} className={styles.tab} rel="noopener noreferrer" target="_blank">{page.name}</a>
+          : <Link to={page.path} className={styles.tab}>{page.name}</Link>
+        }
       </div>
     ))}
   </ToolbarGroup>
@@ -63,9 +75,23 @@ const SearchGroup = ({ location }) => (
 );
 
 const AccountGroup = () => (
-  <ToolbarGroup className={styles.verticalAlign} style={{ marginLeft: 'auto' }}>
+  <ToolbarGroup className={styles.verticalAlign}>
     <AccountWidget />
   </ToolbarGroup>
+);
+
+const ReportBug = () => (
+  <a
+    className={styles.bug}
+    href="https://github.com/odota/ui/issues"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <Bug />
+    <span>
+      {strings.app_report_bug}
+    </span>
+  </a>
 );
 
 const Header = ({ location, width }) => (
@@ -74,6 +100,7 @@ const Header = ({ location, width }) => (
       <LogoGroup width={width} />
       {width > tablet && <LinkGroup />}
       <SearchGroup location={location} />
+      {width > mobile && <ReportBug />}
       {width > tablet && <AccountGroup />}
     </Toolbar>
     <Row center="xs">
