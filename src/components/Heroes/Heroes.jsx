c3ca000483628f0887e5ef8682193ae528f15a3c
Daniel Winter <dev@danielwinter.at>
2016-11-07 16:28:33 +0100

    Filtering Heroes enabled

--- src/components/Heroes/Heroes.jsx
+++ src/components/Heroes/Heroes.jsx
@@ -3,29 +3,46 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import heroes from 'dotaconstants/json/heroes.json';
 import { Tabs, Tab } from 'material-ui/Tabs';
+import TextField from 'material-ui/TextField';
 import HeroList from './HeroList';
 import style from './Heroes.css';
 import Ranking from './Ranking';
 import Benchmark from './Benchmark';
 import HeroBadge from './HeroBadge';
 
-const createHeroList = (heroes) => {
-  const result = Object.keys(heroes)
-    .map(key => ({ ...heroes[key], img: API_HOST + heroes[key].img }))
-    .sort((a, b) => (a.localized_name.localeCompare(b.localized_name)));
-  return result;
+const createHeroList = (heroes, filter) => {
+  const filteredHeroes = [];
+
+  for (const key in heroes) {
+    if (typeof filter === 'undefined' || filter.length === 0) {
+      filteredHeroes[key] = {
+        ...heroes[key],
+        img: API_HOST + heroes[key].img
+      };
+    }
+
+    if (heroes[key].localized_name.toLowerCase().indexOf(filter.toLowerCase()) !== -1) {
+      // localized name of hero contains the substring of filter
+      filteredHeroes[key] = {
+        ...heroes[key],
+        img: API_HOST + heroes[key].img
+      };
+    }
+  }
+
+  return filteredHeroes;
 };
 
 const getSingleHero = heroId => ({ ...heroes[heroId], img: API_HOST + heroes[heroId].img });
 
-const HeroesIndex = ({ heroes }) => (
-  <div>
-    <h1 className={style.Header}>Heroes</h1>
-    <HeroList heroes={heroes} />
-  </div>
-);
-
 class Heroes extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filter: '',
+    };
+  }
+
   componentDidMount() {
     // console.log(this.props);
   }
@@ -43,7 +60,27 @@ class Heroes extends Component {
         </Tabs>
       </div>);
     }
-    return <HeroesIndex heroes={createHeroList(heroes)} />;
+
+    return (
+      <div>
+        <h1 className={style.Header}>Heroes</h1>
+        <TextField
+          hintText={'Filter Heroes'}
+          value={this.state.filter}
+          onChange={e => this.setState({ filter: e.target.value })}
+          fullWidth
+          underlineFocusStyle={{
+            borderColor: style.filterBarColor,
+            bottom: '-4px',
+            left: '0',
+            right: '0',
+            width: 'calc(100% + 40px)',
+          }}
+          underlineStyle={{ borderColor: 'transparent' }}
+        />
+        <HeroList heroes={createHeroList(heroes, this.state.filter)} />
+      </div>
+    );
   }
 }
 
