b4db5a363b55b7ec7cb7096cf39c1a3f56730df6
micael.bergeron <micael.bergeron@solutionstlm.com>
2016-11-11 22:32:19 -0500

    remove a lint bypass

--- src/utility/components.jsx
+++ src/utility/components.jsx
@@ -1,12 +1,12 @@
-/*eslint-disable*/
-
 import React from 'react';
 
-export const Fixed = Component => class extends React.Component {
-  shouldComponentUpdate(newProps) {
-    return this.props.style !== newProps.style;
-  }
-  render() {
-    return <Component {...this.props} />;
-  }
+export default {
+  Fixed: Component => class extends React.Component {
+    shouldComponentUpdate(newProps) {
+      return this.props.style !== newProps.style;
+    }
+    render() {
+      return <Component {...this.props} />;
+    }
+  },
 };
