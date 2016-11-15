f7000b52e3ae8d9ba6c6a08ab51d38241bf0188e
Mike Robertson <michael.robertson@kroger.com>
2016-11-15 18:06:27 -0500

    added set up for test frameworks

---
+++ testUtils/initializeJsdom.js
@@ -0,0 +1,29 @@
+require('babel-register')({
+  extensions: ['.js', '.jsx'],
+  ignore: /node_modules\/(?=(?!kroger-))(?=(?!react-kuic)).*/
+});
+require('css-modules-require-hook')({
+  generateScopedName: '[name]_[local]',
+});
+
+const chai = require('chai');
+const ignore = require('ignore-styles');
+
+ignore.default(['.scss', '.sass', '.stylus', '.styl', '.less', '.gif', '.png', '.jpg']);
+const jsdom = require('jsdom').jsdom;
+const exposedProperties = ['window', 'navigator', 'document'];
+
+global.document = jsdom('');
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+  }
+});
+global.navigator = {
+  userAgent: 'node.js',
+};
+
+chai.use(require('sinon-chai'));
+chai.use(require('chai-enzyme')());
