00d0eab5a26dd7d56cc7cc05300f0dbd99681d3a
blukai <init1@protonmail.com>
2016-11-18 13:23:42 +0200

    Fixes #508

--- src/components/Heroes/RankingTable.jsx
+++ src/components/Heroes/RankingTable.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Table from 'components/Table';
 import strings from 'lang';
 import { transformations } from 'utility';
+import SocialPerson from 'material-ui/svg-icons/social/person';
 
 const rankingColumns = [{
   displayName: '#',
@@ -9,7 +10,17 @@ const rankingColumns = [{
 }, {
   displayName: strings.th_name,
   displayFn: (row, col, field) => {
-    const subtitle = <span>{`${row.solo_competitive_rank || strings.general_unknown} ${strings.th_mmr}`}</span>;
+    const subtitle = (
+      <span>
+        <section
+          data-hint={strings.th_solo_mmr}
+          data-hint-position="bottom"
+        >
+          <SocialPerson />
+        </section>
+        {row.solo_competitive_rank || strings.general_unknown}
+      </span>
+    );
     return transformations.player({ ...row, subtitle }, col, field);
   },
 }, {
