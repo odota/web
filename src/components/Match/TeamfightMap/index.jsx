eb59ccd5421a8617b0555172115934392de96195
Mike Robertson <mjr127+github@case.edu>
2016-11-19 16:37:28 -0500

    make the teamfights 100% responsive. Fixed a couple bugs with how the icons were placed

--- src/components/Match/TeamfightMap/index.jsx
+++ src/components/Match/TeamfightMap/index.jsx
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
 import { formatSeconds } from 'utility';
 import ReactTooltip from 'react-tooltip';
+import Measure from 'react-measure';
 import { IconRadiant, IconDire } from 'components/Icons';
 import Table from 'components/Table';
 import { teamfightColumns } from 'components/Match/matchColumns';
 import styles from './index.css';
 
 const MAP_WIDTH = 400;
-const iconSize = MAP_WIDTH / 12;
+const iconSize = mapWidth => mapWidth / 12;
 
 const style = (width, iconSize, location) => ({
+  width: iconSize,
+  height: iconSize,
   top: ((width / 127) * location.y) - (iconSize / 2),
   left: ((width / 127) * location.x) - (iconSize / 2),
 });
 
 const isRadiant = radiantGoldDelta => radiantGoldDelta > 0;
 
-const TeamfightIcon = ({ isRadiant, position, tooltipKey, onClick }) => {
+const TeamfightIcon = ({ isRadiant, position, tooltipKey, onClick, mapWidth = MAP_WIDTH }) => {
   const IconType = isRadiant ? IconRadiant : IconDire;
 
   return (
     <IconType
       className={styles.teamfightIcon}
-      style={style(MAP_WIDTH, iconSize, position)}
+      style={style(mapWidth, iconSize(mapWidth), position)}
       data-tip
       data-for={tooltipKey}
       onClick={onClick}
@@ -32,7 +35,7 @@ const TeamfightIcon = ({ isRadiant, position, tooltipKey, onClick }) => {
 
 const getIconStyle = radiantGoldDelta => (isRadiant(radiantGoldDelta) ? styles.radiant : styles.dire);
 
-const Teamfight = ({ position, tooltipKey, start, end, onClick, radiantGoldDelta, selected }) => (
+const Teamfight = ({ position, tooltipKey, start, end, onClick, radiantGoldDelta, selected, mapWidth }) => (
   <div>
     <div
       className={`${selected && styles.selected} ${getIconStyle(radiantGoldDelta)}`}
@@ -42,6 +45,7 @@ const Teamfight = ({ position, tooltipKey, start, end, onClick, radiantGoldDelta
         position={position}
         isRadiant={isRadiant(radiantGoldDelta)}
         tooltipKey={tooltipKey}
+        mapWidth={mapWidth}
       />
     </div>
     <ReactTooltip
@@ -79,6 +83,13 @@ const avgPosition = ({ deaths_pos: deathPositions }) => {
   return avgs;
 };
 
+const bindWidth = width => (width >= 400 ? 400 : width);
+
+const setMapSizeStyle = width => ({
+  width: bindWidth(width),
+  height: bindWidth(width),
+});
+
 class TeamfightMap extends Component {
   constructor(props) {
     super();
@@ -102,31 +113,37 @@ class TeamfightMap extends Component {
     const { teamfights = [] } = this.props;
     const { selectedTeamfight } = this.state;
     return (
-      <div className={styles.teamfightContainer}>
-        <div className={styles.map}>
-          {teamfights.map((teamfight, index) => (
-            <Teamfight
-              onClick={this.selectTeamfight}
-              selected={this.isSelected(teamfight)}
-              key={index}
-              position={avgPosition(teamfight)}
-              tooltipKey={`${index}_${teamfight.start}`}
-              start={teamfight.start}
-              end={teamfight.end}
-              radiantGoldDelta={teamfight.radiant_gold_delta}
-            />
-          ))}
-        </div>
-        <div className={styles.tableContainer}>
-          <Table
-            data={
-              teamfights.find(teamfight => teamfight.start === selectedTeamfight)
-              .players.filter(p => p.participate)
-            }
-            columns={teamfightColumns}
-          />
-        </div>
-      </div>
+      <Measure>
+        {({ width }) => (
+          <div className={styles.teamfightContainer}>
+            <div className={styles.map} style={setMapSizeStyle(width)}>
+              {console.log('width, height', width)}
+              {teamfights.map((teamfight, index) => (
+                <Teamfight
+                  onClick={this.selectTeamfight}
+                  selected={this.isSelected(teamfight)}
+                  key={index}
+                  position={avgPosition(teamfight)}
+                  tooltipKey={`${index}_${teamfight.start}`}
+                  start={teamfight.start}
+                  end={teamfight.end}
+                  radiantGoldDelta={teamfight.radiant_gold_delta}
+                  mapWidth={bindWidth(width)}
+                />
+              ))}
+            </div>
+            <div className={styles.tableContainer}>
+              <Table
+                data={
+                  teamfights.find(teamfight => teamfight.start === selectedTeamfight)
+                  .players.filter(p => p.participate)
+                }
+                columns={teamfightColumns}
+              />
+            </div>
+          </div>
+        )}
+      </Measure>
     );
   }
 }
