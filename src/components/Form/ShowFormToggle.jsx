bdecbc8bf59490adb07526477aac35922046a997
Mike Robertson <michael.robertson@kroger.com>
2016-12-04 10:10:13 -0500

    move around filter button into the playerButtons in the playerHeader

--- src/components/Form/ShowFormToggle.jsx
+++ src/components/Form/ShowFormToggle.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import Clear from 'material-ui/svg-icons/content/clear';
-import FloatingActionButton from 'material-ui/FloatingActionButton';
-import styles from './ShowFormToggle.css';
+// import Clear from 'material-ui/svg-icons/content/clear';
+import strings from 'lang';
+import FlatButton from 'material-ui/FlatButton';
 
-const getIcon = (show) => {
-  if (!show) {
-    return (
-      <svg className={styles.svg} viewBox="0 0 24 24">
-        <path d="M3,2H21V2H21V4H20.92L15,9.92V22.91L9,16.91V9.91L3.09,4H3V2M11,16.08L13,18.08V9H13.09L18.09,4H5.92L10.92,9H11V16.08Z" />
-      </svg>
-    );
-  }
-  return <Clear />;
-};
+// const getIcon = (show) => {
+//   if (!show) {
+//     return (
+//       <svg className={styles.svg} viewBox="0 0 24 24">
+//         <path d="M3,2H21V2H21V4H20.92L15,9.92V22.91L9,16.91V9.91L3.09,4H3V2M11,16.08L13,18.08V9H13.09L18.09,4H5.92L10.92,9H11V16.08Z" />
+//       </svg>
+//     );
+//   }
+//   return <Clear />;
+// };
 
-const ShowFormToggle = ({ toggleShowForm, showForm }) => (
-  <FloatingActionButton className={!showForm ? styles.filterButton : styles.closeButton} onClick={() => toggleShowForm()}>
-    {getIcon(showForm)}
-  </FloatingActionButton>
+const ShowFormToggle = ({ toggleShowForm }) => (
+  <FlatButton onClick={() => toggleShowForm()}>
+    {strings.filter_button_text}
+  </FlatButton>
 );
 
 // const mapStateToProps = () => ({});
