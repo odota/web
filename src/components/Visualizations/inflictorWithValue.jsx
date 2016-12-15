25e512b36a1d05e04045aa5eed2750b3921156a3
Victor Oliveira <vamo89@gmail.com>
2016-12-14 23:57:02 -0200

    Creep abilities images - fix Doom casts and damage sources

--- src/components/Visualizations/inflictorWithValue.jsx
+++ src/components/Visualizations/inflictorWithValue.jsx
@@ -5,6 +5,7 @@ import ReactTooltip from 'react-tooltip';
 import uuid from 'uuid';
 import items from 'dotaconstants/json/items.json';
 import abilities from 'dotaconstants/json/abilities.json';
+import neutralAbilities from 'dotaconstants/json/neutral_abilities.json';
 import strings from 'lang';
 import styles from './inflictorWithValue.css';
 
@@ -53,6 +54,7 @@ export default (inflictor, value, type) => {
   if (inflictor !== undefined) {
     // TODO use abilities if we need the full info immediately
     const ability = abilities[inflictor];
+    const neutralAbility = neutralAbilities[inflictor];
     const item = items[inflictor];
     let image;
     let tooltip = strings.tooltip_autoattack_other;
@@ -66,6 +68,9 @@ export default (inflictor, value, type) => {
     } else if (item) {
       image = `${API_HOST}/apps/dota2/images/items/${inflictor}_lg.png`;
       tooltip = tooltipContainer(item);
+    } else if (neutralAbility) {
+      image = neutralAbility.img;
+      tooltip = tooltipContainer(neutralAbility);
     } else {
       image = '/assets/images/default_attack.png';
     }
