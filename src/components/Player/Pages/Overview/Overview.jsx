d842a793637ced3d364a72df3851576bba50e1cb
covertPZ <covertpz@gmail.com>
2017-01-08 17:28:05 -0500

    Update Player Overview page to now display Top Peers. Adjusted max rows for better layout.

--- src/components/Player/Pages/Overview/Overview.jsx
+++ src/components/Player/Pages/Overview/Overview.jsx
@@ -4,19 +4,24 @@ import strings from 'lang';
 import {
   getPlayerMatches,
   getPlayerHeroes,
+  getPlayerPeers,
 } from 'actions';
 import {
   playerMatches,
   playerHeroes,
+  playerPeers,
 } from 'reducers';
 import Table from 'components/Table';
 import Container from 'components/Container';
 import { TableFilterForm } from 'components/Form';
 import playerMatchesColumns from 'components/Player/Pages/Matches/playerMatchesColumns';
 import { playerHeroesOverviewColumns } from 'components/Player/Pages/Heroes/playerHeroesColumns';
+import { playerPeersOverviewColumns } from 'components/Player/Pages/Peers/playerPeersColumns';
 import styles from './Overview.css';
 
-const MAX_OVERVIEW_ROWS = 20;
+const MAX_MATCHES_ROWS = 20;
+const MAX_HEROES_ROWS = 10;
+const MAX_PEERS_ROWS = 5;
 
 const Overview = ({
   matchesData,
@@ -25,6 +30,9 @@ const Overview = ({
   heroesData,
   heroesLoading,
   heroesError,
+  peersData,
+  peersLoading,
+  peersError,
   playerId,
 }) => (
   <div>
@@ -39,32 +47,46 @@ const Overview = ({
         <Table
           columns={playerMatchesColumns}
           data={matchesData}
-          maxRows={MAX_OVERVIEW_ROWS}
+          maxRows={MAX_MATCHES_ROWS}
         />
       </Container>
 
-      <Container
-        title={strings.heading_heroes}
-        className={styles.heroesContainer}
-        loading={heroesLoading}
-        error={heroesError}
-      >
-        <Table
-          columns={playerHeroesOverviewColumns(playerId)}
-          data={heroesData}
-          maxRows={MAX_OVERVIEW_ROWS}
-        />
-      </Container>
+      <div className={styles.heroesContainer}>
+        <Container
+          title={strings.heading_peers}
+          loading={peersLoading}
+          error={peersError}
+        >
+          <Table
+            columns={playerPeersOverviewColumns(playerId)}
+            data={peersData}
+            maxRows={MAX_PEERS_ROWS}
+          />
+        </Container>
+
+        <Container
+          title={strings.heading_heroes}
+          loading={heroesLoading}
+          error={heroesError}
+        >
+          <Table
+            columns={playerHeroesOverviewColumns(playerId)}
+            data={heroesData}
+            maxRows={MAX_HEROES_ROWS}
+          />
+        </Container>
+      </div>
     </div>
   </div>
 );
 
 const getData = (props) => {
   props.getPlayerMatches(props.playerId, { ...props.location.query,
-    limit: MAX_OVERVIEW_ROWS,
+    limit: MAX_MATCHES_ROWS,
     significant: 0,
   });
   props.getPlayerHeroes(props.playerId, props.location.query);
+  props.getPlayerPeers(props.playerId, props.location.query);
 };
 
 class RequestLayer extends React.Component {
@@ -90,11 +112,15 @@ const mapStateToProps = (state, { playerId }) => ({
   heroesData: playerHeroes.getHeroList(state, playerId),
   heroesLoading: playerHeroes.getLoading(state, playerId),
   heroesError: playerHeroes.getError(state, playerId),
+  peersData: playerPeers.getPeerList(state, playerId),
+  peersLoading: playerPeers.getLoading(state, playerId),
+  peersError: playerPeers.getError(state, playerId),
 });
 
 const mapDispatchToProps = dispatch => ({
   getPlayerMatches: (playerId, options) => dispatch(getPlayerMatches(playerId, options)),
   getPlayerHeroes: (playerId, options) => dispatch(getPlayerHeroes(playerId, options)),
+  getPlayerPeers: (playerId, options) => dispatch(getPlayerPeers(playerId, options)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(RequestLayer);
