7a2d74284d7ff95f1fcc9f57f7f563fa36d2dbbb
Howard Chung <howardc93@gmail.com>
2017-02-12 07:38:09 +0000

    update hero page

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -4,27 +4,31 @@ import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
 import heroes from 'dotaconstants/build/heroes.json';
 import strings from 'lang';
-import { Tabs, Tab } from 'material-ui/Tabs';
+import Heading from 'components/Heading';
 import Ranking from './Ranking';
 import Benchmark from './Benchmark';
-import style from './Hero.css';
+import styles from './Hero.css';
 
 const getSingleHero = heroId => ({ ...heroes[heroId], img: API_HOST + heroes[heroId].img });
 
-const Hero = ({ props }) => (<div>
+const Hero = ({ props }) => (<div className={styles.Header}>
   <Helmet title={getSingleHero(props.routeParams.heroId).localized_name} />
-  <div className={style.HeroBadge}>
-    <img role="presentation" src={getSingleHero(props.routeParams.heroId).img} />
-    <h2>{getSingleHero(props.routeParams.heroId).localized_name}</h2>
-  </div>
-  <Tabs>
-    <Tab label={strings.tab_rankings}>
+  <Heading
+    title={getSingleHero(props.routeParams.heroId).localized_name}
+    className={styles.Heading}
+    icon=""
+  />
+  <img role="presentation" src={getSingleHero(props.routeParams.heroId).img} className={styles.image} />
+  <div style={{ display: 'flex' }}>
+    <div style={{ width: '50%', padding: '15px' }}>
+      <Heading title={strings.tab_rankings} />
       <Ranking {...props} />
-    </Tab>
-    <Tab label={strings.tab_benchmarks}>
+    </div>
+    <div style={{ width: '50%', padding: '15px' }}>
+      <Heading title={strings.tab_benchmarks} />
       <Benchmark {...props} />
-    </Tab>
-  </Tabs>
+    </div>
+  </div>
 </div>);
 
 export default connect()(Hero);
