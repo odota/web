f605636ec83b9bc60a87a34615ded7a32b21daf3
Howard Chung <howardc93@gmail.com>
2017-02-15 05:22:19 +0000

    explorer wip

--- src/components/Explorer/Explorer.jsx
+++ src/components/Explorer/Explorer.jsx
@@ -9,41 +9,29 @@ import strings from 'lang';
 import { getScript } from 'utility';
 import Table from 'components/Table';
 import Heading from 'components/Heading';
-// import { Tabs, Tab } from 'material-ui/Tabs';
-/*
-import Popover from 'material-ui/Popover';
-import Menu from 'material-ui/Menu';
+import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
-*/
-// import queries from './queries';
-// import {blue300} from 'material-ui/styles/colors';
+// import Menu from 'material-ui/Menu';
 
 function jsonResponse(response) {
   return response.json();
 }
+// TODO special queries: mega creep wins
 // TODO graphical query builder
-// TODO JSON download/curl?
-// TODO row count
-// TODO query history
-// TODO presaved queries
-// TODO localize strings
-// hero_id,
-// picks/bans,
-// tower damage,
-// hero damage,
-// hero_healing,
-// kills, deaths, assists,
-// gpm, xpm, lh, dn,
-// stat_t[minute], kills->>unit, purchase->>item, casts->>ability, uses->>item
-// where (none, patch, league, hero, player)
-// group by (none, hero, player, league, team, month), aggregation (none, count, avg, sum)
-// order by (stat descending)
-// limit (default 1000)
-// basic player_match data: league.name as leaguename, h.localized_name, np.name, stat
-// basic agg data: count(*), agg(stat)
-/*
-`
-SELECT ${}
+// TODO Remote API calls: league listing, player listing
+const queryTypes = [
+// query type: picks/bans/stat/purchase/casts/uses/damage/received
+];
+const selectFields = [
+  { display: 'Hero', value: 'hero_id' },
+];
+const groupByFields = [
+  { display: 'Player', value: 'account_id' },
+  { display: 'Hero', value: 'hero_id' },
+];
+
+const queryTemplate = ({ select, where, group, agg, order }) => `
+SELECT ${select}, ${agg}(${select}) as ${agg}
 FROM player_matches pm
 LEFT JOIN notable_players np
 ON pm.account_id = np.account_id
@@ -53,11 +41,53 @@ JOIN heroes h
 ON pm.hero_id = h.id
 JOIN leagues le
 ON m.leagueid = le.leagueid
-WHERE ${}
-GROUP BY ${}
-ORDER BY ${}
-LIMIT ${}
-`
+WHERE ${where}
+GROUP BY ${group}
+ORDER BY ${order} DESC
+LIMIT 1000
+`;
+
+// hero_id,
+// picks/bans,
+// tower damage,
+// hero damage,
+// hero_healing,
+// kills, deaths, assists,
+// gpm, xpm, lh, dn, hd, td, hh, level, stuns
+// stat_t[minute],
+// damage_inflictor
+// damage_inflictor_received
+// runes
+// kills->>unit, purchase->>item, casts->>ability, uses->>item
+// where (none, patch, league, hero, player, item purchased)
+// TODO allow multiple where conditions
+// group by (none, hero, player, league, team, month), aggregation (none, count, avg, sum)
+// order by (stat descending)
+// limit (default 1000)
+// basic player_match data: league.name as leaguename, h.localized_name, np.name, stat
+// basic agg data: count(*), agg(stat)
+/*
+Which pro player with at least 10 games has the highest average GPM in 6.85 so far?
+
+What patch has the highest pro winrate for Tiny+Io?
+
+What hero was banned the most at TI4?
+
+What was the winrate of players picking up Hand of Midas in 6.84 pro play? How many people lost with it?
+
+What items does DKPhobos buy on Zeus?
+
+How many times has Night Stalker been played in a dual mid lane over all time in pro play?
+
+What % of the time did pro teams down by 4 towers at 25 minutes end up winning in 6.84?
+
+Which pro player has the 5th highest last hits on Venomancer at 40 minutes across all patches?
+
+Who has the earliest courier kill in 6.85, what time did it happen at, and on what hero?
+
+Who had the most kills in a single game in season 6 of the Amateur Dota 2 League (AD2L)?
+
+What is Nahaz (NahazDota)’s record in ticketed games?
 */
 
 
@@ -68,18 +98,20 @@ class Explorer extends React.Component {
       loadingEditor: true,
       querying: false,
       result: {},
+      builder: {},
     };
     this.instantiateEditor = this.instantiateEditor.bind(this);
     this.handleQuery = this.handleQuery.bind(this);
     this.handleResponse = this.handleResponse.bind(this);
-    /*
-    this.handleRequestOpen = this.handleRequestOpen.bind(this);
-    this.handleRequestClose = this.handleRequestClose.bind(this);
-    this.handleExampleChange = this.handleExampleChange.bind(this);
-    */
+    this.getQueryString = this.getQueryString.bind(this);
+    this.handleJson = this.handleJson.bind(this);
+    this.buildQuery = this.buildQuery.bind(this);
   }
   componentDidMount() {
     getScript('https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.5/ace.js', this.instantiateEditor);
+  }
+  getQueryString() {
+    return `?sql=${encodeURIComponent(this.editor.getSelectedText() || this.editor.getValue())}`;
   }
   instantiateEditor() {
     const editor = ace.edit('editor');
@@ -105,9 +137,12 @@ class Explorer extends React.Component {
       return setTimeout(this.handleQuery, 1000);
     }
     this.setState(Object.assign({}, this.state, { querying: true }));
-    const queryString = `?sql=${encodeURIComponent(this.editor.getSelectedText() || this.editor.getValue())}`;
+    const queryString = this.getQueryString();
     window.history.pushState('', '', queryString);
     fetch(`${API_HOST}/api/explorer${queryString}`).then(jsonResponse).then(this.handleResponse);
+  }
+  handleJson() {
+    window.open(`${API_HOST}/api/explorer${this.getQueryString()}`, '_blank');
   }
   handleResponse(json) {
     this.setState(Object.assign({}, this.state, {
@@ -116,71 +151,65 @@ class Explorer extends React.Component {
       result: json,
     }));
   }
-  /*
-  handleRequestOpen() {
-    this.setState({
-      open: true,
-    });
+  buildQuery() {
+
   }
-  handleRequestClose() {
-    this.setState({
-      open: false,
-    });
-  }
-  handleExampleChange(event, value) {
-    this.editor.setValue(queries[value].sql);
-    this.handleRequestClose();
-  }
-  */
   render() {
     return (<div>
       <Helmet title={strings.title_explorer} />
-      <Heading title="Data Explorer" subtitle="Explore data from Dota 2 matches" />
-      <ul>
-        <li><a href="https://github.com/odota/core/blob/master/sql/create_tables.sql">Table Schema</a></li>
-        <li>matches and player_matches tables only contain competitive matches in Professional and Premium tiers</li>
-        <li>Queries are read-only and time out after 30 seconds</li>
-        <li>Select part of the input to send only the selected text</li>
-      </ul>
-      {
-      /*
+      <Heading title={strings.explorer_title} subtitle={strings.explorer_description} />
       <div>
-        <RaisedButton
-          onClick={this.handleRequestOpen}
-          label={'Examples'}
-        />
-        <Popover
-          open={this.state.open}
-          onRequestClose={this.handleRequestClose}
-        >
-          <Menu onChange={this.handleExampleChange}>
-            {Object.keys(queries).filter(k => Boolean(queries[k].name)).map((k) => {
-              const e = queries[k];
-              return <MenuItem value={k} primaryText={e.name} />;
-            })
-            }
-          </Menu>
-        </Popover>
+        <a href="https://github.com/odota/core/blob/master/sql/create_tables.sql">{strings.explorer_schema}</a>
       </div>
-      */
-      }
-      <Heading title="SQL Query" />
-      {this.state.loadingEditor && <Spinner />}
-      <div
-        id={'editor'}
-        style={{
-          width: '100%',
-          height: 200,
-          display: this.state.loadingEditor ? 'none' : 'block',
-        }}
-      />
-      <RaisedButton
-        style={{ margin: '5px' }}
-        label={'Query'}
-        onClick={this.handleQuery}
-        disabled={this.state.loadingEditor}
-      />
-      <Heading title="Results" />
+      <div style={{ display: 'flex' }}>
+        <div style={{ width: '50%' }}>
+          <Heading title={strings.explorer_builder} subtitle={strings.explorer_builder_description} />
+          <SelectField
+            floatingLabelText="Select"
+            value={this.state.builder.queryType}
+          >
+            <MenuItem value={1} primaryText="Never" />
+          </SelectField>
+          <SelectField
+            floatingLabelText="Aggregate"
+            value={this.state.builder.queryType}
+          />
+          <SelectField
+            floatingLabelText="Group By"
+            value={this.state.builder.queryType}
+          />
+          <SelectField
+            floatingLabelText="Where"
+            value={this.state.builder.queryType}
+          />
+        </div>
+        <div style={{ width: '50%' }}>
+          <Heading title={strings.explorer_query} subtitle={strings.explorer_query_description} />
+          {this.state.loadingEditor && <Spinner />}
+          <div
+            id={'editor'}
+            style={{
+              width: '100%',
+              height: 200,
+            }}
+          />
+        </div>
+      </div>
+      <div style={{ textAlign: 'center' }}>
+        <RaisedButton
+          style={{ margin: '5px' }}
+          label={strings.explorer_query_button}
+          onClick={this.handleQuery}
+          disabled={this.state.loadingEditor}
+        />
+        <RaisedButton
+          style={{ margin: '5px' }}
+          label={strings.explorer_json_button}
+          onClick={this.handleJson}
+          disabled={this.state.loadingEditor}
+        />
+      </div>
+      <Heading title={strings.explorer_results} subtitle={`${(this.state.result.rows || []).length} ${strings.explorer_num_rows}`} />
       <pre style={{ color: 'red' }}>{this.state.result.err}</pre>
       {!this.state.querying ?
         <Table
