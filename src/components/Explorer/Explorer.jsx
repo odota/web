f6c7d646291c9ecad4d9387483379ac2d95a88e9
Howard Chung <howardc93@gmail.com>
2017-02-19 01:25:47 +0000

    more work on explorer

--- src/components/Explorer/Explorer.jsx
+++ src/components/Explorer/Explorer.jsx
@@ -11,85 +11,87 @@ import Table from 'components/Table';
 import Heading from 'components/Heading';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
-// import Menu from 'material-ui/Menu';
+import heroes from 'dotaconstants/build/heroes.json';
 
 function jsonResponse(response) {
   return response.json();
 }
-// TODO special queries: mega creep wins
-// TODO graphical query builder
-// TODO Remote API calls: league listing, player listing
-const queryTypes = [
-// query type: picks/bans/stat/purchase/casts/uses/damage/received
-];
-const selectFields = [
-  { display: 'Hero', value: 'hero_id' },
-];
-const groupByFields = [
-  { display: 'Player', value: 'account_id' },
-  { display: 'Hero', value: 'hero_id' },
-];
-
-const queryTemplate = ({ select, where, group, agg, order }) => `
-SELECT ${select}, ${agg}(${select}) as ${agg}
-FROM player_matches pm
-LEFT JOIN notable_players np
-ON pm.account_id = np.account_id
-JOIN matches m
-ON pm.match_id = m.match_id
-JOIN heroes h
-ON pm.hero_id = h.id
-JOIN leagues le
-ON m.leagueid = le.leagueid
-WHERE ${where}
-GROUP BY ${group}
-ORDER BY ${order} DESC
-LIMIT 1000
-`;
-
-// hero_id,
-// picks/bans,
-// tower damage,
-// hero damage,
-// hero_healing,
-// kills, deaths, assists,
-// gpm, xpm, lh, dn, hd, td, hh, level, stuns
-// stat_t[minute],
-// damage_inflictor
-// damage_inflictor_received
-// runes
-// kills->>unit, purchase->>item, casts->>ability, uses->>item
-// where (none, patch, league, hero, player, item purchased)
-// TODO allow multiple where conditions
-// group by (none, hero, player, league, team, month), aggregation (none, count, avg, sum)
-// order by (stat descending)
-// limit (default 1000)
-// basic player_match data: league.name as leaguename, h.localized_name, np.name, stat
-// basic agg data: count(*), agg(stat)
 /*
 Which pro player with at least 10 games has the highest average GPM in 6.85 so far?
-
 What patch has the highest pro winrate for Tiny+Io?
-
 What hero was banned the most at TI4?
-
 What was the winrate of players picking up Hand of Midas in 6.84 pro play? How many people lost with it?
-
 What items does DKPhobos buy on Zeus?
-
 How many times has Night Stalker been played in a dual mid lane over all time in pro play?
-
 What % of the time did pro teams down by 4 towers at 25 minutes end up winning in 6.84?
-
 Which pro player has the 5th highest last hits on Venomancer at 40 minutes across all patches?
-
 Who has the earliest courier kill in 6.85, what time did it happen at, and on what hero?
-
 Who had the most kills in a single game in season 6 of the Amateur Dota 2 League (AD2L)?
-
 What is Nahaz (NahazDota)’s record in ticketed games?
 */
+// TODO special queries: mega creep wins
+// TODO Remote API calls: league listing, player listing
+const player = { display: 'Player', value: 'notable_players.name', alias: 'playername'};
+const hero = { display: strings.th_hero_id, value: 'hero_id' };
+const league = { display: 'League', value: 'leagues.name', alias: 'leaguename' };
+const patch = { display: 'Patch', value: 'patch' };
+const match = { display: 'Match', value: 'match_id' };
+const none = { display: 'None', value: '' };
+// TODO fuzzy match text entry?
+const selectFields = [
+  { display: strings.th_gold_per_min, value: 'gold_per_min' },
+  { display: strings.th_xp_per_min, value: 'xp_per_min' },
+  { display: strings.th_kills, value: 'kills' },
+  { display: strings.th_deaths, value: 'deaths' },
+  { display: strings.th_assists, value: 'assists' },
+  { display: strings.th_last_hits, value: 'last_hits' },
+  // dn
+  // hd
+  // td
+  // hh
+  // level
+  // stuns
+  { display: strings.th_lhten, value: 'lh_t[10]' },
+  // gold@10
+  // TODO purchase->>item
+  // TODO ability_uses->>ability
+  // TODO item uses->item
+  // TODO damage_inflictor
+  // TODO damage_inflictor_received
+  // TODO runes
+  // TODO kills->>unit
+];
+const groupByFields = [
+player, 
+hero,
+league,
+patch,
+];
+const whereFields = [];
+// TODO where (none, patch, league, hero, player, item purchased)
+// TODO allow multiple where conditions
 
+const queryTemplate = ({ select, group, where }) => `
+SELECT ${[
+group.value, 
+group ? `avg(${select.value}), count(${select.value}), sum(${select.value})` : `${select.value} ${select.alias || ''}`, 
+].filter(Boolean).join(',')}
+FROM player_matches
+LEFT JOIN notable_players
+USING(account_id)
+JOIN matches
+USING(match_id)
+JOIN match_patch
+USING (match_id)
+JOIN heroes
+ON player_matches.hero_id = heroes.id
+JOIN leagues
+USING(leagueid)
+${where ? 'WHERE' : ''}
+${group ? `GROUP BY ${group.value}` : ''}
+ORDER BY ${group ? 'avg' : select.value} DESC
+LIMIT 500
+`;
 
 class Explorer extends React.Component {
   constructor() {
@@ -98,7 +100,11 @@ class Explorer extends React.Component {
       loadingEditor: true,
       querying: false,
       result: {},
-      builder: {},
+      builder: {
+        select: '',
+        group: '',
+        where: '',
+      },
     };
     this.instantiateEditor = this.instantiateEditor.bind(this);
     this.handleQuery = this.handleQuery.bind(this);
@@ -152,7 +158,8 @@ class Explorer extends React.Component {
     }));
   }
   buildQuery() {
-
+    console.log(this.state.builder);
+    this.editor.setValue(queryTemplate(this.state.builder));
   }
   render() {
     return (<div>
@@ -166,22 +173,28 @@ class Explorer extends React.Component {
           <Heading title={strings.explorer_builder} subtitle={strings.explorer_builder_description} />
           <SelectField
             floatingLabelText="Select"
-            value={this.state.builder.queryType}
+            onChange={(event, index, value) => {
+              this.setState({...this.state, builder: {...this.state.builder, select: value}}, this.buildQuery); 
+            }}
           >
-            <MenuItem value={1} primaryText="Never" />
+            {selectFields.map(item => <MenuItem value={item} primaryText={item.display} />)}
           </SelectField>
           <SelectField
-            floatingLabelText="Aggregate"
-            value={this.state.builder.queryType}
-          />
-          <SelectField
             floatingLabelText="Group By"
-            value={this.state.builder.queryType}
-          />
+            onChange={(event, index, value) => {
+              this.setState({...this.state, builder: {...this.state.builder, group: value}}, this.buildQuery); 
+            }}
+          >
+             {groupByFields.map(item => <MenuItem value={item} primaryText={item.display} />)}
+          </SelectField>
           <SelectField
             floatingLabelText="Where"
-            value={this.state.builder.queryType}
-          />
+            onChange={(event, index, value) => {
+              this.setState({...this.state, builder: {...this.state.builder, where: value}}, this.buildQuery); 
+            }}
+          >
+            {whereFields.map(item => <MenuItem value={item} primaryText={item.display} />)}
+          </SelectField>
         </div>
         <div style={{ width: '50%' }}>
           <Heading title={strings.explorer_query} subtitle={strings.explorer_query_description} />
@@ -221,8 +234,13 @@ class Explorer extends React.Component {
               if (column.name === 'match_id') {
                 return <Link to={`/matches/${field}`}>{field}</Link>;
               }
+              else if (column.name === 'hero_id') {
+                return <Link to={`/heroes/${field}`}>{heroes[field] ? heroes[field].localized_name : field}</Link>;
+              }
+              // TODO account_id/player name?
               return typeof field === 'string' ? field : JSON.stringify(field);
             },
+            sortFn: true,
           }))}
         />
         : <Spinner />
