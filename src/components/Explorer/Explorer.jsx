4c1081b27ecf6f363443e9ac3059a666c903273a
Howard Chung <howardc93@gmail.com>
2017-02-20 22:48:07 +0000

    fix lint

--- src/components/Explorer/Explorer.jsx
+++ src/components/Explorer/Explorer.jsx
@@ -1,7 +1,7 @@
 /* global window ace API_HOST */
 import React from 'react';
 import {
-  connect
+  connect,
 }
 from 'react-redux';
 import fetch from 'isomorphic-fetch';
@@ -26,11 +26,11 @@ import patchData from 'dotaconstants/build/patch.json';
 import itemData from 'dotaconstants/build/items.json';
 import {
   getProPlayers,
-  getLeagues
+  getLeagues,
 }
 from 'actions';
 import {
-  TablePercent
+  TablePercent,
 }
 from 'components/Visualizations';
 import util from 'util';
@@ -41,6 +41,9 @@ import ExplorerFormField from './ExplorerFormField';
 function jsonResponse(response) {
   return response.json();
 }
+function getItemSuffix(itemKey) {
+  return ['_2', '_3', '_4', '_5'].some(suffix => itemKey.indexOf(suffix) !== -1) ? itemKey[itemKey.length - 1] : '';
+}
 // TODO mega creep wins
 // TODO bans
 // TODO hero combos
@@ -49,28 +52,28 @@ function jsonResponse(response) {
 const player = {
   text: 'Player',
   value: 'notable_players.name',
-  alias: 'playername'
+  alias: 'playername',
 };
 const hero = {
   text: strings.th_hero_id,
-  value: 'player_matches.hero_id'
+  value: 'player_matches.hero_id',
 };
 const league = {
   text: 'League',
   value: 'leagues.name',
-  alias: 'leaguename'
+  alias: 'leaguename',
 };
 const patch = {
   text: 'Patch',
-  value: 'patch'
+  value: 'patch',
 };
 const jsonSelect = {
   value: 'key',
   groupValue: 'value::text::int',
-  groupKey: 'key'
+  groupKey: 'key',
 };
-const timingSelect = (itemKey) => ({
-  text: `Timing - ${itemData[itemKey].dname} ${['_2', '_3', '_4', '_5'].some(suffix => itemKey.indexOf(suffix) !== -1) ? itemKey[itemKey.length - 1] : ''}`,
+const timingSelect = itemKey => ({
+  text: `Timing - ${itemData[itemKey].dname} ${getItemSuffix(itemKey)}`,
   value: 'match_logs.time',
   order: 'ASC',
   join: `JOIN match_logs 
@@ -81,7 +84,7 @@ AND match_logs.valuename = 'item_${itemKey}'`,
 });
 const killSelect = ({
   text,
-  key
+  key,
 }) => ({
   text: `Kill - ${text}`,
   value: 'match_logs.time',
@@ -94,111 +97,111 @@ AND match_logs.targetname LIKE '${key}'`,
 });
 const fields = {
   select: [{
-        text: strings.heading_kills,
-        value: 'kills'
-      }, {
-        text: strings.heading_deaths,
-        value: 'deaths'
-      }, {
-        text: strings.heading_assists,
-        value: 'assists',
-      }, {
-        text: strings.heading_gold_per_min,
-        value: 'gold_per_min',
-      }, {
-        text: strings.heading_xp_per_min,
-        value: 'xp_per_min',
-      }, {
-        text: strings.heading_last_hits,
-        value: 'last_hits',
-      }, {
-        text: strings.heading_denies,
-        value: 'denies',
-      }, {
-        text: strings.heading_hero_damage,
-        value: 'hero_damage',
-      }, {
-        text: strings.heading_tower_damage,
-        value: 'tower_damage',
-      }, {
-        text: strings.heading_hero_healing,
-        value: 'hero_healing',
-      }, {
-        text: strings.heading_level,
-        value: 'level',
-      }, {
-        text: strings.heading_stuns,
-        value: 'stuns',
-      }, {
-        text: strings.heading_camps_stacked,
-        value: 'camps_stacked',
-      }, {
-        text: strings.heading_lhten,
-        value: 'lh_t[10]',
-      }, {
-        text: strings.heading_lhtwenty,
-        value: 'lh_t[20]',
-      }, {
-        text: strings.heading_lhthirty,
-        value: 'lh_t[30]',
-      }, {
-        text: strings.heading_lhforty,
-        value: 'lh_t[40]',
-      }, {
-        text: strings.heading_lhfifty,
-        value: 'lh_t[50]',
-      }, {
-        text: strings.heading_duration,
-        value: 'duration',
-      }, {...jsonSelect,
-        text: strings.heading_item_purchased,
-        alias: 'item_name',
-        join: ', json_each(player_matches.purchase)',
-      }, {...jsonSelect,
-        text: strings.heading_ability_used,
-        alias: 'ability_name',
-        join: ', json_each(player_matches.ability_uses)',
-      }, {...jsonSelect,
-        text: strings.heading_item_used,
-        alias: 'item_name',
-        join: ', json_each(player_matches.item_uses)',
-      }, {...jsonSelect,
-        text: strings.heading_damage_inflictor,
-        join: ', json_each(player_matches.damage_inflictor)',
-      }, {...jsonSelect,
-        text: strings.heading_damage_inflictor_received,
-        join: ', json_each(player_matches.damage_inflictor_received)',
-      }, {...jsonSelect,
-        text: strings.heading_runes,
-        alias: 'rune_id',
-        join: ', json_each(player_matches.runes)',
-      }, {...jsonSelect,
-        text: strings.heading_unit_kills,
-        join: ', json_each(player_matches.killed)',
-      }, {...jsonSelect,
-        text: strings.heading_damage_instances,
-        join: ', json_each(player_matches.hero_hits)',
-      }, killSelect({
-        text: strings.heading_courier,
-        key: 'npc_dota_courier'
-      }),
-      killSelect({
-        text: strings.heading_roshan,
-        key: 'npc_dota_roshan'
-      }),
-      killSelect({
-        text: strings.heading_tower,
-        key: '%tower%'
-      }),
-      killSelect({
-        text: strings.heading_barracks,
-        key: '%rax%'
-      }),
-      killSelect({
-        text: strings.heading_shrine,
-        key: '%healers%'
-      })
-    ]
+    text: strings.heading_kills,
+    value: 'kills',
+  }, {
+    text: strings.heading_deaths,
+    value: 'deaths',
+  }, {
+    text: strings.heading_assists,
+    value: 'assists',
+  }, {
+    text: strings.heading_gold_per_min,
+    value: 'gold_per_min',
+  }, {
+    text: strings.heading_xp_per_min,
+    value: 'xp_per_min',
+  }, {
+    text: strings.heading_last_hits,
+    value: 'last_hits',
+  }, {
+    text: strings.heading_denies,
+    value: 'denies',
+  }, {
+    text: strings.heading_hero_damage,
+    value: 'hero_damage',
+  }, {
+    text: strings.heading_tower_damage,
+    value: 'tower_damage',
+  }, {
+    text: strings.heading_hero_healing,
+    value: 'hero_healing',
+  }, {
+    text: strings.heading_level,
+    value: 'level',
+  }, {
+    text: strings.heading_stuns,
+    value: 'stuns',
+  }, {
+    text: strings.heading_camps_stacked,
+    value: 'camps_stacked',
+  }, {
+    text: strings.heading_lhten,
+    value: 'lh_t[10]',
+  }, {
+    text: strings.heading_lhtwenty,
+    value: 'lh_t[20]',
+  }, {
+    text: strings.heading_lhthirty,
+    value: 'lh_t[30]',
+  }, {
+    text: strings.heading_lhforty,
+    value: 'lh_t[40]',
+  }, {
+    text: strings.heading_lhfifty,
+    value: 'lh_t[50]',
+  }, {
+    text: strings.heading_duration,
+    value: 'duration',
+  }, { ...jsonSelect,
+    text: strings.heading_item_purchased,
+    alias: 'item_name',
+    join: ', json_each(player_matches.purchase)',
+  }, { ...jsonSelect,
+    text: strings.heading_ability_used,
+    alias: 'ability_name',
+    join: ', json_each(player_matches.ability_uses)',
+  }, { ...jsonSelect,
+    text: strings.heading_item_used,
+    alias: 'item_name',
+    join: ', json_each(player_matches.item_uses)',
+  }, { ...jsonSelect,
+    text: strings.heading_damage_inflictor,
+    join: ', json_each(player_matches.damage_inflictor)',
+  }, { ...jsonSelect,
+    text: strings.heading_damage_inflictor_received,
+    join: ', json_each(player_matches.damage_inflictor_received)',
+  }, { ...jsonSelect,
+    text: strings.heading_runes,
+    alias: 'rune_id',
+    join: ', json_each(player_matches.runes)',
+  }, { ...jsonSelect,
+    text: strings.heading_unit_kills,
+    join: ', json_each(player_matches.killed)',
+  }, { ...jsonSelect,
+    text: strings.heading_damage_instances,
+    join: ', json_each(player_matches.hero_hits)',
+  }, killSelect({
+    text: strings.heading_courier,
+    key: 'npc_dota_courier',
+  }),
+    killSelect({
+      text: strings.heading_roshan,
+      key: 'npc_dota_roshan',
+    }),
+    killSelect({
+      text: strings.heading_tower,
+      key: '%tower%',
+    }),
+    killSelect({
+      text: strings.heading_barracks,
+      key: '%rax%',
+    }),
+    killSelect({
+      text: strings.heading_shrine,
+      key: '%healers%',
+    }),
+  ]
     .concat(Object.keys(itemData).filter(itemKey => itemData[itemKey].cost > 2000).map(timingSelect)),
   group: [
     hero,
@@ -250,11 +253,11 @@ class Explorer extends React.Component {
     this.props.dispatchProPlayers();
     this.props.dispatchLeagues();
     getScript('https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.5/ace.js', this.instantiateEditor);
-    // Hydrate the state from url
-    Object.keys(querystring.parse(window.location.search.substring(1)));
   }
   getQueryString() {
-    return `?builder=${encodeURIComponent(JSON.stringify(this.state.builder))}&sql=${encodeURIComponent(this.editor.getSelectedText() || this.editor.getValue())}`;
+    const builder = encodeURIComponent(JSON.stringify(this.state.builder));
+    const sql = encodeURIComponent(this.editor.getSelectedText() || this.editor.getValue());
+    return `?builder=${builder}&sql=${sql}`;
   }
   instantiateEditor() {
     const editor = ace.edit('editor');
@@ -270,44 +273,11 @@ class Explorer extends React.Component {
     if (sql) {
       editor.setValue(decodeURIComponent(sql));
       this.handleQuery();
-    }
-    else {
+    } else {
       editor.setValue('select count(*) from matches;');
     }
     this.setState(Object.assign({}, this.state, {
       loadingEditor: false,
-    }));
-  }
-  componentDidMount() {
-    this.props.dispatchProPlayers();
-    this.props.dispatchLeagues();
-    getScript('https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.5/ace.js', this.instantiateEditor);
-    // Hydrate the state from url
-    Object.keys(querystring.parse(window.location.search.substring(1)));
-  }
-  getQueryString() {
-    return `?builder=${encodeURIComponent(JSON.stringify(this.state.builder))}&sql=${encodeURIComponent(this.editor.getSelectedText() || this.editor.getValue())}`;
-  }
-  instantiateEditor() {
-    const editor = ace.edit('editor');
-    editor.setTheme('ace/theme/monokai');
-    editor.getSession().setMode('ace/mode/sql');
-    editor.setShowPrintMargin(false);
-    editor.setOptions({
-      minLines: 10,
-      maxLines: Infinity,
-    });
-    this.editor = editor;
-    const sql = this.props && this.props.location && this.props.location.query && this.props.location.query.sql;
-    if (sql) {
-      editor.setValue(decodeURIComponent(sql));
-      this.handleQuery();
-    }
-    else {
-      editor.setValue('select count(*) from matches;');
-    }
-    this.setState(Object.assign({}, this.state, {
-      loadingEditor: false
     }));
   }
   handleQuery() {
@@ -315,7 +285,7 @@ class Explorer extends React.Component {
       return setTimeout(this.handleQuery, 1000);
     }
     this.setState(Object.assign({}, this.state, {
-      querying: true
+      querying: true,
     }));
     const queryString = this.getQueryString();
     window.history.pushState('', '', queryString);
@@ -338,14 +308,16 @@ class Explorer extends React.Component {
   render() {
     const proPlayers = this.props.proPlayers.map(player => ({
       text: player.name,
-      value: player.account_id
+      value: player.account_id,
     }));
     const leagues = this.props.leagues.map(league => ({
       text: league.name,
-      value: league.leagueid
+      value: league.leagueid,
     }));
     const proPlayerMapping = {};
-    proPlayers.forEach(player => proPlayerMapping[player.text] = player.value);
+    proPlayers.forEach((player) => {
+      proPlayerMapping[player.text] = player.value;
+    });
     return (<div>
       <Helmet title={strings.title_explorer} />
       <Heading title={strings.explorer_title} subtitle={strings.explorer_description} />
@@ -359,7 +331,12 @@ class Explorer extends React.Component {
         <ExplorerFormField label={strings.explorer_patch} dataSource={fields.patch} builderField="patch" builderContext={this} />
         <ExplorerFormField label={strings.explorer_player} dataSource={proPlayers} builderField="player" builderContext={this} />
         <ExplorerFormField label={strings.explorer_league} dataSource={leagues} builderField="league" builderContext={this} />
-        <ExplorerFormField label={strings.explorer_player_purchased} dataSource={fields.playerPurchased} builderField="playerPurchased" builderContext={this} />
+        <ExplorerFormField
+          label={strings.explorer_player_purchased}
+          dataSource={fields.playerPurchased}
+          builderField="playerPurchased"
+          builderContext={this}
+        />
         <ExplorerFormField label={strings.explorer_duration} dataSource={fields.duration} builderField="duration" builderContext={this} />
       </div>
       <div>
@@ -401,20 +378,20 @@ class Explorer extends React.Component {
                 return transformations.hero_id(row, col, field);
               } else if (column.name === 'winrate') {
                 return (field >= 0 && field <= 1 ? <TablePercent
-                      val={Number((field * 100).toFixed(2))}
-                    /> : null);
+                  val={Number((field * 100).toFixed(2))}
+                /> : null);
               } else if (column.name === 'rune_id') {
-                    return strings[`rune_${field}`];
-                  } else if (column.name === 'item_name') {
-                    return itemData[field] ? itemData[field].dname : field;
-                  } else if (column.name === 'playername') {
+                return strings[`rune_${field}`];
+              } else if (column.name === 'item_name') {
+                return itemData[field] ? itemData[field].dname : field;
+              } else if (column.name === 'playername') {
                 return <Link to={`/players/${proPlayerMapping[field]}`}>{field}</Link>;
               } else if (column.name === 'time') {
                 return formatSeconds(field);
               }
               return typeof field === 'string' ? field : JSON.stringify(field);
             },
-            sortFn: row => isNaN(Number(row[column.name])) ? row[column.name] : Number(row[column.name]),
+            sortFn: row => (isNaN(Number(row[column.name])) ? row[column.name] : Number(row[column.name])),
           }))}
         />
         : <Spinner />
