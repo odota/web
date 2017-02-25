7c3a051cfa3154c87087ada071c00b288e84aac3
dillerm <dillerm@oregonstate.edu>
2017-02-24 16:27:07 -0800

    added lanes and gameover to the story tab, and updated the radiant and dire colors to match the rest of the ui

--- src/components/Match/MatchStory.jsx
+++ src/components/Match/MatchStory.jsx
@@ -12,22 +12,25 @@ import heroes from 'dotaconstants/build/heroes.json';
 import styles from './Match.css';
 
 const heroesArr = jsonFn(heroes);
+const radiantColor = "#66bb6a";
+const direColor = "#ff4c4c"
 
-const formatList = (items) => {
-  if(items.length <= 1){
-    return items;
+const formatList = (items, none_value = []) => {
+  switch(items.length){
+    case 0:
+      return none_value;
+    case 1:
+      return items;
+    case 2:
+      items.splice(1, 0, ` ${strings.story_conjunctive} `);
+      return items;
+    default:
+      for(var i = 1; i < items.length; i += 2){
+        items.splice(i, 0, ", ");
+      }
+      items.splice(items.length - 1, 0, `${strings.story_conjunctive} `);
+      return items;
   }
-  if(items.length == 2){
-    items.splice(1, 0, ` ${strings.story_conjunctive} `);
-    return items;
-  }
-  if(items.length > 2){
-    for(var i = 1; i < items.length; i += 2){
-      items.splice(i, 0, ", ");
-    }
-  }
-  items.splice(items.length - 1, 0, `${strings.story_conjunctive} `);
-  return items;
 }
 
 const GoldSpan = (amount) => (
@@ -43,7 +46,7 @@ const GoldSpan = (amount) => (
 
 // Modified version of PlayerThumb
 const PlayerSpan = ({ hero_id, name, personaname, hideText, isRadiant }) => (
-  <span style={{ color: (isRadiant ? "green" : "red") }} className={styles.container}>
+  <span style={{ color: (isRadiant ? radiantColor : direColor) }} className={styles.container}>
     <img
       className={styles.heroThumb}
       src={heroes[hero_id]
@@ -114,6 +117,26 @@ const renderEvent = (event, match) => {
       else{
         return renderTemplate(strings.story_roshan, vars_dict);
       }
+    case "lanes":
+      var lanes = event.lanes.map(lane => {
+        vars_dict = {
+          radiant_players: formatList(lane.radiant_players.map(player => PlayerSpan({...player})), strings.story_lane_empty),
+          dire_players: formatList(lane.dire_players.map(player => PlayerSpan({...player})), strings.story_lane_empty),
+          lane: ((lane.lane === 1 && strings.lane_1) ||
+                (lane.lane === 2 && strings.lane_2) ||
+                (lane.lane === 3 && strings.lane_3)),
+        }
+        return renderTemplate(event.radiant_win ? strings.story_lane_radiant_win : strings.story_lane_radiant_lose, vars_dict);
+      });
+      return [ strings.story_lane_intro, <ul>{lanes.map(lane => <li>{lane}</li>)}</ul> ];
+    case "gameover":
+      vars_dict = {
+        duration: formatSeconds(match.duration),
+        winning_team: match.radiant_win ? strings.general_radiant : strings.general_dire,
+        radiant_score: <font color={radiantColor}>{match.radiant_score}</font>,
+        dire_score: <font color={direColor}>{match.dire_score}</font>
+      }
+      return renderTemplate(strings.story_gameover, vars_dict);
     default:
       return `unknown type: ${event.type}`;
   }
@@ -187,6 +210,31 @@ const generateStory = (match) => {
         .filter(String),
     })) : [],
   );
+
+  // Lanes (1=Bottom, 2=Middle, 3=Top) (jungle/roaming not considered a lane here)
+  if(match.duration > 10 * 60) {
+    events.push({
+      type: "lanes",
+      time: 10 * 60, // analysis at 10 minutes in
+      lanes: [ 1, 2, 3 ].map(lane => {
+          let radiant_players = match.players.filter(player => player.lane == lane && player.isRadiant) || [];
+          let dire_players = match.players.filter(player => player.lane == lane && !player.isRadiant) || [];
+          return {
+            lane: lane,
+            radiant_players: radiant_players,
+            dire_players: dire_players,
+            radiant_win: (Math.max(radiant_players.map(player => player.lane_efficiency)) || 0) >
+                      (Math.max(dire_players.map(player => player.lane_efficiency)) || 0) // Which carry got more farm in lane
+          };
+        })
+    });
+  }
+
+  // Gameover
+  events.push({
+    type: "gameover",
+    time: match.duration
+  })
 
   // Sort by time
   console.log(events);
