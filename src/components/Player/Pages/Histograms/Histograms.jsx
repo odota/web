fa3a1e63dce6eaa4f35eac9ab9ac386d656a572b
Stephanie Ng <recurza@gmail.com>
2017-03-04 01:18:40 +0800

    Fix eslint errors

--- src/components/Player/Pages/Histograms/Histograms.jsx
+++ src/components/Player/Pages/Histograms/Histograms.jsx
@@ -10,11 +10,30 @@ import Container from 'components/Container';
 import { browserHistory } from 'react-router';
 import strings from 'lang';
 
+const getMedian = (columns, midpoint) => {
+  let sum = 0;
+  const medianCol = columns.find((col) => {
+    sum += col.games;
+    return (sum >= midpoint);
+  });
+  return medianCol && medianCol.x;
+};
+
+const getSubtitleStats = (columns) => {
+  const total = columns.reduce((sum, col) => (sum + col.games), 0);
+  const median = getMedian(columns, total / 2);
+  let str = `${strings.heading_total_matches}: ${total}`;
+  if (median !== undefined) {
+    str += `, ${strings.heading_median}: ${median}`;
+  }
+  return str;
+};
+
 const Histogram = ({ routeParams, columns, playerId, error, loading, histogramName }) => (
   <div style={{ fontSize: 10 }}>
     <Heading title={strings.histograms_name} subtitle={strings.histograms_description} />
     <ButtonGarden
-      onClick={buttonName => {
+      onClick={(buttonName) => {
         this.histogramName = buttonName;
         browserHistory.push(`/players/${playerId}/histograms/${buttonName}${window.location.search}`);
       }}
@@ -23,7 +42,7 @@ const Histogram = ({ routeParams, columns, playerId, error, loading, histogramNa
     />
     <Container style={{ fontSize: 10 }} error={error} loading={loading}>
       <div>
-        <Heading title={strings[`heading_${histogramName}`]} subtitle={loading ? '' : getSubtitleStats(columns)}/>
+        <Heading title={strings[`heading_${histogramName}`]} subtitle={loading ? '' : getSubtitleStats(columns)} />
         <HistogramGraph columns={columns || []} />
       </div>
     </Container>
@@ -33,24 +52,6 @@ const Histogram = ({ routeParams, columns, playerId, error, loading, histogramNa
 const getData = (props) => {
   props.getPlayerHistogram(props.playerId, props.location.query, props.routeParams.subInfo || histogramNames[0]);
 };
-
-const getMedian = (columns, midpoint) => {
-  let sum = 0;
-  let medianCol = columns.find((col) => {
-    sum += col.games;
-    return (sum >= midpoint);
-  });
-  return medianCol && medianCol.x;
-}
-
-const getSubtitleStats = (columns) => {
-  let total = columns.reduce((sum, col) => (sum + col.games), 0);
-  let median = getMedian(columns, total/2);
-  let str = `${strings['heading_total_matches']}: ${total}`;
-  if (median != undefined)
-    str += `, ${strings['heading_median']}: ${median}`
-  return str;
-}
 
 class RequestLayer extends React.Component {
   componentWillMount() {
@@ -71,7 +72,7 @@ class RequestLayer extends React.Component {
 }
 
 const mapStateToProps = (state, { histogramName = histogramNames[0], playerId }) => ({
-  histogramName: histogramName,
+  histogramName,
   histograms: playerHistogram.getPlayerHistogramById(state, playerId),
   columns: playerHistogram.getHistogramList(histogramName)(state, playerId),
   loading: playerHistogram.getLoading(histogramName)(state, playerId),
