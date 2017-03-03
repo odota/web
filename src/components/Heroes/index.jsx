7d86d8fcc79cf5068f7b0e4a69bbdc5727d3b483
Howard Chung <howardc93@gmail.com>
2017-03-03 04:02:38 +0000

    add pagination to heroes page

--- src/components/Heroes/index.jsx
+++ src/components/Heroes/index.jsx
@@ -76,7 +76,7 @@ class RequestLayer extends React.Component {
           className={styles.Heading}
           icon=""
         />
-        <Table data={data} columns={columns} />
+        <Table data={data} columns={columns} paginated pageLength={10} />
       </div>,
       route: '/heroes/pro',
     }, {
@@ -89,7 +89,7 @@ class RequestLayer extends React.Component {
           className={styles.Heading}
           icon=""
         />
-        <Table data={data} columns={columns} />
+        <Table data={data} columns={columns} paginated pageLength={10} />
       </div>,
       route: '/heroes/public',
     }];
