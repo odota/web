9dab35d4542412a94e6cb459bc3126e89d345203
Howard Chung <howardc93@gmail.com>
2017-04-15 04:52:57 +0000

    lint

--- src/components/Player/Header/PlayedWith.jsx
+++ src/components/Player/Header/PlayedWith.jsx
@@ -1,13 +1,13 @@
 /* global API_HOST fetch */
 import React from 'react';
 
-const shouldShow = (props) => props.loggedInId && props.loggedInId !== props.playerId;
+const shouldShow = props => props.loggedInId && props.loggedInId !== props.playerId;
 
 const getData = (props, context) => {
   if (shouldShow(props)) {
     fetch(`${API_HOST}/api/players/${props.loggedInId}/wl?included_account_id=${props.playerId}`)
     .then(resp => resp.json())
-    .then(json => context.setState({...context.state, ...json}));
+    .then(json => context.setState({ ...context.state, ...json }));
   }
 };
 
@@ -24,12 +24,12 @@ class PlayedWith extends React.Component {
       getData(nextProps, this);
     }
   }
-  render () {
-    return (<div style={{ display: shouldShow(this.props) ? 'block' : 'none'}}>
-    <span>MY RECORD IN GAMES WITH THIS PLAYER: </span>
-    <span>{this.state.win}</span>
-    <span>-</span>
-    <span>{this.state.lose}</span>
+  render() {
+    return (<div style={{ display: shouldShow(this.props) ? 'block' : 'none' }}>
+      <span>MY RECORD IN GAMES WITH THIS PLAYER: </span>
+      <span>{this.state.win}</span>
+      <span>-</span>
+      <span>{this.state.lose}</span>
     </div>);
   }
 }
