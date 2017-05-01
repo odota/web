974d6fb0a9ea63511afe3941598d60f57f876fc6
dillerm <dillerm@oregonstate.edu>
2017-05-01 16:39:02 -0700

    fixed most of linting

--- scripts/updatelangvpk.js
+++ scripts/updatelangvpk.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 // For updating the opendota-ui lang files with data from the vpk
 
 
-// links lang_tag to the language file in the vpk
+// links langTag to the language file in the vpk
 // null indicates that dota does not support this language
-const lang_tag_names = {
+const langTagNames = {
   'bg-BG': 'bulgarian',
   'cs-CZ': 'czech',
   'da-DK': 'danish',
@@ -37,90 +37,90 @@ const lang_tag_names = {
   'uk-UA': 'ukrainian',
   'vi-VN': null,
   'zh-CN': 'schinese',
-  'zh-TW': 'tchinese'
-}
+  'zh-TW': 'tchinese',
+};
 
-const lang_dir = "src/lang/";
+// The rest of these are build later on
+const replacements = {
+  rune_0: 'DOTA_Tooltip_rune_doubledamage',
+  rune_1: 'DOTA_Tooltip_rune_haste',
+  rune_2: 'DOTA_Tooltip_rune_illusion',
+  rune_3: 'DOTA_Tooltip_rune_invisibility',
+  rune_4: 'DOTA_Tooltip_rune_regeneration',
+  rune_5: 'DOTA_Tooltip_rune_bounty',
+  rune_6: 'DOTA_Tooltip_rune_arcane',
+};
 
+const langDir = 'src/lang/';
+
+let englishLang = null;
 try {
-  var english_lang = JSON.parse(fs.readFileSync(`${lang_dir}en-US.json`, 'utf8'));
-} catch(ex) {
-  console.log("Couldn't find en-US.json in the specified directory")
+  englishLang = JSON.parse(fs.readFileSync(`${langDir}en-US.json`, 'utf8'));
+} catch (ex) {
+  console.log("Couldn't find en-US.json in the specified directory");
   process.exit(1);
 }
 
 
-// Build Replacements
-var replacements = {
-  rune_0: "DOTA_Tooltip_rune_doubledamage",
-  rune_1: "DOTA_Tooltip_rune_haste",
-  rune_2: "DOTA_Tooltip_rune_illusion",
-  rune_3: "DOTA_Tooltip_rune_invisibility",
-  rune_4: "DOTA_Tooltip_rune_regeneration",
-  rune_5: "DOTA_Tooltip_rune_bounty",
-  rune_6: "DOTA_Tooltip_rune_arcane"
-};
-// game modes
-for(var i = 0; `game_mode_${i}` in english_lang; i++) {
-  replacements[`game_mode_${i}`] = `game_mode_lobby_name_${i}`;
-}
-
-// npc_dota_(unitstrings)
-Object.keys(english_lang).filter(k => k.match(/^npc_dota_/)).forEach(key => {
-  replacements[key] = key.replace('#', '1');
-});
-// regions & call update
-request("https://raw.githubusercontent.com/dotabuff/d2vpkr/master/dota/scripts/regions.json", (err, resp, body) => {
-  if (err || resp.statusCode !== 200) {
-    console.log(`Error ${resp.statusCode} when getting ${lang_name}: ${err}`);
-    process.exit(1);
-  }
-  var regions = JSON.parse(body).regions;
-  for (const key in regions) {
-    replacements[`region_${regions[key].region}`] = regions[key].display_name.replace(/^#/, '');
-  }
-
-  console.log("Updating lang files...");
-
-  Object.keys(lang_tag_names).forEach(tag => updateLang(tag, lang_tag_names[tag]));
-});
-
-
-const updateLang = (lang_tag, lang_name) => {
-  if(!lang_name){
+const updateLang = (langTag, langName) => {
+  if (!langName) {
     return; // Means dota doesn't have this lang file
   }
-  var strings_url = `https://raw.githubusercontent.com/dotabuff/d2vpkr/master/dota/resource/dota_${lang_name}.json`;
-  var lang_filename = `${lang_dir}${lang_tag}.json`;
+  const stringsUrl = `https://raw.githubusercontent.com/dotabuff/d2vpkr/master/dota/resource/dota_${langName}.json`;
+  const langFilename = `${langDir}${langTag}.json`;
 
-  request(strings_url, (err, resp, body) => {
-    console.log(`${lang_tag} <= ${lang_name}`);
+  request(stringsUrl, (err, resp, body) => {
+    console.log(`${langTag} <= ${langName}`);
     if (err || resp.statusCode !== 200) {
-      console.log(`Error ${resp.statusCode} when getting ${lang_name}: ${err}`)
+      console.log(`Error ${resp.statusCode} when getting ${langName}: ${err}`);
       process.exit(1);
     }
-    var strings = JSON.parse(body).lang.Tokens;
+    const strings = JSON.parse(body).lang.Tokens;
 
+    let lang = null;
     try {
-      var lang = JSON.parse(fs.readFileSync(lang_filename, 'utf8'));
-    } catch(ex) {
-      console.log(`${ex.name} when reading ${lang_tag}: ${ex.message}`)
+      lang = JSON.parse(fs.readFileSync(langFilename, 'utf8'));
+    } catch (ex) {
+      console.log(`${ex.name} when reading ${langTag}: ${ex.message}`);
       process.exit(1);
     }
 
-    Object.keys(replacements).forEach(key => {
-      if((!lang[key] || lang[key] === english_lang[key]) && (replacements[key] in strings)) {
+    Object.keys(replacements).forEach((key) => {
+      if ((!lang[key] || lang[key] === englishLang[key]) && (replacements[key] in strings)) {
         lang[key] = strings[replacements[key]];
       }
     });
 
-    var outString = JSON.stringify(lang, null, 2)
+    let outString = JSON.stringify(lang, null, 2);
     // Fix "key": "value" to "key":"value", because thats how it is currently
-    outString = outString.replace(/": "/g, `":"`); 
-    
-    fs.writeFile(lang_filename, outString, 'utf8');
+    outString = outString.replace(/": "/g, '":"');
+
+    fs.writeFile(langFilename, outString, 'utf8');
   });
 };
 
+// Build the rest of replacements here
+// game modes
+for (let i = 0; `game_mode_${i}` in englishLang; i += 1) {
+  replacements[`game_mode_${i}`] = `game_mode_lobby_name_${i}`;
+}
+// npc_dota_(unitstrings)
+Object.keys(englishLang).filter(k => k.match(/^npc_dota_/)).forEach((key) => {
+  replacements[key] = key.replace('#', '1');
+});
+// regions & call update
+request('https://raw.githubusercontent.com/dotabuff/d2vpkr/master/dota/scripts/regions.json', (err, resp, body) => {
+  if (err || resp.statusCode !== 200) {
+    console.log('Error getting regions info from d2vpkr');
+    process.exit(1);
+  }
+  const regions = JSON.parse(body).regions;
+  Object.keys(regions).forEach((key) => {
+    replacements[`region_${regions[key].region}`] = regions[key].display_name.replace(/^#/, '');
+  });
 
+  console.log('Updating lang files...');
+
+  Object.keys(langTagNames).forEach(tag => updateLang(tag, langTagNames[tag]));
+});
 
