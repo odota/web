bed39968f93bd725dc623b3aa1ee4dc3f32e6d2a
Scott Donnelly <scoot.donnelly@gmail.com>
2017-05-14 14:25:20 -0400

    Update MatchStory.jsx

--- src/components/Match/MatchStory.jsx
+++ src/components/Match/MatchStory.jsx
@@ -225,9 +225,10 @@ class PredictionEvent extends StoryEvent {
     }
   }
   format() {
-    return formatTemplate(strings.story_predicted_victory,
-      { players: formatList(this.players.map(PlayerSpan), strings.story_predicted_victory_empty), team: TeamSpan(this.team), }
-    );
+    return formatTemplate(strings.story_predicted_victory, {
+      players: formatList(this.players.map(PlayerSpan), strings.story_predicted_victory_empty),
+      team: TeamSpan(this.team),
+    });
   }
 }
 
