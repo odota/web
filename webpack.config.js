acd4c3bb4ac89ab94620769bb9e915becffce86f
Howard Chung <howardc93@gmail.com>
2017-05-24 03:27:22 +0000

    update webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -85,6 +85,7 @@ const config = {
   devServer: {
     contentBase: __dirname,
     host: '0.0.0.0',
+    disableHostCheck: true,
     port: Number(process.env.PORT) || 8080,
     historyApiFallback: true,
     hot: true,
