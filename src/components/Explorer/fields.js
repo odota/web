b10abd961d649db9af07dc857a237a74d0410377
Howard Chung <howardc93@gmail.com>
2017-06-01 05:20:19 +0000

    fix lint

--- src/components/Explorer/fields.js
+++ src/components/Explorer/fields.js
@@ -402,7 +402,7 @@ ${props.player && props.player.value ? '' : 'AND player_matches.account_id < pla
   })),
   order: [{ text: strings.explorer_asc, value: 'ASC', key: 'asc' }, { text: strings.explorer_desc, value: 'DESC', key: 'desc' }],
   having,
-  laneRole: Object.keys(strings).filter(str => str.indexOf('lane_role_') === 0).map(str => {
+  laneRole: Object.keys(strings).filter(str => str.indexOf('lane_role_') === 0).map((str) => {
     const laneRoleId = Number(str.substring('lane_role_'.length));
     return { text: strings[str], value: laneRoleId, key: String(laneRoleId) };
   }),
