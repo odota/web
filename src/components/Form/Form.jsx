c3a8e622f862912f0ba78d44ca6e076ee21db1bb
Howard Chung <howardc93@gmail.com>
2017-06-05 09:14:29 +0000

    remove empty file

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import styles from './Form.css';
 
 const onSubmit = (event) => {
   event.preventDefault();
 };
 
 export default ({ children, name, className }) => (
-  <form name={name} className={`${styles.form} ${className}`} onSubmit={onSubmit}>
+  <form name={name} className={className} onSubmit={onSubmit}>
     {children}
   </form>
 );
