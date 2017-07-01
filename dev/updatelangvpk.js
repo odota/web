b84432bbe70ddb84402d631534bb624e5b8d4ddf
dillerm <dillerm@oregonstate.edu>
2017-06-30 17:47:24 -0700

    added chatwheel localizations to update script, and ran script

--- dev/updatelangvpk.js
+++ dev/updatelangvpk.js
@@ -3,6 +3,59 @@ const request = require('request');
 const fs = require('fs');
 // For updating the opendota-ui lang files with data from the vpk
 
+const kv2json = (text) => {
+  // To temporarily hide non-functional quotes
+  let quote_token = 'TEMP_QUOTE_TOKEN';
+  text = text.replace(/\\"/g, quote_token);
+
+  let value_replaces = {
+    "\t": "\\t",
+    "\n": "\\n",
+    "\r": "\\r"
+  };
+
+  // Remove Comments
+  let in_value = false;
+  let in_comment = false;
+  let result = "";
+  for (var i = 0; i < text.length; i++) {
+    if (in_comment) {
+      if (text[i] == "\n") {
+        in_comment = false;
+        result += text[i];
+      }
+    }
+    else if(text[i] == '"') {
+      in_value = !in_value;
+      result += text[i];
+    }
+    else if(!in_value && text[i] == "/") {
+      in_comment = true;
+    }
+    else if(in_value && text[i] in value_replaces) {
+      result += value_replaces[text[i]];
+    }
+    else {
+      result += text[i];
+    }
+  }
+  text = result;
+
+  // Regex string replacements from:
+  // http://dev.dota2.com/showthread.php?t=87191
+  text = text.replace(/"([^"]*)"(\s*){/g, '"$1": {');
+  text = text.replace(/"([^"]*)"\s*"([^"]*)"/g, '"$1": "$2",');
+  text = text.replace(/,(\s*[}\]])/g, '$1');
+  text = text.replace(/([}\]])(\s*)("[^"]*":\s*)?([{\[])/g, '$1,$2$3$4');
+  text = text.replace(/}(\s*"[^"]*":)/g, '},$1');
+  text = `{${text}}`;
+
+  // To re-include non-functional quotes
+  text = text.replace(quote_token, '\\"');
+
+  return JSON.parse(text);
+}
+
 const dontReplace = [
   'npc_dota_brewmaster_earth_#',
   'npc_dota_brewmaster_fire_#',
@@ -117,7 +170,7 @@ Object.keys(englishLang).filter(k => k.match(/^npc_dota_/)).forEach((key) => {
 });
 replacements.npc_dota_phoenix_sun = 'DOTA_Tooltip_ability_phoenix_supernova';
 replacements.npc_dota_weaver_swarm = 'DOTA_Tooltip_ability_weaver_the_swarm';
-// regions & call update
+// regions, chat wheel, & call update
 request('https://raw.githubusercontent.com/dotabuff/d2vpkr/master/dota/scripts/regions.json', (err, resp, body) => {
   if (err || resp.statusCode !== 200) {
     console.log('Error getting regions info from d2vpkr');
@@ -128,14 +181,25 @@ request('https://raw.githubusercontent.com/dotabuff/d2vpkr/master/dota/scripts/r
   Object.keys(regions).forEach((key) => {
     replacements[`region_${regions[key].region}`] = regions[key].display_name.replace(/^#/, '');
   });
+  request('https://raw.githubusercontent.com/dotabuff/d2vpkr/master/dota/scripts/chat_wheel.txt', (err, resp, body) => {
+    if (err || resp.statusCode !== 200) {
+      console.log('Error getting chat wheel info from d2vpkr');
+      process.exit(1);
+    }
+    const chat_wheel = kv2json(body).chat_wheel.messages;
 
-  // Remove ones we don't want to replace
-  dontReplace.forEach((key) => {
-    delete replacements[key];
+    Object.keys(chat_wheel).forEach((key) => {
+      if (chat_wheel[key].message[0] == "#") {
+        replacements[`chatwheel_${chat_wheel[key].message_id}`] = chat_wheel[key].message.replace(/^#/, '');
+      }
+    });
+
+    // Remove ones we don't want to replace
+    dontReplace.forEach((key) => {
+      delete replacements[key];
+    });
+    console.log('Updating lang files...');
+    Object.keys(langTagNames).forEach(tag => updateLang(tag, langTagNames[tag]));
   });
-
-  console.log('Updating lang files...');
-
-  Object.keys(langTagNames).forEach(tag => updateLang(tag, langTagNames[tag]));
 });
 
