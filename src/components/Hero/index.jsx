379b1a076ca1494cd5ea75c44252bd84d7dc345d
Howard Chung <howardc93@gmail.com>
2017-07-22 05:02:02 +0000

    split hero page into tabs

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -5,32 +5,55 @@ import Helmet from 'react-helmet';
 import heroes from 'dotaconstants/build/heroes.json';
 import strings from 'lang';
 import Heading from 'components/Heading';
+import TabBar from 'components/TabBar';
 import Ranking from './Ranking';
 import Benchmark from './Benchmark';
 import styles from './Hero.css';
 
 const getSingleHero = heroId => ({ ...heroes[heroId], img: API_HOST + heroes[heroId].img });
 
-const Hero = ({ props }) => (<div>
-  <Helmet title={getSingleHero(props.match.params.heroId).localized_name} />
-  <div className={styles.Header}>
-    <Heading
-      title={getSingleHero(props.match.params.heroId).localized_name}
-      className={styles.Heading}
-      icon=""
-    />
-    <img role="presentation" src={getSingleHero(props.match.params.heroId).img} className={styles.image} />
-  </div>
-  <div style={{ display: 'flex' }}>
-    <div style={{ width: '50%', padding: '15px' }}>
+const tabs = (heroId) => ([
+  { 
+    name: strings.tab_rankings,
+    key: 'rankings',
+    content: (props) => (<div>
       <Heading title={strings.tab_rankings} subtitle={strings.rankings_description} />
       <Ranking {...props} />
-    </div>
-    <div style={{ width: '50%', padding: '15px' }}>
+    </div>),
+    route: `/heroes/${heroId}/rankings`,
+  },
+  {
+    name: strings.tab_benchmarks,
+    key: 'benchmarks',
+    content: (props) => (<div>
       <Heading title={strings.tab_benchmarks} />
       <Benchmark {...props} />
+    </div>),
+    route: `/heroes/${heroId}/benchmarks`,
+  },
+]);
+
+const Hero = ({ props }) => {
+  const route = props.match.params.info || 'rankings';
+  const heroId = props.match.params.heroId;
+  return (<div>
+    <Helmet title={getSingleHero(props.match.params.heroId).localized_name} />
+    <div className={styles.Header}>
+      <Heading
+        title={getSingleHero(props.match.params.heroId).localized_name}
+        className={styles.Heading}
+        icon=""
+      />
+      <img role="presentation" src={getSingleHero(props.match.params.heroId).img} className={styles.image} />
     </div>
-  </div>
-</div>);
+    <div>
+    <TabBar
+      info={route}
+      tabs={tabs(heroId)}
+    />
+    {tabs(heroId).filter(tab => tab.key === route).map(tab => tab.content(props))}
+    </div>
+  </div>);
+};
 
 export default connect()(Hero);
