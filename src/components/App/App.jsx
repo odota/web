71e1e91eec8e161b45b29391a53523799b0526a3
Howard Chung <howardc93@gmail.com>
2017-08-13 19:08:22 +0000

    remove predictions

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -20,7 +20,7 @@ import Matches from 'components/Matches';
 import Teams from 'components/Teams';
 // import Assistant from 'components/Assistant';
 import Records from 'components/Records';
-import Predictions from 'components/Predictions';
+// import Predictions from 'components/Predictions';
 import Header from '../Header';
 import Footer from '../Footer';
 import styles from './App.css';
@@ -84,7 +84,6 @@ class App extends React.Component {
             <Route exact path="/explorer" component={Explorer} />
             <Route exact path="/search" component={Search} />
             <Route exact path="/records/:info?" component={Records} />
-            <Route exact path="/predictions" component={Predictions} />
           </div>
           <Footer location={location} width={width} />
         </div>
