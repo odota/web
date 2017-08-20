234ad08ad73e0f82fc4ec8b9fe94b52f1f327d41
Jay Honnold <jayhonnold@gmail.com>
2017-08-20 10:44:05 -0500

    Container headers can now link - setup to link overview page to coordesponding tabs

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import ActionLabelOutline from 'material-ui/svg-icons/action/label-outline';
 import styles from './Heading.css';
 
-const Heading = ({ title = '', icon = <ActionLabelOutline />, subtitle, className }) => (
+const Heading = ({ title = '', titleTo, icon = <ActionLabelOutline />, subtitle, className }) => (
   <div className={`${styles.tableHeading} ${className}`}>
     {icon}
     <span className={styles.title}>
-      {title.trim()}
+      {titleTo ?
+        <Link to={titleTo}>
+          {title.trim()}
+        </Link>
+        : title.trim()}
     </span>
     <span className={styles.subtitle}>
       {subtitle}
