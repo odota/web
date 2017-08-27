39005fcc6c38833bd55d021fd751695f31b9a9db
Howard Chung <howardc93@gmail.com>
2017-08-27 03:04:11 +0000

    fix lint

--- src/components/Support/index.jsx
+++ src/components/Support/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Support = () => (
   <div>
     <p>The source code of The OpenDota Project is fully open source and maintained by a community of volunteer developers.</p>
-    <p>Albert and Howard, the founders of the project, maintain a "reference deployment" of the code at opendota.com, the costs of which are covered by sponsors.</p>
+    <p>Albert and Howard, the founders of the project, maintain a reference deployment of the code at opendota.com, the costs of which are covered by sponsors.</p>
     <p>If you are interested in sponsoring, please contact albertqcui@gmail.com</p>
   </div>
 );
