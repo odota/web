611f86c4ec85dcfe22a5a76c576485a4c17d5623
Blukai <blukai@users.noreply.github.com>
2017-09-09 01:36:34 +0300

    Parse chat messages and inject emoticon gifs

---
+++ dev/updateEmoticons.js
@@ -0,0 +1,10 @@
+const fs = require('fs-extra');
+
+process.chdir(__dirname);
+
+try {
+  fs.copySync('../node_modules/dota2-emoticons/resources/images/emoticons', '../assets/images/dota2/emoticons');
+  console.log('Success!');
+} catch (err) {
+  console.error(err);
+}
