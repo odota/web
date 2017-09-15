a2261a5c0e79d320cf7f1dd72bd063d2def744c7
CauchyAbel <cauchyabel@gmail.com>
2017-09-14 21:08:42 -0600

    Cauchy|Change hero link on recent match container to forward to corresponding match.

--- src/utility/index.jsx
+++ src/utility/index.jsx
@@ -219,6 +219,15 @@ const getSubtitle = (row) => {
   return null;
 };
 
+const getTitle = (row, col, heroName) => {
+  if (row.match_id && row.player_slot !== undefined) {
+    return  <TableLink to={`/matches/${row.match_id}`}>{heroName}</TableLink>;
+  } else if (row.last_played) {
+    return  <TableLink to={`/heroes/${row[col.field]}`}>{heroName}</TableLink>;
+  }
+  return null;
+};
+
 /**
  * Transformations of table cell data to display values.
  * These functions are intended to be used as the displayFn property in table columns.
@@ -228,13 +237,12 @@ const getSubtitle = (row) => {
 export const transformations = {
   hero_id: (row, col, field, showPvgnaGuide = false) => {
     const heroName = heroes[row[col.field]] ? heroes[row[col.field]].localized_name : strings.general_no_hero;
+    console.log(row)
     return (
       <TableHeroImage
         parsed={row.version}
         image={heroes[row[col.field]] && API_HOST + heroes[row[col.field]].img}
-        title={
-          <TableLink to={`/heroes/${row[col.field]}`}>{heroName}</TableLink>
-        }
+        title={getTitle(row, col, heroName)}
         subtitle={getSubtitle(row)}
         heroName={heroName}
         showPvgnaGuide={showPvgnaGuide}
