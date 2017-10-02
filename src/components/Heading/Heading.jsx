06b8fcc87ac721e783fc4b15b956301914e04d2c
Howard Chung <howardc93@gmail.com>
2017-10-02 00:01:24 +0000

    migrate heading

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -2,22 +2,55 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import ActionLabelOutline from 'material-ui/svg-icons/action/label-outline';
-import styles from './Heading.css';
+import styled from 'styled-components';
+import constants from '../constants';
 
-const Heading = ({ title = '', titleTo, icon = <ActionLabelOutline />, subtitle, className }) => (
-  <div className={`${styles.tableHeading} ${className}`}>
+const StyledDiv = styled.div`
+  margin-top: 15px;
+  margin-bottom: 15px;
+
+  & svg {
+    vertical-align: top;
+    height: 26px !important;
+    width: 26px !important;
+    margin-right: 6px;
+    opacity: 0.8;
+    fill: ${constants.textColorPrimary};
+  }
+
+  & a {
+    color: ${constants.primaryTextColor};
+    text-decoration: none;
+
+    &:hover {
+      color: ${constants.primaryLinkColor};
+    }
+  }
+
+  & .title {
+    font-size: 20px;
+  }
+
+  & .subtitle {
+    margin-left: 5px;
+    font-size: ${constants.fontSizeMedium};
+    color: ${constants.colorMutedLight};
+  }
+`;
+const Heading = ({ title = '', titleTo, icon = <ActionLabelOutline />, subtitle }) => (
+  <StyledDiv>
     {icon}
-    <span className={styles.title}>
+    <span className="title">
       {titleTo ?
         <Link to={titleTo}>
           {title.trim()}
         </Link>
         : title.trim()}
     </span>
-    <span className={styles.subtitle}>
+    <span className="subtitle">
       {subtitle}
     </span>
-  </div>
+  </StyledDiv>
 );
 
 const { string, element, oneOfType, object } = PropTypes;
@@ -30,7 +63,6 @@ Heading.propTypes = {
     element,
   ]),
   subtitle: object,
-  className: string,
 };
 
 export default Heading;
