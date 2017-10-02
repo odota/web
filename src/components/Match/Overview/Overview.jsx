c20653cb9bd9de12068a7e7e0521944012619588
Howard Chung <howardc93@gmail.com>
2017-10-02 05:47:27 +0000

    migrate overview

--- src/components/Match/Overview/Overview.jsx
+++ src/components/Match/Overview/Overview.jsx
@@ -1,12 +1,64 @@
 import React from 'react';
 import strings from 'lang';
+import styled from 'styled-components';
 import TeamTable from '../TeamTable';
 import AbilityBuildTable from '../AbilityBuildTable';
 import { overviewColumns, abilityColumns } from '../matchColumns';
 import BuildingMap from '../BuildingMap';
 import MatchGraph from '../MatchGraph';
-import styles from './Overview.css';
-// import Timeline from './Timeline';
+
+const Styled = styled.div`
+  width: 100%;
+
+  & > div {
+    display: inline-block;
+    vertical-align: top;
+  }
+
+  & .graph {
+    margin-left: 30px;
+    width: calc(100% - 312px - 50px);
+
+    & svg[style="overflow: hidden;"] {
+      margin-top: 14px;
+    }
+  }
+
+  & .map {
+    margin: 0 auto;
+  }
+
+  & .centeredMap {
+    width: 312px !important;
+    display: block;
+  }
+
+  @media only screen and (max-width: 370px) {
+    & .centeredMap {
+      width: 262px !important;
+      display: block;
+    }
+
+    & .map {
+      width: 262px !important;
+    }
+  }
+
+  @media only screen and (max-width: 1023px) {
+    & > div {
+      display: block;
+    }
+
+    & .graph {
+      margin-left: 0;
+      width: 100%;
+    }
+
+    & .map {
+      width: 312px;
+    }
+  }
+`;
 
 export default {
   name: strings.tab_overview,
@@ -34,15 +86,15 @@ export default {
         />
       }
       {
-        <div className={styles.overviewMapGraph}>
-          <div className={`${styles.map} ${!match.version && styles.centeredMap}`}>
+        <Styled>
+          <div className={`map ${!match.version && 'centeredMap'}`}>
             <BuildingMap match={match} />
           </div>
           {match.version &&
-          <div className={styles.graph}>
+          <div className="graph">
             <MatchGraph match={match} type="difference" />
           </div>}
-        </div>
+        </Styled>
       }
     </div>
   ),
