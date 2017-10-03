be5e83b3168929778f956b40d72b23288cfaaf46
Howard Chung <howardc93@gmail.com>
2017-10-03 03:46:07 +0000

    migrate piepercent

--- src/components/PiePercent/PiePercent.jsx
+++ src/components/PiePercent/PiePercent.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './PiePercent.css';
+import styled from 'styled-components';
+
+const StyledSvg = styled.svg`
+  width: 32px;
+  height: 32px;
+  transform: rotate(-90deg);
+  border-radius: 50%;
+`;
+
+const StyledCircle = styled.circle`
+  fill: var(--primaryTextColor);
+  stroke: var(--colorYelor);
+  stroke-width: 32;
+  stroke-dasharray: 38 100;
+`;
 
 const PiePercent = ({ percent, color, negativeColor }) => (
-  <svg viewBox="0 0 32 32" className={styles.pieChart} styles={{ color: negativeColor }}>
-    <circle
+  <StyledSvg viewBox="0 0 32 32" styles={{ color: negativeColor }}>
+    <StyledCircle
       r="16"
       cx="16"
       cy="16"
-      className={styles.pieInner}
       shapeRendering="crispEdges"
       style={{ strokeDasharray: `${percent} 100`, color }}
     />
-  </svg>
+  </StyledSvg>
 );
 
 PiePercent.propTypes = {
