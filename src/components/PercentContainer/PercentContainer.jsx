c1be0b09a9756f36362b178e482ebec0694085c0
Howard Chung <howardc93@gmail.com>
2017-10-03 03:43:46 +0000

    migrate percentcontainer

--- src/components/PercentContainer/PercentContainer.jsx
+++ src/components/PercentContainer/PercentContainer.jsx
@@ -3,21 +3,33 @@ import PropTypes from 'prop-types';
 import LinearProgress from 'material-ui/LinearProgress';
 // import PiePercent from '../../PiePercent';
 import { getPercentWin } from 'utility';
-import styles from './PercentContainer.css';
+import styled from 'styled-components';
+import constants from '../constants';
 // const getPercentWin = (wins, games) => (games ? Math.ceil(1000 * (wins / games)) / 10 : 0);
+
+const StyledPercentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledTextContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 5px;
+`;
 
 // the LinearProgress can be swapped for PiePercent if it turns out people prefer that
 const PercentContainer = ({ wins, games, percent }) => (
-  <div className={styles.percentContainer}>
-    <div className={styles.textContainer}>{percent || getPercentWin(wins, games).toFixed(1)}</div>
+  <StyledPercentContainer>
+    <StyledTextContainer>{percent || getPercentWin(wins, games).toFixed(1)}</StyledTextContainer>
     <LinearProgress
       style={{ height: 5 }}
       // color={styles.lineColor}
       mode="determinate"
       value={percent || getPercentWin(wins, games)}
-      color={styles.yelor}
+      color={constants.colorYelor}
     />
-  </div>
+  </StyledPercentContainer>
 );
 
 PercentContainer.propTypes = {
