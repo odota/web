9085f99180962607db62f17ecd241a6afbd7a90e
Luchanso <luchansodev@gmail.com>
2017-10-08 01:33:55 +0300

    Fix BurgerMenu

--- src/stories/BurgerMenu.jsx
+++ src/stories/BurgerMenu.jsx
@@ -3,22 +3,13 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
-import BurgerMenu from '../components/BurgerMenu';
+import BurgerMenu from '../components/Header/BurgerMenu';
 
-storiesOf('Burger menu', module).add('With items', () => {
+storiesOf('Header', module).add('Burger menu', () => {
   const menuItems = [
-    {
-      component: <div role="button" tabIndex={0} onClick={action('Click on First item')}>First item</div>,
-      close: true,
-    },
-    {
-      component: <div role="button" tabIndex={-1} onClick={action('Click on Second item')}>Second item</div>,
-      close: true,
-    },
-    {
-      component: <div role="button" tabIndex={-1} onClick={action('Click on Third item')}>Third item</div>,
-      close: true,
-    },
+    <div role="button" tabIndex={0} onClick={action('Click on First item')}>First item</div>,
+    <div role="button" tabIndex={-1} onClick={action('Click on Second item')}>Second item</div>,
+    <div role="button" tabIndex={-1} onClick={action('Click on Third item')}>Third item</div>,
   ];
 
   return <BurgerMenu menuItems={menuItems} />;
