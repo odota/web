c0fa128ddbefc5c88c112eb5be3a8ba08767a82f
Wanderrful <galois@mentlegen.com>
2017-11-20 09:24:31 +0900

    Removed localized string text for the filter choices.

--- src/components/Player/TableFilterForm/TableFilter.config.js
+++ src/components/Player/TableFilterForm/TableFilter.config.js
@@ -73,18 +73,18 @@ export const significantList = [{
   value: 0,
 }];
 export const gamesPlayedList = [{
-  text: strings.filter_gamesplayed_5,
+  text: '5',
   value: 5,
 }, {
-  text: strings.filter_gamesplayed_10,
+  text: '10',
   value: 10,
 }, {
-  text: strings.filter_gamesplayed_15,
+  text: '15',
   value: 15,
 }, {
-  text: strings.filter_gamesplayed_20,
+  text: '20',
   value: 20,
 }, {
-  text: strings.filter_gamesplayed_25,
+  text: '25',
   value: 25,
 }];
