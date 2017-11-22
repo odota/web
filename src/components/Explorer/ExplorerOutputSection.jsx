5017f86d6092d23511eae0a09c0c83d4eb9fd31b
Howard Chung <howardc93@gmail.com>
2017-11-22 07:13:24 +0000

    remove logging

--- src/components/Explorer/ExplorerOutputSection.jsx
+++ src/components/Explorer/ExplorerOutputSection.jsx
@@ -77,7 +77,6 @@ class ExplorerOutputSection extends React.Component {
             } else if (column.field.indexOf('account_id') === 0) {
               return <Link to={`/players/${field}`}>{playerMapping[field] || field}</Link>;
             } else if (column.field.indexOf('winrate') === 0 || column.field.indexOf('pickrate') === 0 || column.field === 'winrate_wilson') {
-            console.log(field)
               return (field >= 0 && field <= 1 ? <TablePercent
                 percent={Number((field * 100).toFixed(2))}
               /> : null);
