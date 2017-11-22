f5d2a86f938c9cf5d69cbcb5f9078b00ef50c478
Howard Chung <howardc93@gmail.com>
2017-11-22 06:48:23 +0000

    four sig figs

--- src/components/Explorer/ExplorerOutputSection.jsx
+++ src/components/Explorer/ExplorerOutputSection.jsx
@@ -77,6 +77,7 @@ class ExplorerOutputSection extends React.Component {
             } else if (column.field.indexOf('account_id') === 0) {
               return <Link to={`/players/${field}`}>{playerMapping[field] || field}</Link>;
             } else if (column.field.indexOf('winrate') === 0 || column.field.indexOf('pickrate') === 0 || column.field === 'winrate_wilson') {
+            console.log(field)
               return (field >= 0 && field <= 1 ? <TablePercent
                 percent={Number((field * 100).toFixed(2))}
               /> : null);
@@ -116,7 +117,7 @@ class ExplorerOutputSection extends React.Component {
             if (row[column.field] === null || typeof row[column.field] === 'boolean' || Number.isNaN(Number(row[column.field]))) {
               return row[column.field];
             }
-            return Number(Number(row[column.field]).toFixed(2));
+            return Number(Number(row[column.field]).toPrecision(4));
           },
         }))}
       />);
