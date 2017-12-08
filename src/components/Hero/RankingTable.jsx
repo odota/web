e124f520ca9392821f3da3d3e559a266a058dcb0
Howard Chung <howardzchung@gmail.com>
2017-12-08 04:37:53 +0000

    add ranks to hero rankings

--- src/components/Hero/RankingTable.jsx
+++ src/components/Hero/RankingTable.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { shape, oneOfType, arrayOf } from 'prop-types';
 import Table from 'components/Table';
 import strings from 'lang';
-import { transformations, getOrdinal } from 'utility';
-import { Mmr } from 'components/Visualizations/Table/HeroImage';
+import { transformations, getOrdinal, rankTierToString } from 'utility';
 
 const rankingColumns = [{
   displayName: strings.th_rank,
@@ -11,7 +10,7 @@ const rankingColumns = [{
 }, {
   displayName: strings.th_name,
   displayFn: (row, col, field) => {
-    const subtitle = <Mmr number={row.solo_competitive_rank} />;
+    const subtitle = rankTierToString(row.rank_tier);
     return transformations.player({ ...row, subtitle }, col, field);
   },
 }, {
