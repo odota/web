3fcb681070849c980a80bf67bf7d4a0d250e2e52
Nathan <njgingrich@gmail.com>
2018-02-17 16:50:16 -0500

    better componentize Draft

--- src/components/Match/Draft/Draft.jsx
+++ src/components/Match/Draft/Draft.jsx
@@ -2,73 +2,144 @@ import React from 'react';
 import PropTypes from 'prop-types';
 // import strings from 'lang';
 import { getTeamName } from 'utility';
+import { TableBody, TableRow, TableRowColumn, Table } from 'material-ui/Table';
+import Heading from 'components/Heading';
+import { IconRadiant, IconDire } from 'components/Icons';
 import heroes from 'dotaconstants/build/heroes.json';
 import styled from 'styled-components';
-// import constants from '../../constants';
+import Next from 'material-ui/svg-icons/hardware/keyboard-arrow-right';
+import Prev from 'material-ui/svg-icons/hardware/keyboard-arrow-left';
+import constants from '../../constants';
 
 const Styled = styled.div`
-.Draft {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  padding: 16px 4px;
-}
+max-width: 800px;
+margin: 0 auto;
 
-.pickban {
-  width: calc(100% / 3);
-  display: flex;
-  flex-direction: column;
-}
-
-.pickban > span {
-  align-self: center;
-}
-
-.dire.Draft > .pickban {
-  margin-left: calc(100% / 3);
-  order: 0;
-}
-
-.dire.Draft > .image {
-  order: 1;
-}
-
-.radiant.Draft > .pickban {
-  margin-right: calc(100% / 3);
-  order: 1;
-}
-
-.radiant.Draft > .image {
-  order: 0;
-}
-
-.image {
-  width: calc(100% / 3);
-  display: flex;
-  align-items: flex-start;
-}
-
-.dire.Draft > .image > img {
-  margin-left: auto;
-}
-
-.image > img {
-  width: 100px;
+.draft-row:nth-of-type(odd) {
+  background-color: ${constants.tableRowOddSurfaceColor};
 }
 
 .teams {
   display: flex;
-}
-
-.teams > .radiant {
-  align-self: flex-start;
-}
-
-.teams > .dire {
-  margin-left: auto;
+  justify-content: space-between;
 }
 `;
+
+const PickBan = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+const Pick = PickBan.extend`
+  color: ${constants.colorGreen};
+
+  svg {
+    color: ${constants.colorGreen};
+  }
+`;
+
+const Ban = PickBan.extend`
+  color: ${constants.colorDanger};
+  svg {
+    color: ${constants.colorDanger};
+  }
+`;
+
+const DraftCell = styled.div`
+  display: flex;
+  width: fit-content;
+  margin-left: ${props => (props.dire ? 'auto' : '0')};
+
+  .time-tracker {
+    display: flex;
+    flex-direction: column;
+    align-items: ${props => (props.radiant ? 'flex-start' : 'flex-end')};
+    justify-content: space-around;
+    order: ${props => (props.radiant ? '1' : '-1')};
+    padding: 8px;
+  }
+
+  .taken {
+    font-size: 16px;
+  }
+
+  .left {
+    font-size: 13px;
+  }
+
+  .extra-used {
+    color: ${constants.colorDanger}
+  }
+
+  @media only screen and (max-width: 650px) {
+    flex-direction: column;
+
+    .time-tracker {
+      order: 1;
+    }
+  }
+`;
+
+const HeroIcon = styled.div`
+  position: relative;
+  width: fit-content;
+  
+  img {
+    width: 100px;
+    &[data-isPick="false"] {
+      filter: grayscale(100%);
+    }
+  }
+
+  .ban {
+    position: absolute;
+    z-index: 1;
+    left: -8px;
+    right: -8px;
+    top: 28px;
+    border-top: 2px solid ${constants.colorDanger};
+    transform: rotate(-28deg);
+  
+    @media only screen and (max-width: 716px) {
+      transform: rotate(-34deg);
+      top: 13px;
+    }
+  }
+`;
+
+const LeftArrow = styled(Prev)`
+  visibility: ${props => (props.visible === 'true' ? 'visible' : 'hidden')};
+`;
+
+const RightArrow = styled(Next)`
+visibility: ${props => (props.visible === 'true' ? 'visible' : 'hidden')};
+`;
+
+
+const TimeTracker = ({ pb, extraTime }) => (
+  <div className="time-tracker">
+    <span className="taken">
+      <span className={pb.total_time_taken > 30 ? 'extra-used' : ''}>
+        {pb.total_time_taken}s
+      </span> used
+    </span>
+    <span className="left">
+      {extraTime}s left
+      {pb.total_time_taken > 30 &&
+        <span className="extra-used">
+          {` (-${pb.total_time_taken - 30}s)`}
+        </span>
+      }
+    </span>
+  </div>
+);
+TimeTracker.propTypes = {
+  pb: PropTypes.shape({}),
+  extraTime: PropTypes.number,
+};
 
 // one-based indexing (since draft[i].order starts at 1)
 const orderOne = [1, 3, 5, 7, 10, 12, 14, 16, 18, 20, 21];
@@ -78,27 +149,93 @@ const picks = [7, 8, 9, 10, 15, 16, 17, 18, 21, 22];
 const Draft = ({ radiantTeam, direTeam, draft }) => {
   const firstIsTeamTwo = draft[0].active_team === 2;
   const radiantOrder = firstIsTeamTwo ? orderTwo : orderOne;
+  const radiantPick = pb => (radiantOrder.includes(pb.order));
+  let radiantTimeLeft = 130;
+  let direTimeLeft = 130;
+  const calcExtraTime = (isRadiantTeam, timeTaken) => {
+    if (isRadiantTeam) {
+      if (timeTaken > 30) radiantTimeLeft -= (timeTaken - 30);
+      return radiantTimeLeft;
+    }
+    if (timeTaken > 30) direTimeLeft -= (timeTaken - 30);
+    return direTimeLeft;
+  };
+
   return (
     <Styled>
       <section className="teams">
+        {/*
         <h3 className="radiant">{getTeamName(radiantTeam)}</h3>
         <h3 className="dire">{getTeamName(direTeam)}</h3>
+        */}
+        <Heading
+          title={`${getTeamName(radiantTeam, true)}`}
+          icon={<IconRadiant />}
+        />
+        <Heading
+          title={`${getTeamName(direTeam, false)}`}
+          icon={<IconDire />}
+        />
       </section>
-      <section className="draft-order">
-        {draft.map(pb => (
-          <section key={pb.order} className={`${radiantOrder.includes(pb.order) ? 'radiant' : 'dire'} Draft`}>
-            <div className="pickban">
-              <span>{picks.includes(pb.order) ? 'Pick' : 'Ban'}</span>
-            </div>
-            <div className="image">
-              <img
-                src={heroes[pb.hero_id] && process.env.REACT_APP_API_HOST + heroes[pb.hero_id].img}
-                alt=""
-              />
-            </div>
-          </section>
-        ))}
-      </section>
+      <Table selectable={false}>
+        <TableBody
+          displayRowCheckbox={false}
+          selectable={false}
+          className="draft-table"
+        >
+          {draft.map(pb => (
+            <TableRow
+              key={pb.order}
+              className={`${radiantOrder.includes(pb.order) ? 'radiant' : 'dire'} draft-row`}
+            >
+              <TableRowColumn style={{ paddingLeft: 0 }}>
+                {radiantPick(pb) &&
+                  <DraftCell radiant>
+                    <TimeTracker pb={pb} extraTime={calcExtraTime(true, pb.total_time_taken)} />
+                    <HeroIcon radiant>
+                      <img
+                        src={heroes[pb.hero_id] && process.env.REACT_APP_API_HOST + heroes[pb.hero_id].img}
+                        alt=""
+                        data-isPick={picks.includes(pb.order)}
+                      />
+                      {!picks.includes(pb.order) && <div className="ban" />}
+                    </HeroIcon>
+                  </DraftCell>
+                }
+              </TableRowColumn>
+              <TableRowColumn>
+                {picks.includes(pb.order) ?
+                  <Pick>
+                    <LeftArrow style={{ color: 'inherit' }} visible={radiantPick(pb) ? 'true' : 'false'} />
+                    Pick {pb.order}
+                    <RightArrow style={{ color: 'inherit' }} visible={radiantPick(pb) ? 'false' : 'true'} />
+                  </Pick> :
+                  <Ban>
+                    <LeftArrow style={{ color: 'inherit' }} visible={radiantPick(pb) ? 'true' : 'false'} />
+                    Ban {pb.order}
+                    <RightArrow style={{ color: 'inherit' }} visible={radiantPick(pb) ? 'false' : 'true'} />
+                  </Ban>
+                }
+              </TableRowColumn>
+              <TableRowColumn style={{ paddingRight: 0 }}>
+                {!radiantPick(pb) &&
+                  <DraftCell dire>
+                    <TimeTracker pb={pb} extraTime={calcExtraTime(false, pb.total_time_taken)} />
+                    <HeroIcon dire>
+                      <img
+                        src={heroes[pb.hero_id] && process.env.REACT_APP_API_HOST + heroes[pb.hero_id].img}
+                        alt=""
+                        data-isPick={picks.includes(pb.order)}
+                      />
+                      {!picks.includes(pb.order) && <div className="ban" />}
+                    </HeroIcon>
+                  </DraftCell>
+                }
+              </TableRowColumn>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </Styled>
   );
 };
