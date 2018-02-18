f33ddc4bde64af78ea920c017c375d70bea6c8eb
Nathan <njgingrich@gmail.com>
2018-02-18 13:58:44 -0500

    add default values for props

--- src/components/Match/Draft/Draft.jsx
+++ src/components/Match/Draft/Draft.jsx
@@ -144,7 +144,12 @@ const orderOne = [1, 3, 5, 7, 10, 12, 14, 16, 18, 20, 21];
 const orderTwo = [2, 4, 6, 8, 9, 11, 13, 15, 17, 19, 22];
 const picks = [7, 8, 9, 10, 15, 16, 17, 18, 21, 22];
 
-const Draft = ({ radiantTeam, direTeam, draft }) => {
+const Draft = ({
+  radiantTeam = {},
+  direTeam = {},
+  draft = [],
+}) => {
+  // if there is no draft data there is no meaning to firstIsTeamTwo
   const firstIsTeamTwo = draft[0] && draft[0].active_team === 2;
   const radiantOrder = firstIsTeamTwo ? orderTwo : orderOne;
   const radiantPick = pb => (radiantOrder.includes(pb.order));
