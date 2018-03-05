906769e975eab4919d61ca679b4c87796b4a9cf4
Argn0 <Argn0@users.noreply.github.com>
2018-03-05 04:48:35 +0100

    google badge

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -169,7 +169,7 @@ export default () => (
         </small>
         <StyledHr />
         <a href="https://play.google.com/store/apps/details?id=com.opendota.mobile&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1">
-          <img alt="Get it on Google Play" src="https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png" height="46px" />
+          <img src="/assets/images/google_play_store.png" alt="" height="46px" />
         </a>
         <a
           href="https://itunes.apple.com/us/app/opendota/id1354762555?ls=1&mt=8"
