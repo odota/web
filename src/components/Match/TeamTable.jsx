634a0b00d005772ed72852336705028f6806a779
Argn0 <Argn0@users.noreply.github.com>
2018-03-07 06:15:14 +0100

    user highlight

--- src/components/Match/TeamTable.jsx
+++ src/components/Match/TeamTable.jsx
@@ -3,8 +3,26 @@ import PropTypes from 'prop-types';
 import { isRadiant, getTeamName } from 'utility';
 import Heading from 'components/Heading';
 import { IconRadiant, IconDire } from 'components/Icons';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
 import Table from 'components/Table';
 import PicksBans from './Overview/PicksBans'; // Displayed only on `Overview` page
+
+const StyledDiv = styled.div`
+${props => (props.user !== undefined ? `
+#${isRadiant(props.user.player_slot) ? 'radiant' : 'dire'} {
+  table {
+    & tbody {
+      & tr {
+        &:nth-child(${props.user.player_slot + 1 - (!isRadiant(props.user.player_slot) * 128)}) {
+          background-color: rgba(0, 60, 180, 0.03);
+        }
+      }
+    }
+  }
+}
+` : '')}
+`;
 
 const filterMatchPlayers = (players, team = '') =>
   players.filter(player =>
@@ -19,22 +37,32 @@ const TeamTable = ({
   direTeam = {},
   summable = false,
   hoverRowColumn = false,
-}) => (
-  <div>
-    <Heading
-      title={`${getTeamName(radiantTeam, true)} - ${heading}`}
-      icon={<IconRadiant />}
-    />
-    <Table data={filterMatchPlayers(players, 'radiant')} columns={columns} summable={summable} hoverRowColumn={hoverRowColumn} />
-    {picksBans && <PicksBans data={picksBans.filter(pb => pb.team === 0)} /> /* team 0 - radiant */}
-    <Heading
-      title={`${getTeamName(direTeam, false)} - ${heading}`}
-      icon={<IconDire />}
-    />
-    <Table data={filterMatchPlayers(players, 'dire')} columns={columns} summable={summable} hoverRowColumn={hoverRowColumn} />
-    {picksBans && <PicksBans data={picksBans.filter(pb => pb.team === 1)} /> /* team 1 - dire */}
-  </div>
-);
+  loggedInId,
+}) => {
+  const user = players.find(player => player.account_id === loggedInId);
+
+  console.log(user && user.player_slot);
+  return (
+    <StyledDiv user={user}>
+      <Heading
+        title={`${getTeamName(radiantTeam, true)} - ${heading}`}
+        icon={<IconRadiant />}
+      />
+      <span id="radiant">
+        <Table data={filterMatchPlayers(players, 'radiant')} columns={columns} summable={summable} hoverRowColumn={hoverRowColumn} />
+      </span >
+      {picksBans && <PicksBans data={picksBans.filter(pb => pb.team === 0)} /> /* team 0 - radiant */}
+      <Heading
+        title={`${getTeamName(direTeam, false)} - ${heading}`}
+        icon={<IconDire />}
+      />
+      <span id="dire">
+        <Table data={filterMatchPlayers(players, 'dire')} columns={columns} summable={summable} hoverRowColumn={hoverRowColumn} />
+      </span>
+      {picksBans && <PicksBans data={picksBans.filter(pb => pb.team === 1)} /> /* team 1 - dire */}
+    </StyledDiv>
+  );
+};
 
 TeamTable.propTypes = {
   players: PropTypes.arrayOf({}),
@@ -47,4 +75,8 @@ TeamTable.propTypes = {
   hoverRowColumn: PropTypes.bool,
 };
 
-export default TeamTable;
+const mapStateToProps = state => ({
+  loggedInId: state.app.metadata.data.user ? state.app.metadata.data.user.account_id : null,
+});
+
+export default connect(mapStateToProps)(TeamTable);
