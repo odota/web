3a58a50f315b2e764a54d3bf7bdcbd425a1480c0
Albert Cui <aqc2109@columbia.edu>
2018-04-10 03:14:43 +0000

    update lint

--- src/components/Api/Api.jsx
+++ src/components/Api/Api.jsx
@@ -5,7 +5,6 @@ import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
 import CircularProgress from 'material-ui/CircularProgress';
 import RaisedButton from 'material-ui/RaisedButton';
-import strings from '../../lang';
 import styled from 'styled-components';
 import StripeCheckout from 'react-stripe-checkout';
 import moment from 'moment';
@@ -17,6 +16,7 @@ import {
   TableRow,
   TableRowColumn,
 } from 'material-ui/Table';
+import strings from '../../lang';
 
 const URI = '/api/keys';
 
@@ -43,100 +43,99 @@ class KeyManagement extends React.Component {
       error: false,
       loading: true,
     };
-    
+
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleUpdate = this.handleUpdate.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
   }
 
-  componentWillMount() {
-    this.setState({ matchId: window.location.hash.slice(1) });
-  }
-
   componentDidMount() {
     fetch(`${process.env.REACT_APP_API_HOST}${URI}`, {
       credentials: 'include',
-      method: 'GET'
+      method: 'GET',
     })
-    .then((res) => {
-      if(res.ok) {
-        return res.json();
-      } else if (res.status === 403) {
-        this.setState({loggedIn: false});
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else if (res.status === 403) {
+          return {};
+        }
         throw Error();
-      }
-    })
-    .then((json) => {
-      json.loading = false;
-      this.setState(json)
-    })
-    .catch(err => this.setState({error: true}))
+      })
+      .then((json) => {
+        const update = json;
+        update.loading = false;
+        this.setState(update);
+      })
+      .catch(() => this.setState({ error: true }));
   }
 
-  handleSubmit(token, address) {
-    this.setState({loading: true});
+  handleSubmit(token) {
+    this.setState({ loading: true });
     fetch(`${process.env.REACT_APP_API_HOST}${URI}`, {
       credentials: 'include',
       method: 'POST',
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        token: token
+        token,
+      }),
+    })
+      .then((res) => {
+        if (res.ok) {
+          window.location.reload(false);
+        } else {
+          throw Error();
+        }
       })
-    })
-    .then((res) => {
-      if (res.ok) {
-        window.location.reload(false);
-      } else {
-        throw Error(strings.api_error)
-      }
-    })
-    .catch(err => this.setState({error: true}))
+      .catch(() => this.setState({ error: true }));
   }
 
   handleDelete() {
-    this.setState({loading: true});
+    this.setState({ loading: true });
     fetch(`${process.env.REACT_APP_API_HOST}${URI}`, {
       credentials: 'include',
-      method: 'DELETE'
+      method: 'DELETE',
     })
-    .then((res) => {
-      if (res.ok) {
-        window.location.reload(false);
-      } else {
-        throw Error(strings.api_error)
-      }
-    })
-    .catch(err => this.setState({error: true}))
+      .then((res) => {
+        if (res.ok) {
+          window.location.reload(false);
+        } else {
+          throw Error();
+        }
+      })
+      .catch(() => this.setState({ error: true }));
   }
-  
-  handleUpdate(token, address) {
-    this.setState({loading: true});
+
+  handleUpdate(token) {
+    this.setState({ loading: true });
     fetch(`${process.env.REACT_APP_API_HOST}${URI}`, {
       credentials: 'include',
       method: 'PUT',
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        token: token
+        token,
+      }),
+    })
+      .then((res) => {
+        if (res.ok) {
+          window.location.reload(false);
+        } else {
+          throw Error(strings.api_error);
+        }
       })
-    })
-    .then((res) => {
-      if (res.ok) {
-        window.location.reload(false);
-      } else {
-        throw Error(strings.api_error)
-      }
-    })
-    .catch(err => this.setState({error: true}))
+      .catch(() => this.setState({ error: true }));
   }
-  
+
   render() {
     const { loading, user } = this.props;
+    const showLoginButton = !user;
+    const showGetKeyButton = user && !(this.state.customer && this.state.customer.api_key);
 
     return (
       <div>
@@ -144,68 +143,103 @@ class KeyManagement extends React.Component {
         <ApiContainer style={{ textAlign: 'center' }}>
           {
             this.state.error ?
-            <div>
-              {strings.api_error}
-            </div>
-            : <div/>
+              <div>
+                {strings.api_error}
+              </div>
+            : <div />
           }
           <h1>{strings.api_title}</h1>
           <h3>{strings.api_subtitle}</h3>
           {
             loading || this.state.loading ?
               <CircularProgress mode="indeterminate" />
-            : <div>
-                { user ? 
-                  (this.state.customer && this.state.customer.api_key ?
-                    <div />
-                  : <StripeCheckout
-                      billingAddress={true}
-                      stripeKey={process.env.REACT_APP_STRIPE_PUBLIC_KEY}
-                      token={this.handleSubmit}
-                      zipCode={true}
-                      locale='auto'
-                    >
-                      <RaisedButton label={strings.api_get_key} style={{margin:'5px 5px'}} />
-                    </StripeCheckout>)
-                : <a href={`${process.env.REACT_APP_API_HOST}/login`}>
-                    <RaisedButton label={strings.api_login} style={{margin:'5px 5px'}} />
+            :
+              <div>
+                { showLoginButton ?
+                  <a href={`${process.env.REACT_APP_API_HOST}/login`}>
+                    <RaisedButton primary label={strings.api_login} style={{ margin: '5px 5px' }} />
                   </a>
-              }
-              <a href={'//docs.opendota.com'}>
-                <RaisedButton label={strings.api_docs} style={{margin:'5px 5px'}} />
-              </a>
-              {
-                this.state.customer ?
+                  : <div />
+                }
+                { showGetKeyButton ?
+                  <StripeCheckout
+                    name="OpenDota"
+                    description={strings.api_title}
+                    billingAddress
+                    stripeKey={process.env.REACT_APP_STRIPE_PUBLIC_KEY}
+                    token={this.handleSubmit}
+                    zipCode
+                    locale="auto"
+                  >
+                    <RaisedButton primary label={strings.api_get_key} style={{ margin: '5px 5px' }} />
+                  </StripeCheckout>
+                  : <span />
+                }
+                <a href="//docs.opendota.com">
+                  <RaisedButton label={strings.api_docs} style={{ margin: '5px 5px' }} />
+                </a>
+                { this.state.customer ?
                   <div>
                     { this.state.customer.api_key ?
-                        <div>
-                          <h4>Your Key</h4>
-                          <KeyContainer>{this.state.customer.api_key}</KeyContainer>
-                          <p>{strings.api_billing_cycle
-                            .replace('$date', moment.unix(this.state.customer.current_period_end).format('MM-DD-YYYY')) + ' ' +
+                      <div>
+                        <h4>{strings.api_header_key}</h4>
+                        <KeyContainer>{this.state.customer.api_key}</KeyContainer>
+                        <p>{`${strings.api_billing_cycle
+                            .replace('$date', moment.unix(this.state.customer.current_period_end).format('MM-DD-YYYY'))} ${
                             strings.api_billed_to
                               .replace('$brand', this.state.customer.credit_brand)
-                              .replace('$last4', this.state.customer.credit_last4)
-                            }</p>
-                          <RaisedButton label={strings.api_delete} style={{margin:'5px 5px'}} onClick={this.handleDelete}/>
-                          <StripeCheckout
-                            billingAddress={true}
-                            stripeKey={process.env.REACT_APP_STRIPE_PUBLIC_KEY}
-                            token={this.handleUpdate}
-                            zipCode={true}
-                            locale='auto'
-                          >
-                            <RaisedButton label={strings.api_update_billing} style={{margin:'5px 5px'}} />
-                          </StripeCheckout>
+                              .replace('$last4', this.state.customer.credit_last4)}`
+                            }
+                        </p>
+                        <RaisedButton label={strings.api_delete} style={{ margin: '5px 5px' }} onClick={this.handleDelete} />
+                        <StripeCheckout
+                          name="OpenDota"
+                          description={strings.api_title}
+                          billingAddress
+                          stripeKey={process.env.REACT_APP_STRIPE_PUBLIC_KEY}
+                          token={this.handleUpdate}
+                          zipCode
+                          locale="auto"
+                        >
+                          <RaisedButton label={strings.api_update_billing} style={{ margin: '5px 5px' }} />
+                        </StripeCheckout>
+                      </div>
+                      : <div />
+                    }
+                    {
+                      this.state.usage ?
+                        <div>
+                          <h4>{strings.api_header_usage}</h4>
+                          <Table style={{ width: '60%', margin: '0 auto' }}>
+                            <TableHeader
+                              displaySelectAll={false}
+                              adjustForCheckbox={false}
+                            >
+                              <TableRow>
+                                <TableHeaderColumn>{strings.api_month}</TableHeaderColumn>
+                                <TableHeaderColumn>{strings.api_usage_calls}</TableHeaderColumn>
+                              </TableRow>
+                            </TableHeader>
+                            <TableBody
+                              displayRowCheckbox={false}
+                            >
+                              { this.state.usage.map(e => (
+                                <TableRow key={e.month}>
+                                  <TableRowColumn>{e.month}</TableRowColumn>
+                                  <TableRowColumn>{e.usage_count}</TableRowColumn>
+                                </TableRow>))}
+                            </TableBody>
+                          </Table>
                         </div>
-                      : <div/>
+                      : <div />
                     }
                   </div>
-                : <div/>
-              }
-            </div>
+                  : <div />
+                }
+              </div>
           }
-          <Table style={{width: '60%', margin:'0 auto' }}>
+          <h3>{strings.api_header_price}</h3>
+          <Table style={{ width: '60%', margin: '0 auto' }}>
             <TableHeader
               displaySelectAll={false}
               adjustForCheckbox={false}
@@ -224,9 +258,9 @@ class KeyManagement extends React.Component {
               </TableRow>
             </TableBody>
           </Table>
-          
-          <h3>{strings.api_details}</h3>
-          <ul style={{textAlign: 'left'}}>
+
+          <h3>{strings.api_header_details}</h3>
+          <ul style={{ textAlign: 'left' }}>
             <li>{strings.api_key_requirement}</li>
             <li>{strings.api_account_level}</li>
             <li>{strings.api_rate_limit}</li>
@@ -242,7 +276,6 @@ class KeyManagement extends React.Component {
 
 KeyManagement.propTypes = {
   loading: PropTypes.bool,
-  error: PropTypes.string,
   user: PropTypes.shape({}),
 };
 
