7551c2b24c94f2923c8da92d8e73448b6eef3b62
Argn0 <Argn0@users.noreply.github.com>
2018-04-14 06:21:40 +0200

    inline styling

--- src/components/Heroes/columns.jsx
+++ src/components/Heroes/columns.jsx
@@ -5,6 +5,7 @@ import {
   abbreviateNumber,
 } from '../../utility';
 import strings from '../../lang';
+import constants from '../constants';
 // import TablePercent from '../Visualizations/Table/Percent';
 
 const decimalToCount = (decimal, total) => (
@@ -48,85 +49,85 @@ export default {
     field: 'pickRate7',
     sortFn: true,
     percentBarsWithValue: row => decimalToCount(row.pickRate7, row.matchCount7),
-    colColor: 'rgba(33, 41, 69, 0.45)',
+    colColor: constants.colorDivine,
   }, {
     displayName: strings.rank_tier_7,
     field: 'winRate7',
     sortFn: true,
     percentBarsWithValue: row => decimalToCount(row.winRate7, row['7_pick']),
-    colColor: 'rgba(33, 41, 69, 0.65)',
+    colColor: constants.colorDivineAlt,
   }, {
     displayName: strings.rank_tier_6,
     field: 'pickRate6',
     sortFn: true,
     percentBarsWithValue: row => decimalToCount(row.pickRate6, row.matchCount6),
-    colColor: 'rgba(82, 52, 91, 0.45)',
+    colColor: constants.colorAncient,
   }, {
     displayName: strings.rank_tier_6,
     field: 'winRate6',
     sortFn: true,
     percentBarsWithValue: row => decimalToCount(row.winRate6, row['6_pick']),
-    colColor: 'rgba(82, 52, 91, 0.65)',
+    colColor: constants.colorAncientAlt,
   }, {
     displayName: strings.rank_tier_5,
     field: 'pickRate5',
     sortFn: true,
     percentBarsWithValue: row => decimalToCount(row.pickRate5, row.matchCount5),
-    colColor: 'rgba(84, 60, 26, 0.45)',
+    colColor: constants.colorLegend,
   }, {
     displayName: strings.rank_tier_5,
     field: 'winRate5',
     sortFn: true,
     percentBarsWithValue: row => decimalToCount(row.winRate5, row['5_pick']),
-    colColor: 'rgba(84, 60, 26, 0.65)',
+    colColor: constants.colorLegendAlt,
   }, {
     displayName: strings.rank_tier_4,
     field: 'pickRate4',
     sortFn: true,
     percentBarsWithValue: row => decimalToCount(row.pickRate4, row.matchCount4),
-    colColor: 'rgba(24, 85, 74, 0.45)',
+    colColor: constants.colorArchon,
   }, {
     displayName: strings.rank_tier_4,
     field: 'winRate4',
     sortFn: true,
     percentBarsWithValue: row => decimalToCount(row.winRate4, row['4_pick']),
-    colColor: 'rgba(24, 85, 74, 0.65)',
+    colColor: constants.colorArchonAlt,
   }, {
     displayName: strings.rank_tier_3,
     field: 'pickRate3',
     sortFn: true,
     percentBarsWithValue: row => decimalToCount(row.pickRate3, row.matchCount3),
-    colColor: 'rgba(58, 73, 124, 0.45)',
+    colColor: constants.colorCrusader,
   }, {
     displayName: strings.rank_tier_3,
     field: 'winRate3',
     sortFn: true,
     percentBarsWithValue: row => decimalToCount(row.winRate3, row['3_pick']),
-    colColor: 'rgba(58, 73, 124, 0.65)',
+    colColor: constants.colorCrusaderAlt,
   }, {
     displayName: strings.rank_tier_2,
     field: 'pickRate2',
     sortFn: true,
     percentBarsWithValue: row => decimalToCount(row.pickRate2, row.matchCount2),
-    colColor: 'rgba(114, 79, 49, 0.45)',
+    colColor: constants.colorGuardian,
   }, {
     displayName: strings.rank_tier_2,
     field: 'winRate2',
     sortFn: true,
     percentBarsWithValue: row => decimalToCount(row.winRate2, row['2_pick']),
-    colColor: 'rgba(114, 79, 49, 0.65)',
+    colColor: constants.colorGuardianAlt,
   }, {
     displayName: strings.rank_tier_1,
     field: 'pickRate1',
     sortFn: true,
     percentBarsWithValue: row => decimalToCount(row.pickRate1, row.matchCount1),
-    colColor: 'rgba(32, 74, 33, 0.45)',
+    colColor: constants.colorHerald,
   }, {
     displayName: strings.rank_tier_1,
     field: 'winRate1',
     sortFn: true,
     percentBarsWithValue: row => decimalToCount(row.winRate1, row['1_pick']),
-    colColor: 'rgba(32, 74, 33, 0.65)',
+    colColor: constants.colorHeraldAlt,
   }].map((col, i) => ({
     ...col,
     displayName: i === 0 ? col.displayName : `${col.displayName.substring(0, 2)} ${col.field.startsWith('pick') ? strings.abbr_pick : strings.abbr_win}%`,
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -55,19 +55,6 @@ const toUnderline = (data, row, field, underline) => {
 
 const rowStyle = (highlightFn, row) => ({ backgroundColor: highlightFn && highlightFn(row) ? 'rgba(74, 149, 247, 0.038)' : 'none' });
 
-const compileColColors = (columns) => {
-  const colColors = [];
-  columns.forEach((col, i) => {
-    if (col.colColor) {
-      colColors.push({
-        index: i + 1,
-        color: col.colColor,
-      });
-    }
-  });
-  return colColors;
-};
-
 const initialState = {
   currentPage: 0,
   sortState: '',
@@ -164,7 +151,7 @@ class Table extends React.Component {
       data = data.slice(currentPage * pageLength, (currentPage + 1) * pageLength);
     }
     return (
-      <StyledBody hoverRowColumn={hoverRowColumn} colColors={compileColColors(columns)}>
+      <StyledBody hoverRowColumn={hoverRowColumn}>
         {paginated && <Pagination
           numPages={Math.ceil(dataLength / pageLength)}
           currentPage={currentPage}
@@ -194,13 +181,14 @@ class Table extends React.Component {
                     {columns.map((column, colIndex) => {
                       const {
                         field, color, center, displayFn, relativeBars, percentBars,
-                        percentBarsWithValue, invertBarColor, underline,
+                        percentBarsWithValue, invertBarColor, underline, colColor,
                       } = column;
                       const columnSortFn = column.sortFn;
                       const getValue = typeof columnSortFn === 'function' ? columnSortFn : null;
                       const value = getValue ? getValue(row) : row[field];
                       const style = {
                         overflow: `${field === 'kills' ? 'visible' : null}`,
+                        backgroundColor: colColor,
                         color,
                         marginBottom: 0,
                         textUnderlinePosition: 'under',
