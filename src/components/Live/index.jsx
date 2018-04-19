3b001f50a3993af000ab209b878a35f30fd19c6d
hchung <howardzchung@gmail.com>
2018-04-19 00:39:47 -0700

    align

--- src/components/Live/index.jsx
+++ src/components/Live/index.jsx
@@ -23,34 +23,36 @@ class Live extends React.Component {
       <div>
         <Heading title={strings.header_live} />
         <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-          {data.map(game => (<div style={{ width: '25%' }}>
-            <div>{game.spectators} watching</div>
-            <div>{formatSeconds(game.game_time)}</div>
-            {game.average_mmr ? <div>{game.average_mmr} Average MMR</div> : null}
-            <div>{game.radiant_score} - {game.dire_score}</div>
-            <div>{game.radiant_lead} Radiant lead</div>
-            <div>
-              {game.players.filter((player, i) => i < 5).map(player => (
-                <div>
-                  <TableHeroImage
-                    image={heroes[player.hero_id] && process.env.REACT_APP_API_HOST + heroes[player.hero_id].img}
-                    title={player.name}
-                    accountId={player.account_id}
-                    heroName={heroes[player.hero_id] ? heroes[player.hero_id].localized_name : strings.general_no_hero}
-                  />
-                </div>))}
-            </div>
-            <div>
-              {game.players.filter((player, i) => i >= 5).map(player => (<div>
-                <TableHeroImage
-                  image={heroes[player.hero_id] && process.env.REACT_APP_API_HOST + heroes[player.hero_id].img}
-                  title={player.name}
-                  accountId={player.account_id}
-                  heroName={heroes[player.hero_id] ? heroes[player.hero_id].localized_name : strings.general_no_hero}
-                />
-                                                                         </div>))}
-            </div>
-                             </div>))}
+          {data.map(game => (
+            <div style={{ width: '25%' }}>
+              <div>{game.spectators} watching</div>
+              <div>{formatSeconds(game.game_time)}</div>
+              {game.average_mmr ? <div>{game.average_mmr} Average MMR</div> : null}
+              <div>{game.radiant_score} - {game.dire_score}</div>
+              <div>{game.radiant_lead} Radiant lead</div>
+              <div>
+                {game.players.filter((player, i) => i < 5).map(player => (
+                  <div>
+                    <TableHeroImage
+                      image={heroes[player.hero_id] && process.env.REACT_APP_API_HOST + heroes[player.hero_id].img}
+                      title={player.name}
+                      accountId={player.account_id}
+                      heroName={heroes[player.hero_id] ? heroes[player.hero_id].localized_name : strings.general_no_hero}
+                    />
+                  </div>))}
+              </div>
+              <div>
+                {game.players.filter((player, i) => i >= 5).map(player => (
+                  <div>
+                    <TableHeroImage
+                      image={heroes[player.hero_id] && process.env.REACT_APP_API_HOST + heroes[player.hero_id].img}
+                      title={player.name}
+                      accountId={player.account_id}
+                      heroName={heroes[player.hero_id] ? heroes[player.hero_id].localized_name : strings.general_no_hero}
+                    />
+                  </div>))}
+              </div>
+            </div>))}
         </div>
       </div>);
   }
