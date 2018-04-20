677e2196aa1c29a6642c57bd6143751229fffd78
hchung <howardzchung@gmail.com>
2018-04-20 03:48:16 -0700

    support dual import of strings

--- src/actions/index.js
+++ src/actions/index.js
@@ -66,8 +66,8 @@ export const getStrings = () => async (dispatch) => {
   const savedLang = window.localStorage && window.localStorage.getItem('localization');
   const defaultLang = langs[0];
   const selectedLang = langs.find(lang => lang.value === savedLang) || langs[0];
-  const defData = await import(`../lang/${defaultLang.data}`);
-  const selData = await import(`../lang/${selectedLang.data}`);
+  const defData = await import(`../lang/${defaultLang.value}.json`);
+  const selData = await import(`../lang/${selectedLang.value}.json`);
   dispatch({ type: 'strings', payload: { ...defData, ...selData } });
 };
 export * from './requestActions';
--- src/lang/index.js
+++ src/lang/index.js
@@ -1,97 +1,91 @@
+/* eslint-disable global-require */
 export const langs = [
   {
     value: 'en-US',
     native: 'English (US)',
-    data: 'en-US.json',
   }, /* {
     value: 'bg-BG',
     native: 'български bǎlgarski',
-    data: require('bg-BG.json'),
   }, {
     value: 'cs-CZ',
     native: 'Čeština',
-    data: require('cs-CZ.json'),
   }, */ {
     value: 'de-DE',
     native: 'Deutsch',
-    data: 'de-DE.json',
   }, {
     value: 'es-ES',
     native: 'Español',
-    data: 'es-ES.json',
   }, /* {
     value: 'es-PE',
     native: 'Español (América Latina)',
-    data: require('es-PE.json'),
   }, {
     value: 'fi-FI',
     native: 'Suomi',
-    data: require('fi-FI.json'),
   },  {
     value: 'fr-FR',
     native: 'Français',
-    data: require('fr-FR.json'),
   }, */ {
     value: 'it-IT',
     native: 'Italiano',
-    data: 'it-IT.json',
   }, {
     value: 'ja-JP',
     native: '日本語',
-    data: 'ja-JP.json',
   }, /* {
     value: 'ko-KR',
     native: '한국어',
-    data: require('ko-KR.json'),
   }, */ {
     value: 'ms-MY',
     native: 'Bahasa Melayu',
-    data: 'ms-MY.json',
   }, /* {
     value: 'nl-NL',
     native: 'Nederlands',
-    data: require('nl-NL.json'),
   }, */ {
     value: 'pl-PL',
     native: 'Polski',
-    data: 'pl-PL.json',
   }, {
     value: 'pt-BR',
     native: 'Português Brasileiro',
-    data: 'pt-BR.json',
   }, {
     value: 'ro-RO',
     native: 'Română',
-    data: 'ro-RO.json',
   }, {
     value: 'ru-RU',
     native: 'Русский',
-    data: 'ru-RU.json',
   }, /* {
     value: 'sv-SE',
     native: 'Svenska',
-    data: require('sv-SE.json'),
   }, {
     value: 'tr-TR',
     native: 'Türkçe',
-    data: require('tr-TR.json'),
   },  {
     value: 'uk-UA',
     native: 'Українська',
-    data: require('uk-UA.json'),
   }, {
     value: 'vi-VN',
     native: 'Tiếng Việt',
-    data: require('vi-VN.json'),
   }, */ {
     value: 'zh-CN',
     native: '中文',
-    data: 'zh-CN.json',
   }, /* {
     value: 'zh-TW',
     native: '中文 (繁體字)',
-    data: require('zh-TW.json'),
   }, */
 ];
 
-export default {};
+// Temporary until we move fully to dynamic loaded strings
+const savedLang = window.localStorage && window.localStorage.getItem('localization');
+const selectedLang = langs.find(lang => lang.value === savedLang) || {};
+const data = {
+  'en-US': require('./en-US.json'),
+  'de-DE': require('./de-DE.json'),
+  'es-ES': require('./es-ES.json'),
+  'it-IT': require('./it-IT.json'),
+  'ja-JP': require('./ja-JP.json'),
+  'ms-MY': require('./ms-MY.json'),
+  'pl-PL': require('./pl-PL.json'),
+  'pt-BR': require('./pt-BR.json'),
+  'ro-RO': require('./ro-RO.json'),
+  'ru-RU': require('./ru-RU.json'),
+  'zh-CN': require('./zh-CN.json'),
+};
+export default { ...data[langs[0].value], ...data[selectedLang.value] };
