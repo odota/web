120022c30bc2b4e805b9d0d87705649681616fc0
Ubuntu <howardzchung@gmail.com>
2018-04-24 09:36:35 +0000

    add autocomplete for IDs to Explorer

--- src/components/Explorer/autocomplete.js
+++ src/components/Explorer/autocomplete.js
@@ -1,3 +1,5 @@
+import heroes from 'dotaconstants/build/heroes.json';
+import items from 'dotaconstants/build/items.json';
 import strings from '../../lang';
 // import fields from './fields';
 
@@ -7,16 +9,26 @@ const tables = ['matches', 'player_matches', 'teams', 'match_logs', 'public_matc
 const sqlks = ['OFFSET', 'LIMIT', 'DISTINCT', 'IN'];
 const sqlfuncs = ['to_timestamp()', 'count()', 'avg()', 'sum()', 'stddev()', 'min()', 'max()', 'using()'];
 
-const autocomplete = cols => ({
-  getCompletions(editor, session, pos, prefix, callback) {
-    callback(null, []
-      .concat(sqlfuncs.map(e => ({ value: e, meta: strings.explorer_postgresql_function })))
-      .concat(sqlfs.map(e => ({ value: e, meta: strings.explorer_sql })))
-      .concat(sqlts.map(e => ({ value: e, meta: strings.explorer_sql })))
-      .concat(sqlks.map(e => ({ value: e, meta: strings.explorer_sql })))
-      .concat(tables.map(e => ({ value: e, meta: strings.explorer_table })))
-      .concat(cols.map(e => ({ value: `${e.column_name}`, meta: `${e.table_name} - ${e.data_type}` }))));
-  },
-});
+const autocomplete = (cols, proPlayers = [], teams = [], leagues = []) => {
+  const filteredPros = proPlayers.filter(p => p.name).slice(0).map(p => ({ value: p.name, snippet: p.account_id.toString(), meta: `${p.account_id.toString()} (${strings.explorer_player})` }));
+  const filteredTeams = teams.filter(t => t.name).slice(0, 100).map(t => ({ value: t.name, snippet: t.team_id.toString(), meta: `${t.team_id.toString()} (${strings.explorer_organization})` }));
+  const filteredLeagues = leagues.filter(l => l.tier === 'premium' || l.tier === 'professional').map(l => ({ value: l.name, snippet: l.leagueid.toString(), meta: `${l.leagueid.toString()} (${strings.explorer_league})` }));
+  return {
+    getCompletions(editor, session, pos, prefix, callback) {
+      callback(null, []
+        .concat(Object.keys(heroes).map(k => ({ value: heroes[k].localized_name, snippet: k, meta: `${k} (${strings.explorer_hero})` })))
+        .concat(Object.keys(items).filter(k => k.indexOf('recipe_') !== 0).map(k => ({ value: items[k].dname, snippet: `'${k}'`, meta: `${k} (${strings.scenarios_item})` })))
+        .concat(filteredPros)
+        .concat(filteredTeams)
+        .concat(filteredLeagues)
+        .concat(sqlfuncs.map(e => ({ value: e, meta: strings.explorer_postgresql_function })))
+        .concat(sqlfs.map(e => ({ value: e, meta: strings.explorer_sql })))
+        .concat(sqlts.map(e => ({ value: e, meta: strings.explorer_sql })))
+        .concat(sqlks.map(e => ({ value: e, meta: strings.explorer_sql })))
+        .concat(tables.map(e => ({ value: e, meta: strings.explorer_table })))
+        .concat(cols.map(e => ({ value: `${e.column_name}`, meta: `${e.table_name} - ${e.data_type}` }))));
+    },
+  };
+};
 
 export default autocomplete;
--- src/components/Explorer/index.jsx
+++ src/components/Explorer/index.jsx
@@ -45,6 +45,7 @@ function expandBuilderState(builder, _fields) {
 
 class Explorer extends React.Component {
   static propTypes = {
+    schema: PropTypes.arrayOf({}),
     proPlayers: PropTypes.arrayOf({}),
     teams: PropTypes.arrayOf({}),
     leagues: PropTypes.shape({}),
@@ -83,6 +84,15 @@ class Explorer extends React.Component {
       import('ace-builds/src-noconflict/mode-sql'),
     ]);
     this.instantiateEditor();
+  }
+
+  componentDidUpdate(nextProps) {
+    if (this.editor && !this.completersSet && nextProps.proPlayers.length && nextProps.teams.length && nextProps.leagues.length) {
+      this.completersSet = true;
+      fetch(`${process.env.REACT_APP_API_HOST}/api/schema`).then(jsonResponse).then((schema) => {
+        this.editor.completers = [autocomplete(schema, nextProps.proPlayers, nextProps.teams, nextProps.leagues)];
+      });
+    }
   }
 
   getSqlString = () => this.editor.getSelectedText() || this.editor.getValue();
@@ -138,14 +148,11 @@ class Explorer extends React.Component {
     const editor = ace.edit('editor');
     editor.setTheme('ace/theme/monokai');
     editor.getSession().setMode('ace/mode/sql');
-    editor.setShowPrintMargin(false);
     editor.setOptions({
       minLines: 10,
       maxLines: Infinity,
       enableLiveAutocompletion: true,
-    });
-    fetch(`${process.env.REACT_APP_API_HOST}/api/schema`).then(jsonResponse).then((schema) => {
-      editor.completers = [autocomplete(schema)];
+      showPrintMargin: false,
     });
     this.editor = editor;
     const { sql } = querystring.parse(window.location.search.substring(1));
