f2815e89e3d64152e39ef3e9f3470bfc9a13909a
Argn0 <Argn0@users.noreply.github.com>
2018-04-27 15:50:58 +0200

    border

--- src/components/Heading/Styled.jsx
+++ src/components/Heading/Styled.jsx
@@ -37,6 +37,7 @@ export const StyledDiv = styled.div`
     margin-left: 5px;
     font-size: ${constants.fontSizeMedium};
     color: ${constants.colorMuted};
+    border-bottom: 1px dotted ${constants.colorMuted};
   }
 `;
 
