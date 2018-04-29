c13b2088cee65e88df0dc9902810b19d49e953a2
Dominik Biedebach <6538827+bdbch@users.noreply.github.com>
2018-04-29 16:26:42 +0200

    Improved Frontend for Player Totals (Records)

--- src/components/Player/Pages/Totals/Totals.jsx
+++ src/components/Player/Pages/Totals/Totals.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { CardTitle } from 'material-ui/Card';
 // import util from 'util';
+import Card from './Card'
 import { getPlayerTotals } from '../../../../actions';
 import Container from '../../../Container';
 import strings from '../../../../lang';
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-left: -8px;
+  margin-right: -8px;
+`
 
 const totalsToShow = {
   kills: 1,
@@ -30,24 +38,9 @@ const totalsToShow = {
   pings: 'parsed',
 };
 
-// Strings are in format e.g. '%d seconds'
-const getAbbrTime = str => str.slice(3, 4);
-
-const formatDurationString = (sec) => {
-  const days = Math.floor(sec / 86400);
-  const hours = Math.floor((sec - (days * 86400)) / 3600);
-  const minutes = Math.floor((sec - (days * 86400) - (hours * 3600)) / 60);
-  const seconds = Math.floor((sec - (days * 86400) - (hours * 3600) - (minutes * 60)));
-  return `${days}${getAbbrTime(strings.time_dd)} ${hours}${getAbbrTime(strings.time_hh)} ${minutes}${getAbbrTime(strings.time_mm)} ${seconds}${getAbbrTime(strings.time_ss)}`;
-};
-
 const drawElement = (element, type) => {
   if (totalsToShow[element.field] === type) {
-    return (
-      <CardTitle
-        subtitle={<div>{element.field === 'duration' ? formatDurationString(element.sum) : Math.floor(element.sum).toLocaleString()}</div>}
-        title={strings[`heading_${element.field}`]}
-      />);
+    return <Card total={element} />
   }
   return null;
 };
@@ -55,14 +48,14 @@ const drawElement = (element, type) => {
 const Totals = ({ data, error, loading }) => (
   <div>
     <Container title={strings.heading_all_matches} error={error} loading={loading}>
-      <div>
+      <CardContainer>
         {data.map(element => drawElement(element, 1))}
-      </div>
+      </CardContainer>
     </Container>
     <Container title={strings.heading_parsed_matches} error={error} loading={loading}>
-      <div>
+      <CardContainer>
         {data.map(element => drawElement(element, 'parsed'))}
-      </div>
+      </CardContainer>
     </Container>
   </div>);
 
