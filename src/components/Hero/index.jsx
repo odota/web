94e77f54d94ade2f662e7a93fdc61ea4e0bf135e
Dominik Biedebach <6538827+bdbch@users.noreply.github.com>
2018-04-29 15:53:54 +0200

    Refactored Hero Component

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -10,124 +10,17 @@ import Heading from '../Heading';
 import TabBar from '../TabBar';
 import Spinner from '../Spinner';
 import ErrorBox from '../Error/ErrorBox';
-import constants from '../constants';
+import Header from './Header';
 import Ranking from './Ranking';
 import Benchmark from './Benchmark';
 import Recent from './Recent';
 import Matchups from './Matchups';
-import AttributesMain from './AttributesMain';
 import AttributesBlock from './AttributesBlock';
 import Durations from './Durations';
 import Players from './Players';
 
-const getHeroImgSrc = src => process.env.REACT_APP_API_HOST + src;
-
-const WRAP_WIDTH = '1000px';
-const MOBILE_WIDTH = '680px';
-
-const HeroAvatar = styled.img`
-  border-radius: 16px;
-  border: solid 1px rgba(0, 0, 0, 0.3);
-  box-shadow: 0 12px 32px rgba(0, 0, 0, .3);
-  display: block;
-  flex-shrink: 0;
-  height: 128px;
-  object-fit: cover;
-  width: 180px;
-
-  @media screen and (max-width: ${WRAP_WIDTH}) {
-    height: 64px;
-    width: 120px;
-  }
-
-  @media screen and (max-width: ${MOBILE_WIDTH}) {
-    margin: auto;
-  }
-`;
-
 const HeroBlock = styled.div`
   margin-bottom: 8px;
-`;
-
-const HeroProfile = styled.div`
-  background: ${constants.almostBlack};
-  overflow: hidden;
-  position: relative;
-  border-radius: 8px;
-`;
-
-const HeroProfileBackground = styled.img`
-  background-repeat: no-repeat;
-  filter: blur(25px);
-  height: 125%;
-  left: -12.5%;
-  object-fit: cover;
-  opacity: .35;
-  position: absolute;
-  top: -12.5%;
-  width: 125%;
-  z-index: 1;
-`;
-
-const HeroProfileContent = styled.div`
-  align-items: center;
-  box-shadow: inset 0 0 125px rgba(0, 0, 0, .25);
-  display: flex;
-  padding: 56px;
-  position: relative;
-  z-index: 2;
-
-  @media screen and (max-width: ${WRAP_WIDTH}) {
-    padding: 16px;
-  }
-
-  @media screen and (max-width: ${MOBILE_WIDTH}) {
-    display: block;
-    text-align: center;
-  }
-`;
-
-const HeroDetails = styled.div`
-  flex-grow: 1;
-  margin: 0 24px;
-
-  @media screen and (max-width: ${MOBILE_WIDTH}) {
-    margin: 12px 0;
-  }
-`;
-
-const HeroName = styled.div`
-  font-size: 40px;
-  font-weight: ${constants.fontWeightMedium};
-  line-height: 40px;
-
-  @media screen and (max-width: ${WRAP_WIDTH}) {
-    font-size: 28px;
-    line-height: 28px;
-  }
-`;
-
-const HeroRoleInformations = styled.div`
-  color: ${constants.primaryTextColor};
-  font-size: 12px;
-  letter-spacing: 1px;
-  margin: 8px 0;
-  text-transform: uppercase;
-
-  @media screen and (max-width: ${WRAP_WIDTH}) {
-    font-size: 10px;
-  }
-`;
-
-const HeroRoles = styled.span`
-  color: ${constants.colorMutedLight};
-  word-wrap: break-word;
-`;
-
-const HeroDescription = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex-grow: 1;
 `;
 
 const HeroFooter = styled.div`
@@ -250,21 +143,7 @@ class Hero extends React.Component {
       <div>
         <Helmet title={hero.localized_name} />
         <HeroBlock>
-          <HeroDescription>
-            <HeroProfile>
-              <HeroProfileBackground alt={hero.localized_name} src={getHeroImgSrc(hero.img)} />
-              <HeroProfileContent>
-                <HeroAvatar alt={hero.localized_name} src={getHeroImgSrc(hero.img)} />
-                <HeroDetails>
-                  <HeroName>{hero.localized_name}</HeroName>
-                  <HeroRoleInformations>{hero.attack_type} - <HeroRoles>{hero.roles.join(', ')}</HeroRoles></HeroRoleInformations>
-                </HeroDetails>
-                <div>
-                  <AttributesMain hero={hero} />
-                </div>
-              </HeroProfileContent>
-            </HeroProfile>
-          </HeroDescription>
+          <Header hero={hero} />
           <HeroFooter>
             <HeroDetailsButton type="button" onClick={this.toggleDetailVisibility}>
               {this.state.detailsOpen ? strings.hide_details : strings.show_more_details}
