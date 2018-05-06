d12dda02e5aac8001925e82f2578f0a60868d431
cass <wcaplinger@gmail.com>
2018-05-05 23:49:52 -0400

    minor changes

--- src/components/Table/TableSkeleton.jsx
+++ src/components/Table/TableSkeleton.jsx
@@ -2,19 +2,18 @@ import React from 'react';
 import ContentLoader from 'react-content-loader';
 
 const TableSkeleton = props => (
-	<ContentLoader
-		height={160}
+  <ContentLoader
+    height={160}
     width={500}
     primaryColor="#666"
-    secondaryColor="#666"
-		{...props}
-	>
-		<rect x="0" y="10" rx="5" ry="5" width="500" height="5" /> 
-		<rect x="0" y="25" rx="5" ry="5" width="500" height="5" /> 
-		<rect x="0" y="40" rx="5" ry="5" width="500" height="5" /> 
-		<rect x="0" y="55" rx="5" ry="5" width="500" height="5" /> 
-		<rect x="0" y="70" rx="5" ry="5" width="500" height="5" />
-	</ContentLoader>
-)
+    {...props}
+  >
+    <rect x="0" y="10" rx="5" ry="5" width="500" height="5" />
+    <rect x="0" y="25" rx="5" ry="5" width="500" height="5" />
+    <rect x="0" y="40" rx="5" ry="5" width="500" height="5" />
+    <rect x="0" y="55" rx="5" ry="5" width="500" height="5" />
+    <rect x="0" y="70" rx="5" ry="5" width="500" height="5" />
+  </ContentLoader>
+);
 
 export default TableSkeleton;
