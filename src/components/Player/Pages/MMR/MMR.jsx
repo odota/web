ded552ac522d881ad9a0dc15c3f328cc1b0c97d9
cass <wcaplinger@gmail.com>
2018-05-13 12:54:32 -0400

    string refs

--- src/components/Player/Pages/MMR/MMR.jsx
+++ src/components/Player/Pages/MMR/MMR.jsx
@@ -3,20 +3,21 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { MMRGraph } from '../../../Visualizations';
 import { getPlayerMmr } from '../../../../actions';
-import strings from '../../../../lang';
 import Container from '../../../Container';
 import Info from '../../../Alerts/Info';
 
-const MMRInfo = (
+const MMRInfo = strings => (
   <Info>
     <a href="https://blog.opendota.com/2016/01/13/opendota-mmr-and-you/" target="_blank" rel="noopener noreferrer">
       {strings.mmr_not_up_to_date}
     </a>
   </Info>);
 
-const MMR = ({ columns, error, loading }) => (
+const MMR = ({
+  columns, error, loading, strings,
+}) => (
   <div>
-    <Container title={strings.heading_mmr} subtitle={MMRInfo} error={error} loading={loading}>
+    <Container title={strings.heading_mmr} subtitle={MMRInfo(strings)} error={error} loading={loading}>
       <MMRGraph columns={columns} />
     </Container>
   </div>
@@ -26,6 +27,7 @@ MMR.propTypes = {
   columns: PropTypes.arrayOf({}),
   error: PropTypes.string,
   loading: PropTypes.bool,
+  strings: PropTypes.shape({}),
 };
 
 
@@ -39,6 +41,7 @@ class RequestLayer extends React.Component {
       key: PropTypes.string,
     }),
     playerId: PropTypes.string,
+    strings: PropTypes.shape({}),
   }
 
   UNSAFE_componentWillMount() {
@@ -61,6 +64,7 @@ const mapStateToProps = state => ({
   columns: state.app.playerMmr.data,
   loading: state.app.playerMmr.loading,
   error: state.app.playerMmr.error,
+  strings: state.app.strings,
 });
 
 export default connect(mapStateToProps, { getPlayerMmr })(RequestLayer);
