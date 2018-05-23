aceeddba959e7f2793f3569a2731235030a97d13
Albert Cui <angelasun55@gmail.com>
2018-05-22 21:33:27 -0700

    Added button for improving your whatever

--- src/components/Match/Match.jsx
+++ src/components/Match/Match.jsx
@@ -8,6 +8,7 @@ import { getMatch } from '../../actions';
 import MatchHeader from './MatchHeader';
 import matchPages from './matchPages';
 import FourOhFour from '../../components/FourOhFour';
+import FlatButton from 'material-ui/FlatButton';
 
 class RequestLayer extends React.Component {
   static propTypes = {
@@ -42,6 +43,7 @@ class RequestLayer extends React.Component {
     const info = this.props.match.params.info || 'overview';
     const page = matchPages(matchId).find(_page => _page.key.toLowerCase() === info);
     const pageTitle = page ? `${matchId} - ${page.name}` : matchId;
+	const weirdOnes = ["overview", "fantasy", "analysis", "graphs", "chat", "cosmetics", "log", "story"];
     if (error) {
       return <FourOhFour msg={strings.request_invalid_match_id} />;
     }
@@ -58,6 +60,13 @@ class RequestLayer extends React.Component {
             tabs={matchPages(matchId, matchData)}
             match={matchData}
           />
+		  <h1> <FlatButton
+          label={`Improve your ${weirdOnes.indexOf(info)==-1? info : "game"}`}
+          icon={<img src="/assets/images/rivalry-icon.png" alt="" height="24px" />}
+          href="https://glhf.rivalry.gg/get-started-dota/?utm_source=opendota&utm_medium=link&utm_campaign=opendota"
+          target="_blank"
+          rel="noopener noreferrer"
+        /></h1>
           {page && page.content(matchData)}
         </div>);
   }
