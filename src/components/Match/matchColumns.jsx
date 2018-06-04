c4ca68c5cc51f3b65e027ea16fcb847fba813837
Argn0 <Argn0@users.noreply.github.com>
2018-06-04 17:20:49 +0200

    lint

--- src/components/Match/matchColumns.jsx
+++ src/components/Match/matchColumns.jsx
@@ -945,11 +945,11 @@ export default (strings) => {
       displayName: strings.th_damage_received,
       field: 'damage_inflictor_received',
       displayFn: (row, col, field) =>
-      (
-        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-          {(field ? Object.keys(field).sort((a, b) => field[b] - field[a]).map(inflictor => inflictorWithValue(inflictor, abbreviateNumber(field[inflictor]))) : '')}
-        </div>
-      ),
+        (
+          <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+            {(field ? Object.keys(field).sort((a, b) => field[b] - field[a]).map(inflictor => inflictorWithValue(inflictor, abbreviateNumber(field[inflictor]))) : '')}
+          </div>
+        ),
     },
   ];
 
