9371a75620307b312bc80c58a9cb58464d3db6d8
Dominik Biedebach <6538827+bdbch@users.noreply.github.com>
2018-06-09 17:12:36 +0200

    Added correct styling to Attribute Icons

--- src/components/Hero/Ability.jsx
+++ src/components/Hero/Ability.jsx
@@ -3,14 +3,40 @@ import styled from 'styled-components'
 import ReactTooltip from 'react-tooltip'
 import uuid from 'uuid'
 
+import constants from '../constants'
+
 const Wrapper = styled.div`
-  
+  background: linear-gradient(to bottom, ${constants.colorBlueMuted}, ${constants.primarySurfaceColor});
+  border-radius: 4px;
+  box-shadow: 0 2px 2px rgba(0, 0, 0, .3);
+  position: relative;
 `
 
 const AbilityIcon = styled.img`
   border-radius: 4px;
+  display: block;
   height: auto;
+  opacity: .7;
+  overflow: hidden;
+  transition: opacity .2s;
   width: 100%;
+
+  &:hover {
+    opacity: 1;
+  }
+`
+
+const AbilityManaComsumption = styled.div`
+  background: ${constants.colorBlackMuted};
+  color: ${constants.colorMana};
+  border-radius: 2px 0 0 0;
+  font-weight: 600;
+  bottom: 0;
+  font-size: 10px;
+  line-height: 1;
+  padding: 1px 4px;
+  position: absolute;
+  right: 0;
 `
 
 const TooltipWrapper = styled.div`
@@ -54,7 +80,12 @@ const AbilityTooltip = ({ability}) => {
 
 const Ability = (props, abilityID) => {
   const ttId = uuid.v4()
-  console.log(props)
+  const showMana = (props.mc && parseInt(props.mc) > 0)
+  let manaString = false
+  
+  if (showMana) {
+    manaString = ((typeof props.mc === 'object') ? props.mc[0] : props.mc)
+  }
 
   return (
     <Wrapper data-tip={true} data-for={ttId}>
@@ -62,6 +93,7 @@ const Ability = (props, abilityID) => {
       <ReactTooltip id={ttId} effect="solid" place="bottom">
         <AbilityTooltip ability={props} />
       </ReactTooltip>
+      {(showMana) && <AbilityManaComsumption>{manaString}</AbilityManaComsumption>}
     </Wrapper>
   )
 }
