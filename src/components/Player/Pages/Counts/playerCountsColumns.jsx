da335c3cba1ef24a48ef988a79aa16b29cdbd747
Argn0 <Argn0@users.noreply.github.com>
2018-06-22 22:26:23 +0200

    removed playerCountsOverviewColumns

--- src/components/Player/Pages/Counts/playerCountsColumns.jsx
+++ src/components/Player/Pages/Counts/playerCountsColumns.jsx
@@ -1,6 +1,3 @@
-import { transformations } from '../../../../utility';
-
-
 export default strings => ([{
   displayName: strings.th_category,
   field: 'category',
@@ -16,21 +13,3 @@ export default strings => ([{
   sortFn: row => row.winPercent / 100, // percentBars expects decimal value
   percentBars: true,
 }]);
-
-export const playerCountsOverviewColumns = strings => [{
-  displayName: strings.th_category,
-  field: 'category',
-  sortFn: true,
-  displayFn: transformations.category,
-}, {
-  displayName: strings.th_matches,
-  field: 'matches',
-  sortFn: true,
-  displayFn: transformations.matches,
-}, {
-  displayName: strings.th_win,
-  field: 'winPercent',
-  sortFn: row => row.winPercent / 100, // percentBars expects decimal value
-  percentBars: true,
-}];
-
