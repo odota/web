05dedaeaaa758560c5c7a922ca4998fd145bb244
Argn0 <Argn0@users.noreply.github.com>
2018-06-23 22:01:45 +0200

    lint

--- src/components/Visualizations/GaugeChart.jsx
+++ src/components/Visualizations/GaugeChart.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import constants from '../constants';
 
 const Styled = styled.div` 
    font-size: 60%;
