3de639f32bbb3eec8bf805ff4e1f9eb8e38e1461
Dustin Dawes <gu3st@dustindawes.com>
2018-07-23 19:54:32 -0700

    another one

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import 'core-js/fn/number/is-nan';
 import 'core-js/fn/number/is-integer';
 import 'core-js/fn/number/is-finite';
 import 'core-js/fn/string/starts-with';
+import 'core-js/fn/string/includes';
 import React from 'react';
 import createHistory from 'history/createBrowserHistory';
 import ReactGA from 'react-ga';
