d21b0038db682ed8e4394602534358f7201a4709
Dustin Dawes <gu3st@dustindawes.com>
2018-07-23 19:25:35 -0700

    another one

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import 'core-js/fn/array/includes';
 import 'core-js/fn/array/find';
 import 'core-js/fn/array/fill';
 import 'core-js/fn/number/is-nan';
+import 'core-js/fn/number/is-integer'
 import React from 'react';
 import createHistory from 'history/createBrowserHistory';
 import ReactGA from 'react-ga';
