98b23c75f63f0e19af908c9d9759dc2c47a87bd8
Darth Araknus <dartharaknus@gmail.com>
2018-07-25 16:52:28 +0200

    Testing chromatic

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { configure, addDecorator } from '@storybook/react';
+import 'storybook-chromatic';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
