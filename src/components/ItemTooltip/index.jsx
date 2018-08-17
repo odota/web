2d1eec06b84379d090b553afdff7a9a7f245c576
Argn0 <Argn0@users.noreply.github.com>
2018-08-17 02:42:00 +0200

    Update index.jsx

--- src/components/ItemTooltip/index.jsx
+++ src/components/ItemTooltip/index.jsx
@@ -236,7 +236,7 @@ const ItemTooltip = ({ item, inflictor }) => (
       return null;
     })}
     {item.hint && <Hint>{item.hint}</Hint>}
-    {item.lore && <Lore> {item.lore} </Lore>}
+    {item.lore && <Lore>{item.lore}</Lore>}
     {item.components &&
     <Components>
       <div id="header">Components:</div>
