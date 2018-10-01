f811ebcd2a1c04d1ed90a63706ad4a9ebc1f3ba3
Argn0 <Argn0@users.noreply.github.com>
2018-10-02 00:03:55 +0200

    add legacy match

--- testcafe/tests/matches.js
+++ testcafe/tests/matches.js
@@ -15,15 +15,21 @@ const paths = [
   '/matches/4080856812/benchmarks',
   '/matches/4080856812/draft',
   '/matches/4080856812/performances',
+  '/matches/4080856812/laning',
   '/matches/4080856812/combat',
   '/matches/4080856812/farm',
   '/matches/4080856812/items',
   '/matches/4080856812/graphs',
+  '/matches/4080856812/casts',
   '/matches/4080856812/objectives',
+  '/matches/4080856812/vision',
   '/matches/4080856812/actions',
+  '/matches/4080856812/teamfights',
   '/matches/4080856812/analysis',
   '/matches/4080856812/cosmetics',
+  '/matches/4080856812/log',
   '/matches/4080856812/fantasy',
+  '/matches/4080856812/chat',
   '/matches/4080856812/story',
 ];
 
@@ -40,48 +46,39 @@ paths.forEach((p) => {
   });
 });
 
-test
-  .page`${host}/matches/4080856812/vision`('/matches/4080856812/vision', async (t) => {
-  const checkboxes = Selector('.vision-filter').find('input');
-  const checkboxCount = await checkboxes.count;
-  for (let i = 0; i < checkboxCount; i++) {
-    await t
-      .click(checkboxes.nth(i))
-      .wait(100)
-      .click(checkboxes.nth(i));
-  }
-});
 
-test
-  .page`${host}/matches/4080856812/laning`('/matches/4080856812/laning', async (t) => {
-  const checkboxes = Selector('.innerContainer').find('input');
-  const checkboxCount = await checkboxes.count;
-  for (let i = 0; i < checkboxCount; i++) {
-    await t
-      .click(checkboxes.nth(i))
-      .wait(100);
-  }
-});
+const legacy = [
+  '/matches/2472899185/overview',
+  '/matches/2472899185/benchmarks',
+  '/matches/2472899185/draft',
+  '/matches/2472899185/performances',
+  '/matches/2472899185/laning',
+  '/matches/2472899185/combat',
+  '/matches/2472899185/farm',
+  '/matches/2472899185/items',
+  '/matches/2472899185/graphs',
+  '/matches/2472899185/casts',
+  '/matches/2472899185/objectives',
+  '/matches/2472899185/vision',
+  '/matches/2472899185/actions',
+  '/matches/2472899185/teamfights',
+  '/matches/2472899185/analysis',
+  '/matches/2472899185/cosmetics',
+  '/matches/2472899185/log',
+  '/matches/2472899185/fantasy',
+  '/matches/2472899185/chat',
+  '/matches/2472899185/story',
+];
 
-test
-  .page`${host}/matches/4080856812/teamfights`('/matches/4080856812/teamfights', async (t) => {
-  const svgs = Selector('.events').find('svg');
-  const svgCount = await svgs.count;
-  for (let i = 0; i < svgCount; i++) {
-    await t
-      .click(svgs.nth(i))
-      .wait(100);
-  }
-});
+fixture`matches/ paths (legacy)`
+  .requestHooks(fixtureRequestHooks)
+  .before(fixtureBeforeHook)
+  .beforeEach(fixtureBeforeEachHook)
+  .after(fixtureAfterHook);
 
-test
-  .page`${host}/matches/4080856812/chat`('/matches/4080856812/chat', async (t) => {
-  const filters = Selector('.Filters').find('input');
-  const filtersCount = await filters.count;
-  for (let i = 0; i < filtersCount; i++) {
-    await t
-      .click(filters.nth(i))
-      .wait(100)
-      .click(filters.nth(i));
-  }
+legacy.forEach((p) => {
+  test
+    .page`${host}${p}`(p, async (t) => {
+    await t.hover(Selector('#root'));
+  });
 });
