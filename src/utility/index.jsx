585101a2dabd5902c0ec3c6156ff0a57f339ece4
howardzchung@gmail.com <howardzchung@gmail.com>
2018-11-17 16:07:11 -0800

    fixes #1927

--- src/utility/index.jsx
+++ src/utility/index.jsx
@@ -239,10 +239,10 @@ export const defaultSort = (array, sortState, sortField, sortFn) =>
   });
 
 export const SORT_ENUM = {
-  0: 'asc',
-  1: 'desc',
-  asc: 0,
-  desc: 1,
+  0: 'desc',
+  1: 'asc',
+  asc: 1,
+  desc: 0,
   next: state => SORT_ENUM[(state >= 1 ? 0 : state + 1)],
 };
 
