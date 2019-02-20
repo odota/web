a52440d76aacad32f3f9f565dd37005a126095ee
Argn0 <Argn0@users.noreply.github.com>
2019-02-20 15:33:37 +0100

    remove extra container

--- src/components/Player/Header/PlayerHeader.jsx
+++ src/components/Player/Header/PlayerHeader.jsx
@@ -318,12 +318,10 @@ const PlayerHeader = ({
             <PlayerStats playerId={playerId} loggedInId={loggedInUser && String(loggedInUser.account_id)} compact={!small} />
             <PlayerButtons playerId={playerId} playerSoloCompetitiveRank={playerSoloCompetitiveRank} compact={!small} />
           </div>
-          <div>
             <div style={{display: 'flex'}}>
               {getDotaPlusBadge(plus, strings)}
               {getRankTierMedal(rankTier, leaderboardRank)}
             </div>
-          </div>
         </div>
       </div>
     </Styled>
