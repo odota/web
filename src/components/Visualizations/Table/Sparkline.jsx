433aca8ab3ad137ca606efeddc3ad2b71f6f3202
litch <litch@me.com>
2019-07-02 09:15:58 -0500

    Index is set from zero since negative times have been filtered already

--- src/components/Visualizations/Table/Sparkline.jsx
+++ src/components/Visualizations/Table/Sparkline.jsx
@@ -43,7 +43,7 @@ const Sparkline = ({
     const delta = v - lastValue;
     lastValue = v;
     return { delta, cumulative: v };
-  }).slice(1, 11);
+  }).slice(0, 11);
 
   return (
     <StyledContainer>
