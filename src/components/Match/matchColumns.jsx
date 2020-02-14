31b90169037a3e180fe72cf526a3eb5f8262f5b3
Argn0 <29599118+Argn0@users.noreply.github.com>
2020-02-14 08:40:55 +0100

    neutral items

--- src/components/Match/matchColumns.jsx
+++ src/components/Match/matchColumns.jsx
@@ -361,27 +361,27 @@ export default (strings) => {
         },
       },
     ].concat(match.players.map(player => player.item_neutral).reduce(sum, 0) > 0
-    ? {
-      field: 'item_neutral',
-      width: 20,
-      paddingRight: 23,
-      paddingLeft: 5,
-      displayFn: row => 
-        <div style={{height: 30, width: 30}}>
-          {inflictorWithValue(itemIds[row.item_neutral], null, 'neutral')}
-        </div>,
-    }
-    : [])
-    .concat(match.players.map(player => player.permanent_buffs && player.permanent_buffs.length).reduce(sum, 0) > 0
       ? {
-        displayName: strings.th_permanent_buffs,
-        tooltip: strings.tooltip_permanent_buffs,
-        field: 'permanent_buffs',
-        width: 135,
-        displayFn: row =>
-          (row.permanent_buffs && row.permanent_buffs.length > 0 ? row.permanent_buffs.map(buff => inflictorWithValue(buffs[buff.permanent_buff], buff.stack_count, 'buff')) : '-'),
+        field: 'item_neutral',
+        width: 20,
+        paddingRight: 23,
+        paddingLeft: 5,
+        displayFn: (row, field) =>
+          <div style={{ height: 30, width: 30 }}>
+            {field ? inflictorWithValue(itemIds[field], null, 'neutral') : null}
+          </div>,
       }
-      : []);
+      : [])
+      .concat(match.players.map(player => player.permanent_buffs && player.permanent_buffs.length).reduce(sum, 0) > 0
+        ? {
+          displayName: strings.th_permanent_buffs,
+          tooltip: strings.tooltip_permanent_buffs,
+          field: 'permanent_buffs',
+          width: 135,
+          displayFn: row =>
+            (row.permanent_buffs && row.permanent_buffs.length > 0 ? row.permanent_buffs.map(buff => inflictorWithValue(buffs[buff.permanent_buff], buff.stack_count, 'buff')) : '-'),
+        }
+        : []);
 
     return cols;
   };
