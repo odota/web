63548f173c61a0c65f23476af1a032a9ac788255
Argn0 <Argn0@users.noreply.github.com>
2020-10-13 17:33:24 +0200

    thead color

--- src/components/Table/Styled.jsx
+++ src/components/Table/Styled.jsx
@@ -15,7 +15,7 @@ export const StyledBody = styled.div`
           
     thead {
       border-bottom: 1px solid rgba(255, 255, 255, .05) !important; 
-      background: linear-gradient(90deg, rgba(20, 255, 212, 0.09) 2%, rgba(10, 77, 109, 0.26) 27%, rgba(5, 181, 249, 0.04) 80%);
+      background: linear-gradient(90deg, rgba(20, 255, 212, 0.09) 2%, rgba(46, 160, 214, 0.26) 27%, rgba(5, 181, 249, 0.04) 80%);
     }
 
     tr {
