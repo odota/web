eb416bae5e0b28e81129ed642f46e283eb5230c4
Argn0 <Argn0@users.noreply.github.com>
2020-10-14 20:04:37 +0200

    safari only background

--- src/components/Table/Styled.jsx
+++ src/components/Table/Styled.jsx
@@ -20,6 +20,12 @@ export const StyledBody = styled.div`
         background: -moz-linear-gradient(90deg, rgba(20,255,212,0.09) 2%, rgba(46,160,214,0.26) 27%, rgba(5,181,249,0.04) 80%);
         background: -webkit-linear-gradient(90deg, rgba(20,255,212,0.09) 2%, rgba(46,160,214,0.26) 27%, rgba(5,181,249,0.04) 80%); 
         background: linear-gradient(90deg, rgba(20,255,212,0.09) 2%, rgba(46,160,214,0.26) 27%, rgba(5,181,249,0.04) 80%);
+
+        /* Safari */
+        @media not all and (min-resolution:.001dpcm)
+        { @supports (-webkit-appearance:none) {
+          background: rgba(46, 160, 214, 0.26);
+        }}
       }
     }
 
