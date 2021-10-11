baf85e9aaa99fc0f2da6279b87c1fd9490ded298
Roman Gusev <Roman_Gusev1@epam.com>
2021-10-11 16:26:57 +0600

    Improve icon links a11y
    https://www.w3.org/WAI/WCAG21/Techniques/aria/ARIA8.html

--- src/components/Footer/SocialLinks.jsx
+++ src/components/Footer/SocialLinks.jsx
@@ -3,21 +3,23 @@ import { IconGithub, IconDiscord } from '../Icons';
 import { GITHUB_REPO, DISCORD_LINK } from '../../config';
 
 export default ({ strings }) => {
-  const links = [{
-    tooltip: strings.app_github,
-    path: `//github.com/${GITHUB_REPO}`,
-    icon: <IconGithub />,
-  }];
+  const links = [
+    {
+      tooltip: strings.app_github,
+      path: `//github.com/${GITHUB_REPO}`,
+      icon: <IconGithub aria-hidden />,
+    },
+  ];
 
   if (DISCORD_LINK) {
     links.push({
       tooltip: strings.app_discord,
       path: `//discord.gg/${DISCORD_LINK}`,
-      icon: <IconDiscord />,
+      icon: <IconDiscord aria-hidden />,
     });
   }
 
-  return links.map(link => (
+  return links.map((link) => (
     <a
       key={link.path}
       target="_blank"
@@ -25,6 +27,7 @@ export default ({ strings }) => {
       data-hint-position="top"
       data-hint={link.tooltip}
       href={link.path}
+      aria-label={link.tooltip}
     >
       {link.icon}
     </a>
