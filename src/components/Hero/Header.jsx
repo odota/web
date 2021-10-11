dd43b4be85b00c4c930d967bf5f312711db7e659
Roman Gusev <Roman_Gusev1@epam.com>
2021-10-11 16:09:45 +0600

    Remove extra explicit z-indices

--- src/components/Hero/Header.jsx
+++ src/components/Hero/Header.jsx
@@ -5,7 +5,7 @@ import constants from '../constants';
 import AttributesMain from './AttributesMain';
 import Abilities from './Abilities';
 
-const getHeroImgSrc = src => process.env.REACT_APP_IMAGE_CDN + src;
+const getHeroImgSrc = (src) => process.env.REACT_APP_IMAGE_CDN + src;
 
 const HeroProfile = styled.div`
   background: ${constants.almostBlack};
@@ -20,20 +20,18 @@ const HeroProfileBackground = styled.img`
   height: 125%;
   left: -12.5%;
   object-fit: cover;
-  opacity: .35;
+  opacity: 0.35;
   position: absolute;
   top: -12.5%;
   width: 125%;
-  z-index: 1;
 `;
 
 const HeroProfileContent = styled.div`
   align-items: center;
-  box-shadow: inset 0 0 125px rgba(0, 0, 0, .25);
+  box-shadow: inset 0 0 125px rgba(0, 0, 0, 0.25);
   display: flex;
   padding: 56px;
   position: relative;
-  z-index: 2;
 
   @media screen and (max-width: ${constants.wrapTablet}) {
     padding: 16px;
@@ -57,7 +55,7 @@ const HeroDetails = styled.div`
 const HeroAvatar = styled.img`
   border-radius: 16px;
   border: solid 1px rgba(0, 0, 0, 0.3);
-  box-shadow: 0 12px 32px rgba(0, 0, 0, .3);
+  box-shadow: 0 12px 32px rgba(0, 0, 0, 0.3);
   display: block;
   flex-shrink: 0;
   height: 128px;
@@ -115,12 +113,17 @@ const HeroStatsWrapper = styled.div`
 const Header = ({ hero }) => (
   <HeroDescription>
     <HeroProfile>
-      <HeroProfileBackground alt={hero.localized_name} src={getHeroImgSrc(hero.img)} />
+      <HeroProfileBackground
+        alt={hero.localized_name}
+        src={getHeroImgSrc(hero.img)}
+      />
       <HeroProfileContent>
         <HeroAvatar alt={hero.localized_name} src={getHeroImgSrc(hero.img)} />
         <HeroDetails>
           <HeroName>{hero.localized_name}</HeroName>
-          <HeroRoleInformations>{hero.attack_type} - <HeroRoles>{hero.roles.join(', ')}</HeroRoles></HeroRoleInformations>
+          <HeroRoleInformations>
+            {hero.attack_type} - <HeroRoles>{hero.roles.join(', ')}</HeroRoles>
+          </HeroRoleInformations>
         </HeroDetails>
         <HeroStatsWrapper>
           <AttributesMain hero={hero} />
