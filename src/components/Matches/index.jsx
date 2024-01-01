d71df195c61f5f4731645ddd3463abda79dccf49
Howard Chung <howardzchung@gmail.com>
2024-01-01 15:36:06 -0800

    Update index.jsx

--- src/components/Matches/index.jsx
+++ src/components/Matches/index.jsx
@@ -91,13 +91,13 @@ const publicMatchesColumns = strings => [
   {
     displayName: <StyledTeamIconContainer>{strings.general_radiant}</StyledTeamIconContainer>,
     field: 'radiant_team',
-    displayFn: (row, col, field) => (field || '').split(',').map(heroId =>
+    displayFn: (row, col, field) => field?.map(heroId =>
       (heroes[heroId] ? <HeroImage id={heroId} key={heroId} style={{ width: '50px' }} alt="" /> : null)),
   },
   {
     displayName: <StyledTeamIconContainer >{strings.general_dire}</StyledTeamIconContainer>,
     field: 'dire_team',
-    displayFn: (row, col, field) => (field || '').split(',').map(heroId =>
+    displayFn: (row, col, field) => field?.map(heroId =>
       (heroes[heroId] ? <HeroImage id={heroId} key={heroId} style={{ width: '50px' }} alt="" /> : null)),
   },
 ];
