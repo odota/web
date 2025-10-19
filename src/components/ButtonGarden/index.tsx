287d5ad50dec52fd77cb0e718884a836e718eabd
Howard Chung <howardzchung@gmail.com>
2025-10-19 00:06:02 +0000

    convert ButtonGarden to ts

---
+++ src/components/ButtonGarden/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+import useStrings from '../../hooks/useStrings.hook';
+
+const ButtonGarden = ({
+  buttonNames, selectedButton, onClick
+}: {buttonNames: string[], selectedButton: string, onClick: Function }) => {
+  const strings = useStrings();
+  return <SelectField
+    floatingLabelText={strings.explorer_select}
+    value={selectedButton}
+    onChange={(event, index, value) => onClick(value)}
+  >
+    {buttonNames.map(buttonName => (<MenuItem
+      value={buttonName}
+      key={buttonName}
+      primaryText={strings[`heading_${buttonName}` as keyof typeof strings]}
+    />))}
+  </SelectField>;
+};
+
+export default ButtonGarden;
