25dfffbc7cc0b5efb0753b430e5168c5602818f3
geracosta <gerignacioacosta@icloud.com>
2026-09-16 18:39:07 -0300

    define design tokens as css custom properties

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import store from "./store";
 import { getMetadata, getStrings } from "./actions";
 import App from "./components/App/App";
 // import { unregister } from './common/serviceWorker';
+import "./tokens.css";
 import "./index.css";
 
 // Fetch metadata (used on all pages)
